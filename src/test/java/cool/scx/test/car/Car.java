49f56938ef0ca40d8658b4e27ec70640b4a0e3ae
scx567888 <scx567888@outlook.com>
2021-12-12 10:39:45 +0800

    1.9.18

--- src/test/java/cool/scx/test/car/Car.java
+++ src/test/java/cool/scx/test/car/Car.java
@@ -1,11 +1,13 @@
-package cool.scx.test.car_module;
+package cool.scx.test.car;
 
 import cool.scx.annotation.Column;
 import cool.scx.annotation.ScxModel;
 import cool.scx.base.BaseModel;
 
-@ScxModel
+@ScxModel(tablePrefix = "test")
 public class Car extends BaseModel {
+
     @Column(unique = true)
     public String name;
+
 }
