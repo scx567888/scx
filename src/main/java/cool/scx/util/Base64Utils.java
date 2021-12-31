e6c55b597e81b881c9a6c3617c6c819f54ff3d39
scx567888 <scx567888@outlook.com>
2021-12-31 18:39:58 +0800

    1, 添加 加密解密实用方法 (#74)

--- src/main/java/cool/scx/util/Base64Utils.java
+++ src/main/java/cool/scx/util/Base64Utils.java
@@ -1,5 +1,6 @@
 package cool.scx.util;
 
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
@@ -21,11 +22,22 @@ public final class Base64Utils {
     /**
      * 根据 字符串 获取(加密) BASE64
      *
-     * @param str a {@link java.lang.String} object
+     * @param str a {@link String} object
      * @return a {@link java.lang.String} object
      */
-    public static String encode(String str) {
+    public static byte[] encode(String str) {
         return encode(str.getBytes(StandardCharsets.UTF_8));
+    }
+
+    /**
+     * a
+     *
+     * @param str     a
+     * @param charset a
+     * @return a
+     */
+    public static byte[] encode(String str, Charset charset) {
+        return encode(str.getBytes(charset));
     }
 
     /**
@@ -34,8 +46,29 @@ public final class Base64Utils {
      * @param base64 a {@link java.lang.String} object
      * @return a {@link java.lang.String} object
      */
-    public static String decode(String base64) {
-        return new String(defaultBase64Decoder.decode(base64), StandardCharsets.UTF_8);
+    public static byte[] decode(String base64) {
+        return decode(base64, StandardCharsets.UTF_8);
+    }
+
+    /**
+     * a
+     *
+     * @param base64  a
+     * @param charset a
+     * @return a
+     */
+    public static byte[] decode(String base64, Charset charset) {
+        return decode(base64.getBytes(charset));
+    }
+
+    /**
+     * 根据 BASE64 获取(解密) 字符串
+     *
+     * @param base64 a {@link java.lang.String} object
+     * @return a {@link java.lang.String} object
+     */
+    public static byte[] decode(byte[] base64) {
+        return defaultBase64Decoder.decode(base64);
     }
 
     /**
@@ -44,8 +77,8 @@ public final class Base64Utils {
      * @param bytes a {@link java.lang.String} object
      * @return a {@link java.lang.String} object
      */
-    public static String encode(byte[] bytes) {
-        return new String(defaultBase64Encoder.encode(bytes), StandardCharsets.UTF_8);
+    public static byte[] encode(byte[] bytes) {
+        return defaultBase64Encoder.encode(bytes);
     }
 
 }
