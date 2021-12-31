8343a143a2cada7adc5a891099ed0f1989ab0929
scx567888 <scx567888@outlook.com>
2021-12-31 15:38:40 +0800

    1, 识别 accept 以确定返回值类型

--- src/main/java/cool/scx/mvc/return_value_handler/impl/LastMethodReturnValueHandler.java
+++ src/main/java/cool/scx/mvc/return_value_handler/impl/LastMethodReturnValueHandler.java
@@ -3,6 +3,8 @@ package cool.scx.mvc.return_value_handler.impl;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import cool.scx.mvc.return_value_handler.ScxMappingMethodReturnValueHandler;
 import cool.scx.vo.VoHelper;
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpHeaderValues;
 import io.vertx.ext.web.RoutingContext;
 
 /**
@@ -22,7 +24,12 @@ public final class LastMethodReturnValueHandler implements ScxMappingMethodRetur
 
     @Override
     public void handle(Object result, RoutingContext context) throws JsonProcessingException {
-        VoHelper.fillJsonContentType(context.request().response()).end(VoHelper.toJson(result));
+        var accept = context.request().getHeader(HttpHeaderNames.ACCEPT);
+        if (accept != null && accept.toLowerCase().startsWith(HttpHeaderValues.APPLICATION_XML.toString())) {
+            VoHelper.fillXmlContentType(context.request().response()).end(VoHelper.toXml(result));
+        } else {
+            VoHelper.fillJsonContentType(context.request().response()).end(VoHelper.toJson(result));
+        }
     }
 
 }
