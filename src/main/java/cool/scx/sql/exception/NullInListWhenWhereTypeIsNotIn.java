9b8b04df8291ed6401627fd7d7c9673b5aa3f79c
scx567888 <scx567888@outlook.com>
2022-05-12 18:17:11 +0800

    当 WhereType 为 IN 或 NOT IN 时 对参数进行过滤空值和去重操作 close #181

--- src/main/java/cool/scx/sql/exception/NullInListWhenWhereTypeIsNotIn.java
+++
@@ -1,9 +0,0 @@
-package cool.scx.sql.exception;
-
-public final class NullInListWhenWhereTypeIsNotIn extends IllegalArgumentException {
-
-    public NullInListWhenWhereTypeIsNotIn() {
-        super("当 WhereType 为 NOT_IN 时, 参数列表中不允许任何元素为 null (因为会造成 SQL 查询时无数据以产生歧义) !!!");
-    }
-
-}
