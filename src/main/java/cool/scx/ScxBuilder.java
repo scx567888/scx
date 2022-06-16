0d6e522094be0615d9f0d0ba48829d095cc4316b
scx567888 <scx567888@outlook.com>
2022-06-16 16:41:13 +0800

    1

--- src/main/java/cool/scx/ScxBuilder.java
+++ src/main/java/cool/scx/ScxBuilder.java
@@ -148,7 +148,7 @@ public final class ScxBuilder {
         var scxEnvironment = new ScxEnvironment(mainClass);
         //配置源 注意顺序 以保证可以逐个覆盖
         var defaultMapConfigSource = MapConfigSource.of(DEFAULT_CONFIG_MAP);
-        var defaultJsonFileConfigSource = JsonFileConfigSource.of(scxEnvironment.getFileByAppRoot(DEFAULT_SCX_CONFIG_PATH));
+        var defaultJsonFileConfigSource = JsonFileConfigSource.of(scxEnvironment.getPathByAppRoot(DEFAULT_SCX_CONFIG_PATH).toFile());
         var defaultArgsConfigSource = ArgsConfigSource.of(args);
         scxConfigSources.add(defaultMapConfigSource);
         scxConfigSources.add(defaultJsonFileConfigSource);
