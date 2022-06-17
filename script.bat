b28c02b50d89bc9c2d803bfdf3df7274875822b1
scx567888 <scx567888@outlook.com>
2022-06-17 09:02:56 +0800

    Add files via upload

--- script.bat
+++ script.bat
@@ -1,2 +1,1 @@
-﻿@echo off
-powershell.exe -ExecutionPolicy RemoteSigned -File ".\script.ps1" -JAVA_HOME "C:\Apps\jdk" -MAVEN_HOME "C:\Apps\ideaIU\plugins\maven\lib\maven3\bin"
+start powershell.exe -ExecutionPolicy RemoteSigned -File ".\script.ps1" -JAVA_HOME "C:\Apps\jdk" -MAVEN_HOME "C:\Apps\ideaIU\plugins\maven\lib\maven3\bin"
