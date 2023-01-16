ff91e34a45df207a81ec7481106a0eeb0ef4ffd1
scx567888 <scx567888@outlook.com>
2023-01-16 15:16:18 +0800

    dev

---
+++ scx-common/src/main/java/cool/scx/util/FormData.java
@@ -0,0 +1,187 @@
+package cool.scx.util;
+
+import io.netty.buffer.Unpooled;
+import io.netty.buffer.UnpooledByteBufAllocator;
+import io.netty.handler.codec.http.DefaultFullHttpRequest;
+import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.HttpVersion;
+import io.netty.handler.codec.http.multipart.DefaultHttpDataFactory;
+import io.netty.handler.codec.http.multipart.HttpPostRequestEncoder;
+import io.netty.handler.codec.http.multipart.MemoryFileUpload;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.http.HttpClientRequest;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+import static cool.scx.util.ScxExceptionHelper.wrap;
+import static cool.scx.util.StringUtils.notBlank;
+import static io.netty.handler.codec.http.multipart.HttpPostRequestEncoder.EncoderMode;
+import static io.netty.handler.codec.http.multipart.HttpPostRequestEncoder.ErrorDataEncoderException;
+import static io.vertx.core.http.impl.MimeMapping.getMimeTypeForExtension;
+import static io.vertx.core.http.impl.MimeMapping.getMimeTypeForFilename;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.util.Objects.requireNonNull;
+
+/**
+ * a
+ *
+ * @author scx567888
+ * @version 0.0.1
+ */
+public final class FormData {
+
+    private static final UnpooledByteBufAllocator ALLOC = new UnpooledByteBufAllocator(false);
+
+    private final List<FormDataItem> items = new ArrayList<>();
+
+    private static HttpPostRequestEncoder initEncoder(FormData formData, HttpRequest request) throws ErrorDataEncoderException, IOException {
+        var encoder = new HttpPostRequestEncoder(new DefaultHttpDataFactory(), request, true, UTF_8, EncoderMode.HTML5);
+        for (var formDataPart : formData.items) {
+            switch (formDataPart.type) {
+                case ATTRIBUTE -> {
+                    encoder.addBodyAttribute(formDataPart.name, formDataPart.attributeValue);
+                }
+                case FILE_UPLOAD_PATH -> {
+                    encoder.addBodyFileUpload(formDataPart.name,
+                            formDataPart.filename, formDataPart.fileUploadPath.toFile(),
+                            formDataPart.contentType, false);
+                }
+                case FILE_UPLOAD_BYTES -> {
+                    var fileUpload = new MemoryFileUpload(
+                            formDataPart.name,
+                            formDataPart.filename,
+                            formDataPart.contentType, null, null, formDataPart.fileUploadPathBytes.length);
+                    fileUpload.setContent(Unpooled.buffer().writeBytes(formDataPart.fileUploadPathBytes));
+                    encoder.addBodyHttpData(fileUpload);
+                }
+            }
+        }
+        encoder.finalizeRequest();
+        return encoder;
+    }
+
+    public FormData attribute(String name, Object text) {
+        this.items.add(new FormDataItem(name, text.toString()));
+        return this;
+    }
+
+    public FormData fileUpload(String name, byte[] fileByte) {
+        this.items.add(new FormDataItem(name, fileByte));
+        return this;
+    }
+
+    public FormData fileUpload(String name, byte[] fileByte, String filename) {
+        this.items.add(new FormDataItem(name, fileByte, filename));
+        return this;
+    }
+
+    public FormData fileUpload(String name, byte[] fileByte, String filename, String contentType) {
+        this.items.add(new FormDataItem(name, fileByte, filename, contentType));
+        return this;
+    }
+
+    public FormData fileUpload(String name, Path filePath) {
+        this.items.add(new FormDataItem(name, filePath));
+        return this;
+    }
+
+    public FormData fileUpload(String name, Path filePath, String filename) {
+        this.items.add(new FormDataItem(name, filePath, filename));
+        return this;
+    }
+
+    public FormData fileUpload(String name, Path filePath, String filename, String contentType) {
+        this.items.add(new FormDataItem(name, filePath, filename, contentType));
+        return this;
+    }
+
+    public FormData remove(String name) {
+        this.items.removeIf(formDataItem -> name.equals(formDataItem.name));
+        return this;
+    }
+
+    public void write(HttpClientRequest clientRequest) {
+        wrap(() -> {
+            clientRequest.setChunked(true);
+            var request = new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.POST, "/");
+            var encoder = initEncoder(this, request);
+            request.headers().forEach((k) -> {
+                clientRequest.putHeader(k.getKey(), k.getValue());
+            });
+            write0(clientRequest, encoder);
+        });
+    }
+
+    private void write0(HttpClientRequest clientRequest, HttpPostRequestEncoder encoder) throws Exception {
+        if (!encoder.isEndOfInput()) {
+            var chunk = encoder.readChunk(ALLOC);
+            var content = chunk.content();
+            var buff = Buffer.buffer(content);
+            clientRequest.write(buff).onSuccess(c -> wrap(() -> write0(clientRequest, encoder)));
+        } else {
+            clientRequest.end();
+        }
+    }
+
+    static final class FormDataItem {
+        private final FormDataItemType type;
+        private final String name;
+        private final String filename;
+        private final String contentType;
+        private String attributeValue;
+        private Path fileUploadPath;
+        private byte[] fileUploadPathBytes;
+
+        FormDataItem(String name, String value) {
+            this.type = FormDataItemType.ATTRIBUTE;
+            this.name = requireNonNull(name);
+            this.attributeValue = value;
+            this.filename = null;
+            this.contentType = null;
+        }
+
+        FormDataItem(String name, byte[] content) {
+            this(name, content, RandomUtils.randomUUID());
+        }
+
+        FormDataItem(String name, byte[] content, String filename) {
+            this(name, content, filename, getMimeTypeForFilename(filename));
+        }
+
+        FormDataItem(String name, byte[] content, String filename, String contentType) {
+            this.type = FormDataItemType.FILE_UPLOAD_BYTES;
+            this.name = requireNonNull(name);
+            this.fileUploadPathBytes = content;
+            this.filename = notBlank(filename) ? filename : RandomUtils.randomUUID();
+            this.contentType = notBlank(contentType) ? contentType : getMimeTypeForExtension("bin");
+        }
+
+        FormDataItem(String name, Path filePath) {
+            this(name, filePath, filePath.getFileName().toString());
+        }
+
+        FormDataItem(String name, Path filePath, String filename) {
+            this(name, filePath, filename, getMimeTypeForFilename(filename));
+        }
+
+        FormDataItem(String name, Path filePath, String filename, String contentType) {
+            this.type = FormDataItemType.FILE_UPLOAD_PATH;
+            this.name = requireNonNull(name);
+            this.fileUploadPath = filePath;
+            this.filename = notBlank(filename) ? filename : RandomUtils.randomUUID();
+            this.contentType = notBlank(contentType) ? contentType : getMimeTypeForExtension("bin");
+        }
+
+    }
+
+    enum FormDataItemType {
+        ATTRIBUTE,
+        FILE_UPLOAD_PATH,
+        FILE_UPLOAD_BYTES,
+    }
+
+}
