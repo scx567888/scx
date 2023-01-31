65eeda7a5a92e0d1fa0c8f8242cfb07acb99d28a
scx567888 <scx567888@outlook.com>
2023-01-31 14:30:22 +0800

    2.0.9

---
+++ scx-mvc/src/main/java/cool/scx/mvc/ScxMvcOptions.java
@@ -0,0 +1,55 @@
+package cool.scx.mvc;
+
+import cool.scx.util.FileUtils;
+
+import java.nio.file.Path;
+
+public class ScxMvcOptions {
+
+    private boolean useDevelopmentErrorPage;
+    private String allowedOrigin;
+    private Path uploadsDirectory;
+    private long bodyLimit;
+
+    public ScxMvcOptions() {
+        // 默认 http 请求 body 限制大小
+        this.bodyLimit = FileUtils.displaySizeToLong("16384KB");
+    }
+
+    public boolean useDevelopmentErrorPage() {
+        return this.useDevelopmentErrorPage;
+    }
+
+    public String allowedOrigin() {
+        return this.allowedOrigin;
+    }
+
+    public Path uploadsDirectory() {
+        return this.uploadsDirectory;
+    }
+
+    public long bodyLimit() {
+        return this.bodyLimit;
+    }
+
+    public ScxMvcOptions useDevelopmentErrorPage(boolean useDevelopmentErrorPage) {
+        this.useDevelopmentErrorPage = useDevelopmentErrorPage;
+        return this;
+    }
+
+    public ScxMvcOptions allowedOrigin(String allowedOrigin) {
+        this.allowedOrigin = allowedOrigin;
+        return this;
+    }
+
+    public ScxMvcOptions uploadsDirectory(Path uploadsDirectory) {
+        this.uploadsDirectory = uploadsDirectory;
+        return this;
+    }
+
+    public ScxMvcOptions bodyLimit(long bodyLimit) {
+        this.bodyLimit = bodyLimit;
+        return this;
+    }
+
+}
