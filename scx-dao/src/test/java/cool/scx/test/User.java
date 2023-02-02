b20dfa34a5aa046c173e3a9c74322d422b336b64
scx567888 <scx567888@outlook.com>
2023-02-02 17:57:18 +0800

    2.0.9

--- scx-dao/src/test/java/cool/scx/test/User.java
+++ scx-dao/src/test/java/cool/scx/test/User.java
@@ -1,9 +1,11 @@
 package cool.scx.test;
 
 import cool.scx.dao.annotation.Column;
+import cool.scx.dao.annotation.Table;
 
 import java.time.LocalDateTime;
 
+@Table(tablePrefix = "scx_dao")
 public class User {
     @Column(primaryKey = true, autoIncrement = true)
     public Long id;
