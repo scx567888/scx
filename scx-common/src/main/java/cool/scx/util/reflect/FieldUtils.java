edbe5c40d223bbeb9b22b790abbaaebc02dc9210
scx567888 <scx567888@outlook.com>
2023-02-03 16:21:37 +0800

    dev

---
+++ scx-common/src/main/java/cool/scx/util/reflect/FieldUtils.java
@@ -0,0 +1,21 @@
+package cool.scx.util.reflect;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+
+public final class FieldUtils {
+
+    public static Field[] findFields(Class<?> clazz) {
+        var list = new ArrayList<Field>();
+        while (clazz != null && !clazz.isInterface()) {
+            var fields = clazz.getDeclaredFields();
+            for (var field : fields) {
+                field.setAccessible(true);
+                list.add(field);
+            }
+            clazz = clazz.getSuperclass();
+        }
+        return list.toArray(Field[]::new);
+    }
+
+}
