bc7cb5fd0c5a49a421651338f78f160fe933e057
scx567888 <scx567888@outlook.com>
2023-02-07 11:02:41 +0800

    1, 添加 ArrayUtils

---
+++ scx-common/src/main/java/cool/scx/util/ArrayUtils.java
@@ -0,0 +1,28 @@
+package cool.scx.util;
+
+public final class ArrayUtils {
+
+    /**
+     * indexOf 数组版本
+     *
+     * @param a  原数组
+     * @param a1 带查找的数组
+     * @return 索引
+     */
+    public static int indexOf(byte[] a, byte[] a1) {
+        for (int i = 0; i <= a.length - a1.length; i = i + 1) {
+            var found = true;
+            for (int j = 0; j < a1.length; j = j + 1) {
+                if (a[i + j] != a1[j]) {
+                    found = false;
+                    break;
+                }
+            }
+            if (found) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+}
