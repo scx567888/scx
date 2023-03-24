41941db8acdaf6b2b1f2d8b7ef47f5d9b183ddda
scx567888 <scx567888@outlook.com>
2023-03-24 18:48:02 +0800

    dev

--- scx-core/src/main/java/cool/scx/core/ScxHelper.java
+++ scx-core/src/main/java/cool/scx/core/ScxHelper.java
@@ -2,6 +2,7 @@ package cool.scx.core;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.mysql.cj.jdbc.MysqlDataSource;
+import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import cool.scx.config.ScxConfig;
 import cool.scx.config.ScxEnvironment;
@@ -158,8 +159,9 @@ public final class ScxHelper {
             }
         }
         //使用 HikariDataSource 进行包装
-        var hikariDataSource = new HikariDataSource();
-        hikariDataSource.setDataSource(mysqlDataSource);
+        var hikariConfig = new HikariConfig();
+        hikariConfig.setDataSource(mysqlDataSource);
+        var hikariDataSource = new HikariDataSource(hikariConfig);
         return scxFeatureConfig.get(ScxCoreFeature.USE_SPY) ? Spy.wrap(hikariDataSource) : hikariDataSource;
     }
 
