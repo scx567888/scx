72b60cefc6437718ea839ad79e4b09e8912c5d43
scx567888 <scx567888@outlook.com>
2023-03-25 14:42:27 +0800

    dev

--- scx-sql/src/main/java/cool/scx/sql/ResultHandler.java
+++ scx-sql/src/main/java/cool/scx/sql/ResultHandler.java
@@ -1,6 +1,7 @@
 package cool.scx.sql;
 
 import cool.scx.functional.ScxFunction;
+import cool.scx.sql.result_handler.*;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -12,5 +13,25 @@ import java.sql.SQLException;
  * @version 0.0.7
  */
 public interface ResultHandler<T> extends ScxFunction<ResultSet, T, SQLException> {
+
+    static MapHandler ofMap() {
+        return new MapHandler();
+    }
+
+    static MapListHandler ofMapList() {
+        return new MapListHandler();
+    }
+
+    static <C> BeanHandler<C> ofBean(Class<C> clazz) {
+        return new BeanHandler<>(BeanBuilder.of(clazz));
+    }
+
+    static <C> BeanListHandler<C> ofBeanList(Class<C> clazz) {
+        return new BeanListHandler<>(BeanBuilder.of(clazz));
+    }
+
+    static <C> SingleValueHandler<C> ofSingleValue(String columnName, Class<C> clazz) {
+        return new SingleValueHandler<>(columnName, clazz);
+    }
 
 }
