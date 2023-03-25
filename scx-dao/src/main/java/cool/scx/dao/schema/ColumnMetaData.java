062ec75e3a04240806a5a1ba8fa3c25e79d03979
scx567888 <scx567888@outlook.com>
2023-03-25 19:59:31 +0800

    dev

--- scx-dao/src/main/java/cool/scx/dao/schema/ColumnMetaData.java
+++ scx-dao/src/main/java/cool/scx/dao/schema/ColumnMetaData.java
@@ -1,52 +1,30 @@
 package cool.scx.dao.schema;
 
-import java.sql.DatabaseMetaData;
 import java.util.Objects;
 
-public class ColumnMetaData {
+public record ColumnMetaData(String tableName, String columnName, String typeName, Integer columnSize,
+                             Boolean isNullable, Boolean isAutoincrement, String remarks,
+                             _Column _column) {
 
-    private final TableMetaData table;
-    private final String columnName;
-    private final String typeName;
-    private final Integer columnSize;
-    private final Boolean isNullable;
-    private final Boolean isAutoincrement;
-    private final TableMetaData._Column _column;
-    private final String remarks;
-
-    public ColumnMetaData(TableMetaData tableMetaData, DatabaseMetaData dbMetaData, TableMetaData._Column column) {
-        this.table = tableMetaData;
-        this.columnName = column.COLUMN_NAME();
-        this.typeName = column.TYPE_NAME();
-        this.columnSize = column.COLUMN_SIZE();
-        this.remarks = column.REMARKS();
-        this.isNullable = Objects.equals("YES", column.IS_NULLABLE());
-        this.isAutoincrement = Objects.equals("YES", column.IS_AUTOINCREMENT());
-        this._column = column;
+    public static ColumnMetaData of(_Column _column) {
+        var tabledName = _column.TABLE_NAME();
+        var columnName = _column.COLUMN_NAME();
+        var typeName = _column.TYPE_NAME();
+        var columnSize = _column.COLUMN_SIZE();
+        var remarks = _column.REMARKS();
+        var isNullable = Objects.equals("YES", _column.IS_NULLABLE());
+        var isAutoincrement = Objects.equals("YES", _column.IS_AUTOINCREMENT());
+        return new ColumnMetaData(tabledName, columnName, typeName, columnSize, isNullable, isAutoincrement, remarks, _column);
     }
 
-    public String columnName() {
-        return columnName;
-    }
+    public record _Column(String SCOPE_TABLE, String TABLE_CAT, Integer BUFFER_LENGTH, String IS_NULLABLE,
+                          String TABLE_NAME, String COLUMN_DEF, String SCOPE_CATALOG, String TABLE_SCHEM,
+                          String COLUMN_NAME, Integer NULLABLE, String REMARKS, Integer DECIMAL_DIGITS,
+                          Integer NUM_PREC_RADIX, Integer SQL_DATETIME_SUB, String IS_GENERATEDCOLUMN,
+                          String IS_AUTOINCREMENT, Integer SQL_DATA_TYPE, Integer CHAR_OCTET_LENGTH,
+                          Integer ORDINAL_POSITION, String SCOPE_SCHEMA, String SOURCE_DATA_TYPE, Integer DATA_TYPE,
+                          String TYPE_NAME, Integer COLUMN_SIZE) {
 
-    public String typeName() {
-        return typeName;
-    }
-
-    public Integer columnSize() {
-        return columnSize;
-    }
-
-    public Boolean isAutoincrement() {
-        return isAutoincrement;
-    }
-
-    public Boolean isNullable() {
-        return isNullable;
-    }
-
-    public String remarks() {
-        return remarks;
     }
 
 }
