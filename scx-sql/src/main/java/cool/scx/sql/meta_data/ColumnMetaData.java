ed981117cb78d077a9e20551d858aab2e5ab0f03
scx567888 <scx567888@outlook.com>
2023-03-29 10:46:18 +0800

    dev

--- scx-sql/src/main/java/cool/scx/sql/meta_data/ColumnMetaData.java
+++ scx-sql/src/main/java/cool/scx/sql/meta_data/ColumnMetaData.java
@@ -1,14 +1,17 @@
 package cool.scx.sql.meta_data;
 
-import cool.scx.sql.mapping.ColumnMapping;
-import cool.scx.sql.meta_data.MetaDataHelper._Column;
+import cool.scx.sql.mapping.Column;
 
-public record ColumnMetaData(String tableName, String columnName, String typeName, Integer columnSize,
-                             boolean notNull, boolean autoIncrement,
+public record ColumnMetaData(String table,
+                             String name,
+                             String typeName,
+                             Integer columnSize,
+                             boolean notNull,
+                             boolean autoIncrement,
                              boolean unique,
+                             boolean primaryKey,
                              String defaultValue,
                              String onUpdateValue,
-                             String remarks,
-                             _Column _column) implements ColumnMapping {
+                             String remarks) implements Column {
 
 }
