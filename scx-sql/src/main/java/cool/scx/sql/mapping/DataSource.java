feac6e95c118ef8e4c3bf1157055baf4aa6b642b
scx567888 <scx567888@outlook.com>
2023-03-29 10:36:05 +0800

    dev

---
+++ scx-sql/src/main/java/cool/scx/sql/mapping/DataSource.java
@@ -0,0 +1,7 @@
+package cool.scx.sql.mapping;
+
+public interface DataSource {
+
+    Catalog[] catalogs();
+
+}
--- scx-sql/src/main/java/cool/scx/sql/meta_data/DataSourceMetaData.java
+++ scx-sql/src/main/java/cool/scx/sql/meta_data/DataSourceMetaData.java
@@ -1,12 +1,12 @@
 package cool.scx.sql.meta_data;
 
-import cool.scx.sql.mapping.DataSourceMapping;
+import cool.scx.sql.mapping.DataSource;
 
 import java.sql.DatabaseMetaData;
 
 import static cool.scx.sql.meta_data.MetaDataHelper.initCatalogs;
 
-public final class DataSourceMetaData implements DataSourceMapping {
+public final class DataSourceMetaData implements DataSource {
 
     private CatalogMetaData[] catalogs;
 
