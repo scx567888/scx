78b9ce46017717957bda1a0cb5df3fdb278477b0
scx567888 <scx567888@outlook.com>
2023-03-31 10:52:38 +0800

    dev

--- scx-mvc/src/test/java/cool/scx/test/HelloWorldController.java
+++ scx-mvc/src/test/java/cool/scx/test/HelloWorldController.java
@@ -2,14 +2,14 @@ package cool.scx.test;
 
 import cool.scx.mvc.annotation.ScxRoute;
 import cool.scx.mvc.exception.ForbiddenException;
-import cool.scx.mvc.vo.Data;
+import cool.scx.mvc.vo.Result;
 
 @ScxRoute
 public class HelloWorldController {
 
     @ScxRoute("hello")
     public Object hello() {
-        return Data.ok().put("name", "scx567888😁");
+        return Result.ok().put("name", "scx567888😁");
     }
 
     @ScxRoute("no-perm")
