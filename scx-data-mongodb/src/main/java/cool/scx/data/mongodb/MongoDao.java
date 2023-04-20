66c69ba83b0a982e69fe6d33c31c88fbdc4f8ea2
scx567888 <scx567888@outlook.com>
2023-04-20 16:13:37 +0800

    dev

--- scx-data-mongodb/src/main/java/cool/scx/data/mongodb/MongoDao.java
+++ scx-data-mongodb/src/main/java/cool/scx/data/mongodb/MongoDao.java
@@ -1,34 +1,13 @@
 package cool.scx.data.mongodb;
 
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
 import cool.scx.data.Dao;
 import cool.scx.data.Query;
-import org.bson.Document;
 
 import java.util.Collection;
 import java.util.List;
 
+// todo 
 public class MongoDao<Entity> implements Dao<Entity, String> {
-
-
-    public static void main(String[] args) {
-        String uri = "mongodb://127.0.0.1:27017";
-        try (MongoClient mongoClient = MongoClients.create(uri)) {
-            MongoDatabase database = mongoClient.getDatabase("sample_mflix");
-            MongoCollection<Document> collection = database.getCollection("movies");
-            collection.insertOne(Document.parse("{}"));
-//            Document doc = collection.find(eq("title", "Back to the Future")).first();
-            Document doc = collection.find().first();
-            if (doc != null) {
-                System.out.println(doc.toJson());
-            } else {
-                System.out.println("No matching documents found.");
-            }
-        }
-    }
 
     @Override
     public String insert(Object o) {
