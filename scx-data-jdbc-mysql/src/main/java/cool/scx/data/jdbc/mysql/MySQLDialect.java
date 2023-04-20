f1865f427c2dc8d35ca7fa39a30a794babfc790e
scx567888 <scx567888@outlook.com>
2023-04-20 16:12:08 +0800

    dev

--- scx-data-jdbc-mysql/src/main/java/cool/scx/data/jdbc/mysql/MySQLDialect.java
+++ scx-data-jdbc-mysql/src/main/java/cool/scx/data/jdbc/mysql/MySQLDialect.java
@@ -1,4 +1,4 @@
-package cool.scx.data.jdbc.dialect;
+package cool.scx.data.jdbc.mysql;
 
 import com.mysql.cj.MysqlType;
 import com.mysql.cj.NativeQueryBindings;
@@ -6,6 +6,7 @@ import com.mysql.cj.PreparedQuery;
 import com.mysql.cj.jdbc.ClientPreparedStatement;
 import com.mysql.cj.jdbc.JdbcConnection;
 import com.mysql.cj.jdbc.MysqlDataSource;
+import cool.scx.data.jdbc.dialect.Dialect;
 import cool.scx.data.jdbc.mapping.Column;
 import cool.scx.data.jdbc.mapping.Table;
 
