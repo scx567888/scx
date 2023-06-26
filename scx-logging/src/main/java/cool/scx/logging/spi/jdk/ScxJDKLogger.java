ed6d19fb25d82df8d5ed6bfc7d1df82181ed881f
scx567888 <scx567888@outlook.com>
2023-06-26 11:03:04 +0800

    dev

---
+++ scx-logging/src/main/java/cool/scx/logging/spi/jdk/ScxJDKLogger.java
@@ -0,0 +1,50 @@
+package cool.scx.logging.spi.jdk;
+
+import cool.scx.logging.ScxLogger;
+import cool.scx.logging.ScxLoggerFactory;
+
+import java.util.ResourceBundle;
+
+import static org.slf4j.helpers.MessageFormatter.arrayFormat;
+import static org.slf4j.helpers.NormalizedParameters.getThrowableCandidate;
+import static org.slf4j.helpers.NormalizedParameters.trimmedCopy;
+
+public class ScxJDKLogger implements System.Logger {
+
+    private final ScxLogger scxLogger;
+
+    public ScxJDKLogger(String name) {
+        this.scxLogger = ScxLoggerFactory.getLogger(name);
+    }
+
+    @Override
+    public String getName() {
+        return this.scxLogger.name();
+    }
+
+    @Override
+    public boolean isLoggable(Level level) {
+        return level.getSeverity() >= this.scxLogger.level().getSeverity();
+    }
+
+    @Override
+    public void log(Level level, ResourceBundle bundle, String msg, Throwable thrown) {
+        if (isLoggable(level)) {
+            this.scxLogger.logMessage(level, msg, thrown);
+        }
+    }
+
+    @Override
+    public void log(Level level, ResourceBundle bundle, String format, Object... params) {
+        if (isLoggable(level)) {
+            var throwableCandidate = getThrowableCandidate(params);
+            if (throwableCandidate != null) {
+                var trimmedCopy = trimmedCopy(params);
+                this.scxLogger.logMessage(level, arrayFormat(format, trimmedCopy).getMessage(), throwableCandidate);
+            } else {
+                this.scxLogger.logMessage(level, arrayFormat(format, params).getMessage(), null);
+            }
+        }
+    }
+
+}
