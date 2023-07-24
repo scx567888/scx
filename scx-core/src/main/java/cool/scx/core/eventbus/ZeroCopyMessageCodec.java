37b4c1cc12386897695d2f2768465fa26f3e23c9
scx567888 <scx567888@outlook.com>
2023-07-24 15:22:43 +0800

    dev

--- scx-core/src/main/java/cool/scx/core/eventbus/ZeroCopyMessageCodec.java
+++ scx-core/src/main/java/cool/scx/core/eventbus/ZeroCopyMessageCodec.java
@@ -4,27 +4,18 @@ import io.vertx.core.buffer.Buffer;
 import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.eventbus.MessageCodec;
 
-import java.util.function.Function;
-
 /**
  * 零拷贝编解码器 (因为并不适用于集群模式,所以此处不实现 encodeToWire和decodeFromWire)
  *
  * @author scx567888
  * @version 1.18.0
  */
-public final class ZeroCopyMessageCodec implements MessageCodec<Object, Object> {
+public final class ZeroCopyMessageCodec<T> implements MessageCodec<T, Object> {
 
     /**
      * ZERO_COPY_CODEC_NAME
      */
     public static final String ZERO_COPY_CODEC_NAME = ZeroCopyMessageCodec.class.getName();
-
-    private static final ZeroCopyMessageCodec DEFAULT_INSTANCE = new ZeroCopyMessageCodec();
-
-    private static final Function<Object, String> CODEC_SELECTOR = o -> {
-        var zeroCopyMessage = o.getClass().getAnnotation(ZeroCopyMessage.class);
-        return zeroCopyMessage != null ? ZERO_COPY_CODEC_NAME : null;
-    };
 
     /**
      * <p>registerCodec.</p>
@@ -32,8 +23,11 @@ public final class ZeroCopyMessageCodec implements MessageCodec<Object, Object> 
      * @param eventBus a {@link io.vertx.core.eventbus.EventBus} object
      */
     public static void registerCodec(EventBus eventBus) {
-        eventBus.registerCodec(DEFAULT_INSTANCE);
-        eventBus.codecSelector(CODEC_SELECTOR);
+        eventBus.registerDefaultCodec(ZeroCopyMessageWrapper.class, new ZeroCopyMessageCodec<>());
+        eventBus.codecSelector(o -> {
+            var zeroCopyMessage = o.getClass().getAnnotation(ZeroCopyMessage.class);
+            return zeroCopyMessage != null ? ZERO_COPY_CODEC_NAME : null;
+        });
     }
 
     /**
@@ -57,6 +51,9 @@ public final class ZeroCopyMessageCodec implements MessageCodec<Object, Object> 
      */
     @Override
     public Object transform(Object o) {
+        if (o instanceof ZeroCopyMessageWrapper<?> z) {
+            return z.message();
+        }
         return o;
     }
 
