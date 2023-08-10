bac74f4a43f0288b89a37721a25942523af397bf
scx567888 <scx567888@outlook.com>
2023-08-10 09:38:35 +0800

    dev

--- scx-data/src/main/java/cool/scx/data/Query.java
+++ scx-data/src/main/java/cool/scx/data/Query.java
@@ -2,6 +2,9 @@ package cool.scx.data;
 
 import cool.scx.data.query.*;
 
+import static cool.scx.data.query.OrderByType.ASC;
+import static cool.scx.data.query.OrderByType.DESC;
+
 public interface Query {
 
     static QueryImpl query() {
@@ -46,6 +49,32 @@ public interface Query {
 
     static WhereBodySet orSet() {
         return new WhereBodySet(LogicType.OR);
+    }
+
+    /**
+     * 正序 : 也就是从小到大 (1,2,3,4,5,6)
+     *
+     * @param name    a
+     * @param options 配置
+     * @return a
+     */
+    static OrderByBody asc(String name, OrderByOption... options) {
+        return new OrderByBody(name, ASC, options);
+    }
+
+    /**
+     * 倒序 : 也就是从大到小 (6,5,4,3,2,1)
+     *
+     * @param name    a
+     * @param options 配置
+     * @return a
+     */
+    static OrderByBody desc(String name, OrderByOption... options) {
+        return new OrderByBody(name, DESC, options);
+    }
+
+    static OrderByBodySet orderBySet() {
+        return new OrderByBodySet();
     }
 
     default Where getWhere() {
