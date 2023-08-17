5ea9e61c188b178209a1df258e84e0b3bbc63c79
scx567888 <scx567888@outlook.com>
2023-08-17 15:30:30 +0800

    dev

---
+++ scx-common/src/main/java/cool/scx/util/cycle/CycleReverseIterable.java
@@ -0,0 +1,18 @@
+package cool.scx.util.cycle;
+
+import java.util.Iterator;
+
+public final class CycleReverseIterable<T> implements Iterable<T> {
+
+    private final CycleIterable<T> cycleIterable;
+
+    public CycleReverseIterable(CycleIterable<T> cycleIterable) {
+        this.cycleIterable = cycleIterable;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new CycleReverseIterator<>(cycleIterable.last());
+    }
+
+} 
