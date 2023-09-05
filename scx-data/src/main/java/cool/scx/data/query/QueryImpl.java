9aa7d7c00ce50d3baabde18de88da946660da9a0
scx567888 <scx567888@outlook.com>
2023-09-05 15:32:36 +0800

    v2.4.6

--- scx-data/src/main/java/cool/scx/data/query/QueryImpl.java
+++ scx-data/src/main/java/cool/scx/data/query/QueryImpl.java
@@ -43,16 +43,34 @@ public class QueryImpl implements Query {
         this.limitInfo = new LimitInfo();
     }
 
-    /**
-     * a
-     *
-     * @param oldQuery a
-     */
     public QueryImpl(Query oldQuery) {
         this.where = new Where(oldQuery.getWhere());
         this.groupBy = new GroupBy(oldQuery.getGroupBy());
         this.orderBy = new OrderBy(oldQuery.getOrderBy());
         this.limitInfo = new LimitInfo(oldQuery.getLimitInfo());
+    }
+
+    QueryImpl(Where oldWhere) {
+        this(oldWhere, new GroupBy(), new OrderBy(), new LimitInfo());
+    }
+
+    QueryImpl(GroupBy oldGroupBy) {
+        this(new Where(), oldGroupBy, new OrderBy(), new LimitInfo());
+    }
+
+    QueryImpl(OrderBy oldOrderBy) {
+        this(new Where(), new GroupBy(), oldOrderBy, new LimitInfo());
+    }
+
+    QueryImpl(LimitInfo oldLimitInfo) {
+        this(new Where(), new GroupBy(), new OrderBy(), oldLimitInfo);
+    }
+
+    private QueryImpl(Where oldWhere, GroupBy oldGroupBy, OrderBy oldOrderBy, LimitInfo oldLimitInfo) {
+        this.where = oldWhere;
+        this.groupBy = oldGroupBy;
+        this.orderBy = oldOrderBy;
+        this.limitInfo = oldLimitInfo;
     }
 
     @Override
