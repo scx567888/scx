146d1a7adf261603f080c1be78173dd665d6a196
scx567888 <scx567888@outlook.com>
2023-09-11 00:48:41 +0800

    dev

--- scx-core/src/main/java/cool/scx/core/scheduler/ScxScheduler.java
+++ scx-core/src/main/java/cool/scx/core/scheduler/ScxScheduler.java
@@ -16,7 +16,7 @@ import java.util.function.Consumer;
  * Scx 核心调度器 只是 同时代理了 {@link ScheduledExecutorService} 和 {@link org.springframework.scheduling.TaskScheduler}
  *
  * <br>
- * 同时实现了一些支持自我取消的 任务调度
+ * 同时实现了一些支持自我取消和固定次数的 任务调度
  *
  * @author scx567888
  * @version 2.4.8
