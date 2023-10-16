d91c8a28b9d7faf320c4bc40de5721aa9036a896
scx567888 <scx567888@outlook.com>
2023-10-16 10:15:28 +0800

    dev

--- scx-common/src/main/java/cool/scx/http_client/ScxHttpClientResponseBody.java
+++ scx-common/src/main/java/cool/scx/http_client/ScxHttpClientResponseBody.java
@@ -4,9 +4,14 @@ import cool.scx.util.io_stream_source.RawInputStreamSource;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
+import java.nio.charset.Charset;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class ScxHttpClientResponseBody extends RawInputStreamSource {
+
+    private String string;
+    private byte[] bytes;
 
     public ScxHttpClientResponseBody(InputStream inputStream) {
         super(inputStream);
@@ -15,10 +20,26 @@ public class ScxHttpClientResponseBody extends RawInputStreamSource {
     @Override
     public String toString() {
         try {
-            return toString(StandardCharsets.UTF_8);
+            return this.toString(UTF_8);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @Override
+    public String toString(Charset charset) throws IOException {
+        if (this.string == null) {
+            this.string = super.toString(UTF_8);
+        }
+        return this.string;
+    }
+
+    @Override
+    public byte[] toBytes() throws IOException {
+        if (this.bytes == null) {
+            this.bytes = super.toBytes();
+        }
+        return this.bytes;
     }
 
 }
