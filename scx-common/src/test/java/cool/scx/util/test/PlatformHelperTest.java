09d8b15ba6f83a98f70d53953c334cafc26cc8f9
scx567888 <scx567888@outlook.com>
2024-02-20 10:40:53 +0800

    dev

---
+++ scx-common/src/test/java/cool/scx/util/test/PlatformHelperTest.java
@@ -0,0 +1,17 @@
+package cool.scx.util.test;
+
+import cool.scx.util.PlatformHelper;
+import org.testng.annotations.Test;
+
+public class PlatformHelperTest {
+    
+    public static void main(String[] args) {
+        test1();
+    }
+
+    @Test
+    public static void test1() {
+        System.out.println(PlatformHelper.getOSInfo());
+    }
+    
+}
