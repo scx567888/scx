70677e38f2d7d63ab5327dc33abd51608d2f44d6
scx567888 <scx567888@outlook.com>
2024-03-08 21:59:16 +0800

    dev

--- scx-data-jdbc/src/main/java/cool/scx/data/jdbc/annotation/DataType.java
+++ scx-data-jdbc/src/main/java/cool/scx/data/jdbc/annotation/DataType.java
@@ -5,6 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * TODO 同时支持枚举和字面量 枚举用来实现多数据源匹配
+ */
 @Target({})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface DataType {
