7510d314be672e318c54f3f14e34729095be4a75
scx567888 <scx567888@outlook.com>
2024-08-19 17:38:58 +0800

    dev

--- scx-core/src/main/java/cool/scx/core/annotation/ScxComponent.java
+++ scx-core/src/main/java/cool/scx/core/annotation/ScxComponent.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * 标识此类是一个需要注入到容器中的类
+ * 标识此类是一个需要注入到容器中的类 和 {@link cool.scx.core.annotation.ScxService} 的区别仅在于注解的语义不同
  *
  * @author scx567888
  * @version 1.11.5
