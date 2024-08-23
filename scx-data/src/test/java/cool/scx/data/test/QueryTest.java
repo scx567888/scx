deca784c5ea1b6e34f896dda2daebb22a74e281d
scx567888 <scx567888@outlook.com>
2024-08-23 17:18:45 +0800

    dev

--- scx-data/src/test/java/cool/scx/data/test/QueryTest.java
+++ scx-data/src/test/java/cool/scx/data/test/QueryTest.java
@@ -1,7 +1,6 @@
 package cool.scx.data.test;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import cool.scx.data.query.serializer.QuerySerializer;
 import org.testng.annotations.Test;
 
 import java.util.Arrays;
@@ -14,10 +13,11 @@ public class QueryTest {
 
     public static void main(String[] args) throws JsonProcessingException {
         test1();
+        test2();
     }
 
     @Test
-    public static void test1() throws JsonProcessingException {
+    public static void test1() {
         var and = and(eq("name", "abc"), eq("age", "10")).limit(1);
         var andSet = andSet().eq("abc", "123").limit(10).gt("bvd", "456");
         var parser = new TestWhereParser();
@@ -26,10 +26,23 @@ public class QueryTest {
         System.out.println(parse1.whereClause() + " " + Arrays.toString(parse1.params()) + " LIMIT " + and.getLimit());
         System.out.println(parse2.whereClause() + " " + Arrays.toString(parse2.params()) + " LIMIT " + andSet.getLimit());
 
-        String json = QUERY_SERIALIZER.toJson(andSet);
+    }
+
+    public static void test2() throws JsonProcessingException {
+        var q1 = andSet().eq("name", "小明").between("age", 1, 10).orderBy("desc name", desc("age")).groupBy("class", groupBy("abc")).limit(10).offset(12);
+        var q2 = and(
+                or(
+                        and(
+                                eq("name", "小明")
+                        )
+                )
+        ).orderBy("desc name", desc("age")).groupBy("class", groupBy("abc")).limit(10).offset(12);
+        String json = QUERY_SERIALIZER.toJson(q1);
         var andNew = QUERY_DESERIALIZER.fromJson(json);
         System.out.println(json);
-
+        String json1 = QUERY_SERIALIZER.toJson(q2);
+        var andNew2 = QUERY_DESERIALIZER.fromJson(json1);
+        System.out.println(json1);
     }
 
 }
