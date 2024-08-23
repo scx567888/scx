8bb01f733bc71f9cfc3cc8f19dc5c6acef1547f0
scx567888 <scx567888@outlook.com>
2024-08-23 12:15:13 +0800

    dev

---
+++ scx-data/src/main/java/cool/scx/data/query/serializer/LimitInfoSerializer.java
@@ -0,0 +1,25 @@
+package cool.scx.data.query.serializer;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import cool.scx.data.query.LimitInfo;
+
+import java.util.LinkedHashMap;
+
+public class LimitInfoSerializer {
+
+    public LinkedHashMap<String, Object> serialize(LimitInfo limitInfo) {
+        var m = new LinkedHashMap<String, Object>();
+        m.put("@type", "LimitInfo");
+        m.put("offset", limitInfo.getOffset());
+        m.put("limit", limitInfo.getLimit());
+        return m;
+    }
+
+    public LimitInfo deserialize(JsonNode objectNode) {
+        var limitInfo = new LimitInfo();
+        limitInfo.offset(objectNode.get("offset").asLong());
+        limitInfo.limit(objectNode.get("limit").asLong());
+        return limitInfo;
+    }
+
+}
