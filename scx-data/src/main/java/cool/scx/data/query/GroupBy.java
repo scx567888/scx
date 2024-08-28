30847364f3d97be3311f4866ef49bc1f69254dbb
scx567888 <scx567888@outlook.com>
2024-08-28 12:50:14 +0800

    dev

--- scx-data/src/main/java/cool/scx/data/query/GroupBy.java
+++ scx-data/src/main/java/cool/scx/data/query/GroupBy.java
@@ -2,63 +2,43 @@ package cool.scx.data.query;
 
 import cool.scx.data.Query;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static java.util.Collections.addAll;
+import static cool.scx.common.util.StringUtils.isBlank;
+import static cool.scx.data.query.GroupByOption.Info;
 
 /**
- * 分组
+ * GroupBy 封装体
  *
  * @author scx567888
  * @version 0.0.1
  */
 public final class GroupBy extends QueryLike<GroupBy> {
 
-    /**
-     * 分组字段列表
-     */
-    private final List<Object> clauses;
+    private final String name;
+    private final Info info;
 
-    /**
-     * 创建一个 OrderBy 对象
-     */
-    public GroupBy() {
-        this.clauses = new ArrayList<>();
+    public GroupBy(String name, Info info) {
+        if (isBlank(name)) {
+            throw new IllegalArgumentException("GroupBy 参数错误 : 名称 不能为空 !!!");
+        }
+        this.name = name.trim();
+        this.info = info;
     }
 
-    /**
-     * 根据旧的 GroupBy 创建一个 GroupBy 对象
-     *
-     * @param oldGroupBy 旧的 GroupBy
-     */
-    public GroupBy(GroupBy oldGroupBy) {
-        this.clauses = new ArrayList<>(oldGroupBy.clauses);
+    public GroupBy(String name, GroupByOption... options) {
+        this(name, new Info(options));
     }
 
-    public GroupBy set(Object... groupByClauses) {
-        this.clear();
-        this.add(groupByClauses);
-        return this;
+    public String name() {
+        return name;
     }
 
-    public GroupBy add(Object... groupByClauses) {
-        addAll(this.clauses, groupByClauses);
-        return this;
-    }
-
-    public Object[] clauses() {
-        return this.clauses.toArray();
-    }
-
-    public GroupBy clear() {
-        this.clauses.clear();
-        return this;
+    public Info info() {
+        return info;
     }
 
     @Override
     public Query toQuery() {
-        return new QueryImpl(this);
+        return new QueryImpl().groupBy(this);
     }
 
 }
