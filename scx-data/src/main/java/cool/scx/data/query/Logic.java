6ccb4afbb72c6d27f90aa658add9ab11cc2e5d5e
scx567888 <scx567888@outlook.com>
2024-08-30 12:47:50 +0800

    dev

--- scx-data/src/main/java/cool/scx/data/query/Logic.java
+++ scx-data/src/main/java/cool/scx/data/query/Logic.java
@@ -2,85 +2,104 @@ package cool.scx.data.query;
 
 import cool.scx.data.q.Where;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static cool.scx.data.query.WhereType.*;
+import static java.util.Collections.addAll;
 
-public interface Logic {
+public class Logic {
 
-    LogicType logicType();
+    private final LogicType logicType;
+    private final List<Object> clauses;
 
-    Object[] clauses();
+    public Logic(LogicType logicType) {
+        this.logicType = logicType;
+        this.clauses = new ArrayList<>();
+    }
 
-    Logic add(Object... logicCauses);
+    public LogicType logicType() {
+        return logicType;
+    }
 
-    default Logic addWhere(String name, WhereType whereType, Object value1, Object value2, WhereOption... options) {
+    public Object[] clauses() {
+        return clauses.toArray();
+    }
+
+    public Logic add(Object... logicCauses) {
+        addAll(clauses, logicCauses);
+        return this;
+    }
+
+    public final Logic addWhere(String name, WhereType whereType, Object value1, Object value2, WhereOption... options) {
         return add(new Where(name, whereType, value1, value2, options));
     }
 
-    default Logic eq(String fieldName, Object value, WhereOption... options) {
+    public final Logic eq(String fieldName, Object value, WhereOption... options) {
         return addWhere(fieldName, EQUAL, value, null, options);
     }
 
-    default Logic ne(String fieldName, Object value, WhereOption... options) {
+    public final Logic ne(String fieldName, Object value, WhereOption... options) {
         return addWhere(fieldName, NOT_EQUAL, value, null, options);
     }
 
-    default Logic lt(String fieldName, Object value, WhereOption... options) {
+    public final Logic lt(String fieldName, Object value, WhereOption... options) {
         return addWhere(fieldName, LESS_THAN, value, null, options);
     }
 
-    default Logic le(String fieldName, Object value, WhereOption... options) {
+    public final Logic le(String fieldName, Object value, WhereOption... options) {
         return addWhere(fieldName, LESS_THAN_OR_EQUAL, value, null, options);
     }
 
-    default Logic gt(String fieldName, Object value, WhereOption... options) {
+    public final Logic gt(String fieldName, Object value, WhereOption... options) {
         return addWhere(fieldName, GREATER_THAN, value, null, options);
     }
 
-    default Logic ge(String fieldName, Object value, WhereOption... options) {
+    public final Logic ge(String fieldName, Object value, WhereOption... options) {
         return addWhere(fieldName, GREATER_THAN_OR_EQUAL, value, null, options);
     }
 
-    default Logic isNull(String fieldName, WhereOption... options) {
+    public final Logic isNull(String fieldName, WhereOption... options) {
         return addWhere(fieldName, IS_NULL, null, null, options);
     }
 
-    default Logic isNotNull(String fieldName, WhereOption... options) {
+    public final Logic isNotNull(String fieldName, WhereOption... options) {
         return addWhere(fieldName, IS_NOT_NULL, null, null, options);
     }
 
-    default Logic like(String fieldName, Object value, WhereOption... options) {
+    public final Logic like(String fieldName, Object value, WhereOption... options) {
         return addWhere(fieldName, LIKE, value, null, options);
     }
 
-    default Logic notLike(String fieldName, Object value, WhereOption... options) {
+    public final Logic notLike(String fieldName, Object value, WhereOption... options) {
         return addWhere(fieldName, NOT_LIKE, value, null, options);
     }
 
-    default Logic likeRegex(String fieldName, String value, WhereOption... options) {
+    public final Logic likeRegex(String fieldName, String value, WhereOption... options) {
         return addWhere(fieldName, LIKE_REGEX, value, null, options);
     }
 
-    default Logic notLikeRegex(String fieldName, String value, WhereOption... options) {
+    public final Logic notLikeRegex(String fieldName, String value, WhereOption... options) {
         return addWhere(fieldName, NOT_LIKE_REGEX, value, null, options);
     }
 
-    default Logic in(String fieldName, Object value, WhereOption... options) {
+    public final Logic in(String fieldName, Object value, WhereOption... options) {
         return addWhere(fieldName, IN, value, null, options);
     }
 
-    default Logic notIn(String fieldName, Object value, WhereOption... options) {
+    public final Logic notIn(String fieldName, Object value, WhereOption... options) {
         return addWhere(fieldName, NOT_IN, value, null, options);
     }
 
-    default Logic between(String fieldName, Object value1, Object value2, WhereOption... options) {
+    public final Logic between(String fieldName, Object value1, Object value2, WhereOption... options) {
         return addWhere(fieldName, BETWEEN, value1, value2, options);
     }
 
-    default Logic notBetween(String fieldName, Object value1, Object value2, WhereOption... options) {
+    public final Logic notBetween(String fieldName, Object value1, Object value2, WhereOption... options) {
         return addWhere(fieldName, NOT_BETWEEN, value1, value2, options);
     }
 
-    default Logic jsonContains(String fieldName, Object value, WhereOption... options) {
+    public final Logic jsonContains(String fieldName, Object value, WhereOption... options) {
         return addWhere(fieldName, JSON_CONTAINS, value, null, options);
     }
 
--- scx-data/src/main/java/cool/scx/data/query/LogicImpl.java
+++ scx-data/src/main/java/cool/scx/data/query/LogicImpl.java
@@ -7,28 +7,6 @@ import static java.util.Collections.addAll;
 
 public class LogicImpl implements Logic {
 
-    private final LogicType logicType;
-    private final List<Object> clauses;
-
-    public LogicImpl(LogicType logicType) {
-        this.logicType = logicType;
-        this.clauses = new ArrayList<>();
-    }
-
-    @Override
-    public LogicType logicType() {
-        return logicType;
-    }
-
-    @Override
-    public Object[] clauses() {
-        return clauses.toArray();
-    }
-
-    @Override
-    public Logic add(Object... logicCauses) {
-        addAll(clauses, logicCauses);
-        return this;
-    }
+    
 
 }
