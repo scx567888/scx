c2d380bdb2a57cbe02854383a1439338ee58b9ce
scx567888 <scx567888@outlook.com>
2024-08-30 11:46:21 +0800

    dev

---
+++ scx-data/src/main/java/cool/scx/data/field_filter/deserializer/FieldFilterDeserializer.java
@@ -0,0 +1,38 @@
+package cool.scx.data.field_filter.deserializer;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import cool.scx.data.FieldFilter;
+import cool.scx.data.field_filter.ExcludedFieldFilter;
+import cool.scx.data.field_filter.FilterMode;
+import cool.scx.data.field_filter.IncludedFieldFilter;
+
+import static cool.scx.common.util.ObjectUtils.convertValue;
+
+public class FieldFilterDeserializer {
+
+    public static final FieldFilterDeserializer FIELD_FILTER_DESERIALIZER = new FieldFilterDeserializer();
+
+    public Object deserialize(JsonNode v) {
+        if (v.isObject()) {
+            var type = v.get("@type").asText();
+            if (type.equals("FieldFilter")) {
+                return deserializeFieldFilter(v);
+            }
+        }
+        return v;
+    }
+
+    public FieldFilter deserializeFieldFilter(JsonNode objectNode) {
+        if (objectNode == null) {
+            return new ExcludedFieldFilter();
+        }
+        var filterMode = convertValue(objectNode.get("filterMode"), FilterMode.class);
+        var fieldNames = convertValue(objectNode.get("fieldNames"), String[].class);
+        var ignoreNullValue = objectNode.get("ignoreNullValue").asBoolean();
+        return switch (filterMode) {
+            case INCLUDED -> new IncludedFieldFilter().addIncluded(fieldNames).ignoreNullValue(ignoreNullValue);
+            case EXCLUDED -> new ExcludedFieldFilter().addExcluded(fieldNames).ignoreNullValue(ignoreNullValue);
+        };
+    }
+
+}
