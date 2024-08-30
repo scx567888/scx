b31a60b121c3bdee2a1fb38b81d46231d5f33779
scx567888 <scx567888@outlook.com>
2024-08-30 11:48:01 +0800

    dev

---
+++ scx-data/src/main/java/cool/scx/data/field_filter/serializer/FieldFilterSerializer.java
@@ -0,0 +1,27 @@
+package cool.scx.data.field_filter.serializer;
+
+import cool.scx.data.FieldFilter;
+
+import java.util.LinkedHashMap;
+
+public class FieldFilterSerializer {
+
+    public static final FieldFilterSerializer FIELD_FILTER_SERIALIZER = new FieldFilterSerializer();
+
+    public Object serialize(Object obj) {
+        return switch (obj) {
+            case FieldFilter s -> serializeFieldFilter(s);
+            default -> obj;
+        };
+    }
+
+    public LinkedHashMap<String, Object> serializeFieldFilter(FieldFilter fieldFilter) {
+        var m = new LinkedHashMap<String, Object>();
+        m.put("@type", "FieldFilter");
+        m.put("filterMode", fieldFilter.getFilterMode());
+        m.put("fieldNames", fieldFilter.getFieldNames());
+        m.put("ignoreNullValue", fieldFilter.getIgnoreNullValue());
+        return m;
+    }
+
+}
