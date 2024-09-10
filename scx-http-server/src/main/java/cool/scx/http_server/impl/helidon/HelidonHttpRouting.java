112e7f055edf7ac2a5ece225fb5f48403b9dd110
scx567888 <scx567888@outlook.com>
2024-09-10 15:31:16 +0800

    dev

--- scx-http-server/src/main/java/cool/scx/http_server/impl/helidon/HelidonHttpRouting.java
+++ scx-http-server/src/main/java/cool/scx/http_server/impl/helidon/HelidonHttpRouting.java
@@ -25,47 +25,47 @@ class HelidonHttpRouting implements HttpRouting, HttpRouting.Builder {
 
     @Override
     public Builder register(HttpService... service) {
-        return null;
+        return this;
     }
 
     @Override
     public Builder register(String path, HttpService... service) {
-        return null;
+        return this;
     }
 
     @Override
     public Builder route(HttpRoute route) {
-        return null;
+        return this;
     }
 
     @Override
     public Builder addFilter(Filter filter) {
-        return null;
+        return this;
     }
 
     @Override
     public Builder addFeature(Supplier<? extends HttpFeature> feature) {
-        return null;
+        return this;
     }
 
     @Override
     public <T extends Throwable> Builder error(Class<T> exceptionClass, ErrorHandler<? super T> handler) {
-        return null;
+        return this;
     }
 
     @Override
     public Builder maxReRouteCount(int maxReRouteCount) {
-        return null;
+        return this;
     }
 
     @Override
     public Builder security(HttpSecurity security) {
-        return null;
+        return this;
     }
 
     @Override
     public Builder copy() {
-        return null;
+        return this;
     }
 
     @Override
