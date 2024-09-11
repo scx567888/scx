82864cef2aa8cc7bbe0195022494f21c229d269b
scx567888 <scx567888@outlook.com>
2024-09-11 09:31:45 +0800

    dev

--- scx-http-server/src/main/java/cool/scx/http_server/ScxHttpServerOptions.java
+++ scx-http-server/src/main/java/cool/scx/http_server/ScxHttpServerOptions.java
@@ -1,10 +1,24 @@
 package cool.scx.http_server;
 
-public class ScxHttpServerOptions extends ScxTCPServerOptions {
+public class ScxHttpServerOptions {
+
+    private int port;
+
+    public ScxHttpServerOptions() {
+        this.port = 0;
+    }
+
+    public int getPort() {
+        return port;
+    }
 
     public ScxHttpServerOptions setPort(int port) {
-        super.setPort(port);
-        return this;
+        if (port > 65535) {
+            throw new IllegalArgumentException("port must be <= 65535");
+        } else {
+            this.port = port;
+            return this;
+        }
     }
 
 }
