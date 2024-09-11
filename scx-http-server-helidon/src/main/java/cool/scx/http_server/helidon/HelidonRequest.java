c891875780b776ed22877fb8b7391c30354c49f8
scx567888 <scx567888@outlook.com>
2024-09-11 10:15:02 +0800

    dev

--- scx-http-server-helidon/src/main/java/cool/scx/http_server/helidon/HelidonRequest.java
+++ scx-http-server-helidon/src/main/java/cool/scx/http_server/helidon/HelidonRequest.java
@@ -1,19 +1,14 @@
-package cool.scx.http_server.impl.helidon;
+package cool.scx.http_server.helidon;
 
 import cool.scx.http_server.*;
 import io.helidon.webserver.ConnectionContext;
 import io.helidon.webserver.http.RoutingRequest;
 import io.helidon.webserver.http.RoutingResponse;
 
-import java.io.IOException;
-import java.io.InputStream;
-
-import static cool.scx.http_server.Helper.*;
+import static cool.scx.http_server.helidon.HelidonHelper.*;
 
 public class HelidonRequest implements ScxHttpRequest {
 
-    private final ConnectionContext ctx;
-    private final RoutingRequest request;
     private final ScxHttpMethod method;
     private final ScxHttpPath path;
     private final ScxHttpVersion version;
@@ -22,11 +17,9 @@ public class HelidonRequest implements ScxHttpRequest {
     private final ScxHttpResponse response;
 
     public HelidonRequest(ConnectionContext ctx, RoutingRequest request, RoutingResponse response) {
-        this.ctx = ctx;
-        this.request = request;
         this.method = createScxHttpMethod(request.prologue().method());
-        this.path = createScxHttpPath(request.prologue());
-        this.version = createScxHttpVersion(request.prologue().rawProtocol());
+        this.path = createScxHttpPath(request.prologue().uriPath(), request.query());
+        this.version = createScxHttpVersion(request.prologue().protocol());
         this.headers = createScxHttpHeaders(request.headers());
         this.body = new HelidonHttpBody(request.content());
         this.response = new HelidonResponse(response);
