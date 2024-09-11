f04889270948836b193392afd1b4d49db2d960f3
scx567888 <scx567888@outlook.com>
2024-09-11 14:30:54 +0800

    dev

--- scx-http/src/main/java/cool/scx/http/ScxClientWebSocket.java
+++ scx-http/src/main/java/cool/scx/http/ScxClientWebSocket.java
@@ -2,5 +2,5 @@ package cool.scx.http;
 
 //todo 接口方法待定义
 public interface ScxClientWebSocket {
-    
+
 }
--- scx-http/src/main/java/cool/scx/http/ScxHttpFormData.java
+++ scx-http/src/main/java/cool/scx/http/ScxHttpFormData.java
@@ -1,6 +1,6 @@
-package cool.scx.http_server;
+package cool.scx.http;
 
 //todo 方法待定义
 public interface ScxHttpFormData {
-    
+
 }
