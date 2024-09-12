0f4f34b808d4e8adb474cf79749216a482135e1f
scx567888 <scx567888@outlook.com>
2024-09-12 17:28:17 +0800

    dev

--- scx-http-helidon/src/test/java/cool/scx/http/helidon/test/Test.java
+++ scx-http-helidon/src/test/java/cool/scx/http/helidon/test/Test.java
@@ -14,6 +14,28 @@ public class Test {
 
     public static void test1() {
         var l = System.nanoTime();
+        var server = new HelidonHttpServer(new ScxHttpServerOptions().setPort(8080));
+
+        server.requestHandler(c -> {
+            var response = c.response();
+            response.send("响应数据");
+        }).webSocketHandler(c -> {
+
+            c.onTextMessage(s -> {
+                System.out.println("Text Message: " + s);
+            });
+
+            c.send("This is Server", false);
+
+        });
+
+        server.start();
+
+        System.out.println("HelidonHttpServer 启动完成 !!! 耗时 : " + (System.nanoTime() - l) / 1000_000);
+    }
+
+    public static void test2() {
+        var l = System.nanoTime();
         Vertx vertx = Vertx.vertx();
 
         var server = vertx.createHttpServer(new HttpServerOptions().setPort(8081));
@@ -34,28 +56,6 @@ public class Test {
         server.listen().onSuccess(c -> {
             System.out.println("VertxHttpServer 启动完成 !!! 耗时 : " + (System.nanoTime() - l) / 1000_000);
         });
-    }
-
-    public static void test2() {
-        var l = System.nanoTime();
-        var server = new HelidonHttpServer(new ScxHttpServerOptions().setPort(8080));
-
-        server.requestHandler(c -> {
-            var response = c.response();
-            response.send("响应数据");
-        }).webSocketHandler(c -> {
-
-            c.onTextMessage(s -> {
-                System.out.println("Text Message: " + s);
-            });
-
-            c.send("This is Server", false);
-
-        });
-
-        server.start();
-
-        System.out.println("HelidonHttpServer 启动完成 !!! 耗时 : " + (System.nanoTime() - l) / 1000_000);
     }
 
 }
