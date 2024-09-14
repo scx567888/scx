62f8029a33116a62a12d64015573e8c796ade95a
scx567888 <scx567888@outlook.com>
2024-09-14 21:28:51 +0800

    dev

--- scx-http/src/main/java/cool/scx/http/ScxHttpMethod.java
+++ scx-http/src/main/java/cool/scx/http/ScxHttpMethod.java
@@ -1,16 +1,15 @@
 package cool.scx.http;
 
 /**
- * HTTP Method 本质上是区分大小写的 但这里我们全部按照大写处理
+ * HTTP Method (注意 需要 区分大小写)
  */
 public sealed interface ScxHttpMethod permits HttpMethod, ScxHttpMethodImpl {
 
     static ScxHttpMethod of(String httpMethod) {
         try {
-            // 优先使用 HttpMethod
             return HttpMethod.of(httpMethod);
         } catch (IllegalArgumentException e) {
-            return new ScxHttpMethodImpl(httpMethod.toUpperCase());
+            return new ScxHttpMethodImpl(httpMethod);
         }
     }
 
