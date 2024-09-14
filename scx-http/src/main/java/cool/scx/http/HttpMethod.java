bff78c4ec53d61ea02345c226ed53d4e46bb8e6e
scx567888 <scx567888@outlook.com>
2024-09-14 21:25:40 +0800

    dev

--- scx-http/src/main/java/cool/scx/http/HttpMethod.java
+++ scx-http/src/main/java/cool/scx/http/HttpMethod.java
@@ -12,56 +12,72 @@ public enum HttpMethod implements ScxHttpMethod {
     /**
      * CONNECT 方法建立一个到由目标资源标识的服务器的隧道。
      */
-    CONNECT,
+    CONNECT("CONNECT"),
 
     /**
      * DELETE 方法删除指定的资源。
      */
-    DELETE,
+    DELETE("DELETE"),
 
     /**
      * GET 方法请求一个指定资源的表示形式，使用 GET 的请求应该只被用于获取数据。
      */
-    GET,
+    GET("GET"),
 
     /**
      * HEAD 方法请求一个与 GET 请求的响应相同的响应，但没有响应体。
      */
-    HEAD,
+    HEAD("HEAD"),
 
     /**
      * OPTIONS 方法用于描述目标资源的通信选项。
      */
-    OPTIONS,
+    OPTIONS("OPTIONS"),
 
     /**
      * PATCH 方法用于对资源应用部分修改。
      */
-    PATCH,
+    PATCH("PATCH"),
 
     /**
      * POST 方法用于将实体提交到指定的资源，通常导致在服务器上的状态变化或副作用。
      */
-    POST,
+    POST("POST"),
 
     /**
      * PUT 方法用有效载荷请求替换目标资源的所有当前表示。
      */
-    PUT,
+    PUT("PUT"),
 
     /**
      * TRACE 方法沿着到目标资源的路径执行一个消息环回测试。
      */
-    TRACE;
+    TRACE("TRACE");
+
+    private final String value;
+
+    HttpMethod(String value) {
+        this.value = value;
+    }
 
     public static HttpMethod of(String v) {
-        var upperCase = v.toUpperCase();
-        return valueOf(upperCase);
+        return switch (v) {
+            case "CONNECT" -> CONNECT;
+            case "DELETE" -> DELETE;
+            case "GET" -> GET;
+            case "HEAD" -> HEAD;
+            case "OPTIONS" -> OPTIONS;
+            case "PATCH" -> PATCH;
+            case "POST" -> POST;
+            case "PUT" -> PUT;
+            case "TRACE" -> TRACE;
+            default -> throw new IllegalArgumentException("Unknown http method: " + v);
+        };
     }
 
     @Override
     public String value() {
-        return this.name();
+        return this.value;
     }
 
 }
