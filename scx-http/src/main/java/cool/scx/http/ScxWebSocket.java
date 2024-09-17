49f8b15756002f16220b992d6556eedc03caa4c6
scx567888 <scx567888@outlook.com>
2024-09-17 08:11:53 +0800

    dev

---
+++ scx-http/src/main/java/cool/scx/http/ScxWebSocket.java
@@ -0,0 +1,29 @@
+package cool.scx.http;
+
+import java.util.function.Consumer;
+
+public interface ScxWebSocket {
+
+    ScxServerWebSocket onTextMessage(Consumer<String> textMessageHandler);
+
+    ScxServerWebSocket onBinaryMessage(Consumer<byte[]> binaryMessageHandler);
+
+    ScxServerWebSocket onPing(Consumer<byte[]> pingHandler);
+
+    ScxServerWebSocket onPong(Consumer<byte[]> pongHandler);
+
+    ScxServerWebSocket onClose(Consumer<Integer> closeHandler);
+
+    ScxServerWebSocket onError(Consumer<Throwable> errorHandler);
+
+    ScxServerWebSocket send(String textMessage, boolean var2);
+
+    ScxServerWebSocket send(byte[] binaryMessage, boolean var2);
+
+    ScxServerWebSocket ping(byte[] data);
+
+    ScxServerWebSocket pong(byte[] data);
+
+    ScxServerWebSocket close(int var1, String var2);
+    
+}
