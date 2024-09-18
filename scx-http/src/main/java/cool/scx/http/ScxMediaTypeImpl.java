3ea96c6f6694b031fb83f45855f879766c6c3ca7
scx567888 <scx567888@outlook.com>
2024-09-18 10:58:11 +0800

    dev

---
+++ scx-http/src/main/java/cool/scx/http/ScxMediaTypeImpl.java
@@ -0,0 +1,30 @@
+package cool.scx.http;
+
+public class ScxMediaTypeImpl implements ScxMediaType {
+
+    private final String type;
+    private final String subtype;
+    private final String value;
+
+    public ScxMediaTypeImpl(String type, String subtype) {
+        this.type = type;
+        this.subtype = subtype;
+        this.value = type + "/" + subtype;
+    }
+
+    @Override
+    public String type() {
+        return type;
+    }
+
+    @Override
+    public String subtype() {
+        return subtype;
+    }
+
+    @Override
+    public String value() {
+        return value;
+    }
+    
+}
