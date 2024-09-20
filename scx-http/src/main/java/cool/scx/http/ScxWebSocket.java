0941dfde2526da413bd0100342e894d636a2eddd
scx567888 <scx567888@outlook.com>
2024-09-20 09:12:13 +0800

    dev

--- scx-http/src/main/java/cool/scx/http/ScxWebSocket.java
+++ scx-http/src/main/java/cool/scx/http/ScxWebSocket.java
@@ -3,6 +3,8 @@ package cool.scx.http;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
+import static cool.scx.http.WebSocketCloseCode.NORMAL_CLOSE;
+
 /**
  * ScxWebSocket
  */
@@ -20,28 +22,34 @@ public interface ScxWebSocket {
 
     ScxWebSocket onError(Consumer<Throwable> errorHandler);
 
-    ScxWebSocket send(String textMessage, boolean var2);
+    ScxWebSocket send(String textMessage, boolean last);
 
-    ScxWebSocket send(byte[] binaryMessage, boolean var2);
+    ScxWebSocket send(byte[] binaryMessage, boolean last);
 
     ScxWebSocket ping(byte[] data);
 
     ScxWebSocket pong(byte[] data);
 
-    ScxWebSocket close(int var1, String var2);
+    ScxWebSocket close(int code, String reason);
+
+    ScxWebSocket terminate();
 
     boolean isClosed();
 
     default ScxWebSocket send(String textMessage) {
-        return send(textMessage, false);
+        return send(textMessage, true);
     }
 
     default ScxWebSocket send(byte[] binaryMessage) {
-        return send(binaryMessage, false);
+        return send(binaryMessage, true);
+    }
+
+    default ScxWebSocket close(WebSocketCloseCode closeCode) {
+        return close(closeCode.code(), closeCode.reason());
     }
 
     default ScxWebSocket close() {
-        return close(666, "NORMAL");
+        return close(NORMAL_CLOSE);
     }
 
 }
