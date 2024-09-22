038223b9c5c1ded502c41bb553d0732f18f0ae71
scx567888 <scx567888@outlook.com>
2024-09-22 21:46:34 +0800

    dev

--- scx-common/src/main/java/cool/scx/common/ffm/Ref.java
+++ scx-common/src/main/java/cool/scx/common/ffm/Ref.java
@@ -1,7 +1,6 @@
-package cool.scx.common.ffm.type;
+package cool.scx.common.ffm;
 
 import java.lang.foreign.Arena;
-import java.lang.foreign.MemorySegment;
 
 /**
  * 引用类型需要继承此接口
@@ -14,13 +13,15 @@ public interface Ref {
      * @param arena 作用域
      * @return MemorySegment
      */
-    MemorySegment writeToMemorySegment(Arena arena);
+    Object toMemorySegment(Arena arena);
 
     /**
      * 从内存段加载
      * 因为脱离 Arena 作用域之后 MemorySegment 将无法读取
      * 所以此处预先 将 数据 读取到 Ref 中
      */
-    void readFromMemorySegment();
+    default void readFromMemorySegment() {
+
+    }
 
 }
