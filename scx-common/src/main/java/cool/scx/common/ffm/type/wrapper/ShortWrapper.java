2916c8b35f8c33e1f5be1f7a741ad1719a038c35
scx567888 <scx567888@outlook.com>
2024-09-23 10:04:51 +0800

    dev

--- scx-common/src/main/java/cool/scx/common/ffm/type/wrapper/ShortWrapper.java
+++ scx-common/src/main/java/cool/scx/common/ffm/type/wrapper/ShortWrapper.java
@@ -1,5 +1,5 @@
 package cool.scx.common.ffm.type.wrapper;
 
 public interface ShortWrapper extends Wrapper<Short> {
-    
+
 }
--- scx-common/src/main/java/cool/scx/common/ffm/type/wrapper/Wrapper.java
+++ scx-common/src/main/java/cool/scx/common/ffm/type/wrapper/Wrapper.java
@@ -1,14 +1,22 @@
 package cool.scx.common.ffm.type.wrapper;
 
+import cool.scx.common.ffm.type.Parameter;
 import cool.scx.common.ffm.type.mapper.Mapper;
+
+import java.lang.foreign.Arena;
 
 /**
  * 创建动机是为了规范化一些参数类型
  * 此参数类型其实可以直接被替换成对应的基本类型
  * 将其理解为只读 如果需要在外部函数中修改 请使用 {@link Mapper}
  */
-public interface Wrapper<V> {
+public interface Wrapper<V> extends Parameter {
 
     V getValue();
+
+    @Override
+    default Object toNativeParameter(Arena arena) {
+        return getValue();
+    }
 
 }
