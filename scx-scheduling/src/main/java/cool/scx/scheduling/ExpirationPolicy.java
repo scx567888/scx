52ff75b959fb6fe9a24cbcf9337753af085d7349
scx567888 <scx567888@outlook.com>
2024-09-25 13:49:25 +0800

    dev

---
+++ scx-scheduling/src/main/java/cool/scx/scheduling/ExpirationPolicy.java
@@ -0,0 +1,18 @@
+package cool.scx.scheduling;
+
+/**
+ * 过期策略
+ */
+public enum ExpirationPolicy {
+
+    /**
+     * 忽略
+     */
+    IGNORE,
+
+    /**
+     * 补偿
+     */
+    COMPENSATE
+    
+}
