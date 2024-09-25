5a853571d783caf59c692c4d332e543e4aa00932
scx567888 <scx567888@outlook.com>
2024-09-25 16:50:09 +0800

    dev

--- scx-scheduling/src/main/java/cool/scx/scheduling/ExpirationPolicy.java
+++ scx-scheduling/src/main/java/cool/scx/scheduling/ExpirationPolicy.java
@@ -16,6 +16,7 @@ public enum ExpirationPolicy {
      * 回溯忽略
      * 针对单次任务 可以理解为不执行
      * 针对多次任务 会计算距离当前最近的调度时间点 之前的跳过 (runCount 会增加)
+     * 可以理解为之前的任务执行了 (实际并没有,被忽略了) 但是 runCount 还是会增加
      */
     BACKTRACKING_IGNORE,
 
