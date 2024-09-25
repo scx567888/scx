1178338e1f67a63d44788d9f797f17ac092858ea
scx567888 <scx567888@outlook.com>
2024-09-25 16:32:55 +0800

    dev

--- scx-scheduling/src/main/java/cool/scx/scheduling/ExpirationPolicy.java
+++ scx-scheduling/src/main/java/cool/scx/scheduling/ExpirationPolicy.java
@@ -6,13 +6,31 @@ package cool.scx.scheduling;
 public enum ExpirationPolicy {
 
     /**
-     * 忽略
+     * 立即忽略
+     * 针对单次任务 可以理解为不执行
+     * 针对多次任务 会计算距离当前最近的调度时间点 之前的跳过 (runCount 不会增加)
      */
-    IGNORE,
+    IMMEDIATE_IGNORE,
 
     /**
-     * 补偿
+     * 回溯忽略
+     * 针对单次任务 可以理解为不执行
+     * 针对多次任务 会计算距离当前最近的调度时间点 之前的跳过 (runCount 会增加)
      */
-    COMPENSATE
+    BACKTRACKING_IGNORE,
+
+    /**
+     * 立即补偿
+     * 针对单次任务 可以理解为立即执行
+     * 针对多次任务 会立即执行一次 之后按照正常调度时间点处理
+     */
+    IMMEDIATE_COMPENSATION,
+
+    /**
+     * 回溯补偿
+     * 针对单次任务 可以理解为立即执行 和 IMMEDIATE_COMPENSATION 相同
+     * 针对多次任务 会计算未执行的次数 然后立即执行 之后按照正常调度时间点处理
+     */
+    BACKTRACKING_COMPENSATION
 
 }
