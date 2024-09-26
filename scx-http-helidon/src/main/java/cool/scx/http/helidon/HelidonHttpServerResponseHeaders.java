ae5714e674d1476222338c187f990460655e8027
scx567888 <scx567888@outlook.com>
2024-09-26 14:38:57 +0800

    dev

--- scx-http-helidon/src/main/java/cool/scx/http/helidon/HelidonHttpServerResponseHeaders.java
+++ scx-http-helidon/src/main/java/cool/scx/http/helidon/HelidonHttpServerResponseHeaders.java
@@ -2,12 +2,17 @@ package cool.scx.http.helidon;
 
 import cool.scx.http.ScxHttpHeaderName;
 import cool.scx.http.ScxHttpHeadersWritable;
+import cool.scx.http.ScxHttpServerResponseHeaders;
+import cool.scx.http.cookie.Cookie;
 import io.helidon.http.HeaderNames;
 import io.helidon.http.ServerResponseHeaders;
+import io.helidon.http.SetCookie;
 
-class HelidonHttpHeadersWritable extends HelidonHttpHeaders<ServerResponseHeaders> implements ScxHttpHeadersWritable {
+import java.time.Duration;
 
-    public HelidonHttpHeadersWritable(ServerResponseHeaders headers) {
+class HelidonHttpServerResponseHeaders extends HelidonHttpHeaders<ServerResponseHeaders> implements ScxHttpServerResponseHeaders {
+
+    public HelidonHttpServerResponseHeaders(ServerResponseHeaders headers) {
         super(headers);
     }
 
@@ -41,6 +46,27 @@ class HelidonHttpHeadersWritable extends HelidonHttpHeaders<ServerResponseHeader
     @Override
     public ScxHttpHeadersWritable remove(String headerName) {
         headers.remove(HeaderNames.create(headerName));
+        return this;
+    }
+
+    @Override
+    public ScxHttpServerResponseHeaders addCookie(Cookie cookie) {
+        var c = SetCookie
+                .builder(cookie.name(), cookie.name())
+                .domain(cookie.domain())
+                .path(cookie.path())
+                .maxAge(Duration.ofSeconds(cookie.maxAge()))
+                .secure(cookie.secure())
+                .httpOnly(cookie.httpOnly())
+                .sameSite(SetCookie.SameSite.valueOf(cookie.sameSite().name()))
+                .build();
+        this.headers.addCookie(c);
+        return null;
+    }
+
+    @Override
+    public ScxHttpServerResponseHeaders removeCookie(String name) {
+        this.headers.clearCookie(name);
         return this;
     }
 
