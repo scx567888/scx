9292c6df34aa57bb1737f359fb7196165a7911c1
scx567888 <scx567888@outlook.com>
2024-09-27 09:53:35 +0800

    dev

---
+++ scx-http/src/main/java/cool/scx/http/media/MediaReader.java
@@ -0,0 +1,16 @@
+package cool.scx.http.media;
+
+import cool.scx.http.ScxHttpServerRequestHeaders;
+
+import java.io.InputStream;
+
+/**
+ * 媒体读取器 一般用来从 ServerRequest 或 ClientResponse 中读取数据
+ *
+ * @param <T>
+ */
+public interface MediaReader<T> {
+
+    T read(InputStream inputStream, ScxHttpServerRequestHeaders headers);
+
+}
