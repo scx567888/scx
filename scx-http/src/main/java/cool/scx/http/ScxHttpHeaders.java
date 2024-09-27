0b712f3450a3bbbccbe8e2894dc11c610d463633
scx567888 <scx567888@outlook.com>
2024-09-27 12:47:05 +0800

    dev

--- scx-http/src/main/java/cool/scx/http/ScxHttpHeaders.java
+++ scx-http/src/main/java/cool/scx/http/ScxHttpHeaders.java
@@ -1,11 +1,18 @@
 package cool.scx.http;
 
+import cool.scx.http.content_disposition.ContentDisposition;
+import cool.scx.http.content_type.ContentType;
+import cool.scx.http.cookie.Cookie;
+import cool.scx.http.cookie.Cookies;
+
 import java.util.List;
 
+import static cool.scx.http.HttpFieldName.*;
 import static cool.scx.http.ScxHttpHeadersHelper.parseHeaders;
 
 /**
- * ScxHttpHeaders
+ * 只读的 Headers 可用于 ServerRequest 和 ClientResponse
+ * 在 Parameters 的基础上实现了一些 方便操作 Http 头协议的方法
  */
 public interface ScxHttpHeaders extends Parameters<ScxHttpHeaderName, String> {
 
@@ -27,6 +34,30 @@ public interface ScxHttpHeaders extends Parameters<ScxHttpHeaderName, String> {
 
     default boolean contains(String name) {
         return contains(ScxHttpHeaderName.of(name));
+    }
+
+    default Cookies cookies() {
+        return Cookies.of(get(COOKIE));
+    }
+
+    default Cookies setCookies() {
+        return Cookies.of(get(SET_COOKIE));
+    }
+
+    default ContentType contentType() {
+        return ContentType.of(get(CONTENT_TYPE));
+    }
+
+    default ContentDisposition contentDisposition() {
+        return ContentDisposition.of(get(CONTENT_DISPOSITION));
+    }
+
+    default Cookie getCookie(String name) {
+        return cookies().get(name);
+    }
+
+    default Cookie getSetCookie(String name) {
+        return setCookies().get(name);
     }
 
 }
