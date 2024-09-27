db34b60509256ee1b6e8b4568a5ab23a77821b50
scx567888 <scx567888@outlook.com>
2024-09-27 09:40:43 +0800

    dev

---
+++ scx-http/src/main/java/cool/scx/http/media/input_stream/InputStreamSupport.java
@@ -0,0 +1,26 @@
+package cool.scx.http.media.input_stream;
+
+import cool.scx.http.media.MediaReader;
+import cool.scx.http.media.MediaSupport;
+
+import java.io.InputStream;
+
+import static cool.scx.http.media.input_stream.InputStreamReader.INPUT_STREAM_READER;
+
+/**
+ * 此类保持单例模式即可
+ */
+public class InputStreamSupport implements MediaSupport<InputStream> {
+
+    public static final InputStreamSupport INPUT_STREAM_SUPPORT = new InputStreamSupport();
+
+    private InputStreamSupport() {
+
+    }
+
+    @Override
+    public MediaReader<InputStream> reader() {
+        return INPUT_STREAM_READER;
+    }
+
+}
