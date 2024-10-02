8b1bcceae30cfbb3332fe1723fbe14e6d7999cdd
scx567888 <scx567888@outlook.com>
2024-10-02 10:35:47 +0800

    dev

--- scx-http/src/main/java/cool/scx/http/ParametersImpl.java
+++ scx-http/src/main/java/cool/scx/http/ParametersImpl.java
@@ -2,17 +2,14 @@ package cool.scx.http;
 
 import cool.scx.common.util.MultiMap;
 
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * ParametersImpl
  */
 public class ParametersImpl<K, V> implements ParametersWritable<K, V> {
 
-    private final MultiMap<K, V> map = new MultiMap<>();
+    private final MultiMap<K, V> map = new MultiMap<>(LinkedHashMap::new, ArrayList::new);
 
     @SuppressWarnings("unchecked")
     @Override
@@ -74,5 +71,5 @@ public class ParametersImpl<K, V> implements ParametersWritable<K, V> {
     public String toString() {
         return map.toString();
     }
-    
+
 }
