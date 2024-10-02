4e1f032e9ad6292e7dd7d92e73f0c0c933e9dd28
scx567888 <scx567888@outlook.com>
2024-10-02 11:14:26 +0800

    dev

--- scx-http-helidon/src/test/java/cool/scx/http/helidon/test/ClientTest.java
+++ scx-http-helidon/src/test/java/cool/scx/http/helidon/test/ClientTest.java
@@ -3,8 +3,12 @@ package cool.scx.http.helidon.test;
 import cool.scx.http.ScxHttpServerOptions;
 import cool.scx.http.helidon.HelidonHttpClient;
 import cool.scx.http.helidon.HelidonHttpServer;
+import cool.scx.http.uri.ScxURIHelper;
+import io.helidon.common.uri.UriEncoding;
 
 import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 public class ClientTest {
 
@@ -16,19 +20,23 @@ public class ClientTest {
 
     public static void test1() throws IOException, InterruptedException {
         var httpServer = new HelidonHttpServer(new ScxHttpServerOptions().setPort(8990));
+        httpServer.requestHandler(c->{
+            System.out.println(c.uri());
+            c.response().send("Hi Client !!!");
+        });
         httpServer.webSocketHandler(c -> {
             System.out.println(c.uri());
             c.onTextMessage(t -> {
                 System.out.println(t);
             });
-            c.send("Hi Client !!!");
+            c.send("Hi WS Client !!!");
         });
         httpServer.start();
     }
 
     public static void test2() {
         var httpClient = new HelidonHttpClient();
-        var webSocketBuilder = httpClient.webSocket().uri("http://localhost:8990/test");
+        var webSocketBuilder = httpClient.webSocket().uri("http://localhost:8990/中:文|路径/ddd?查询=🎈🎈|🎈");
         webSocketBuilder.onConnect(webSocket -> {
             webSocket.onTextMessage(t -> {
                 System.out.println(t);
@@ -38,13 +46,13 @@ public class ClientTest {
         webSocketBuilder.connect();
     }
 
-    public static void test3() throws IOException, InterruptedException {
+    public static void test3() {
         var httpClient = new HelidonHttpClient();
         var response = httpClient.request()
-                .uri("http://www.baidu.com")
+                .uri("http://localhost:8990/中:文|路径/ddd?查询=🎈🎈|🎈")
                 .send();
         var string = response.body().asString();
-        System.out.println(string.length());
+        System.out.println(string);
     }
 
 }
