88145150e0fc6d12ae1eac1c63b12ccd52525836
scx567888 <scx567888@outlook.com>
2024-10-07 20:15:06 +0800

    dev

--- scx-core/src/main/java/cool/scx/core/ScxVersion.java
+++ scx-core/src/main/java/cool/scx/core/ScxVersion.java
@@ -13,7 +13,7 @@ public class ScxVersion {
     /**
      * SCX 版本号
      */
-    public static final String SCX_VERSION = "3.1.5";
+    public static final String SCX_VERSION = "3.1.6";
 
     /**
      * 在控制台上打印 banner
