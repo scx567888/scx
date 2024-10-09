4c795eb572e97dbeb82b98efac4e67d57768ed52
scx567888 <scx567888@outlook.com>
2024-10-09 15:27:58 +0800

    dev

---
+++ scx-net/src/main/java/cool/scx/net/ScxTCPServerImpl.java
@@ -0,0 +1,101 @@
+package cool.scx.net;
+
+import javax.net.ssl.SSLSocket;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.util.function.Consumer;
+
+public class ScxTCPServerImpl implements ScxTCPServer {
+
+    private final ScxTCPServerOptions options;
+    private final Thread serverThread;
+    private Consumer<ScxTCPSocket> connectHandler;
+    private ServerSocket serverSocket;
+    private boolean running;
+
+    public ScxTCPServerImpl() {
+        this(new ScxTCPServerOptions());
+    }
+
+    public ScxTCPServerImpl(ScxTCPServerOptions options) {
+        this.options = options;
+        this.serverThread = Thread.ofPlatform().unstarted(this::listen);
+    }
+
+    @Override
+    public ScxTCPServer onConnect(Consumer<ScxTCPSocket> connectHandler) {
+        this.connectHandler = connectHandler;
+        return this;
+    }
+
+    @Override
+    public void start() {
+        if (running) {
+            throw new IllegalStateException("Server is already running");
+        }
+
+        try {
+            var tls = options.tls();
+            if (tls != null && tls.enabled()) {
+                this.serverSocket = tls.createServerSocket();
+            } else {
+                this.serverSocket = new ServerSocket();
+            }
+            this.serverSocket.bind(new InetSocketAddress(options.port()));
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+
+        running = true;
+
+        serverThread.start();
+    }
+
+    @Override
+    public void stop() {
+        if (!running) {
+            return;
+        }
+
+        running = false;
+
+        try {
+            serverSocket.close();
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+
+        serverThread.interrupt();
+    }
+
+    @Override
+    public int port() {
+        return serverSocket.getLocalPort();
+    }
+
+    private void listen() {
+        while (running) {
+            try {
+                var socket = this.serverSocket.accept();
+                Thread.ofVirtual().start(() -> {
+                    try {
+                        //尝试握手
+                        if (socket instanceof SSLSocket sslSocket) {
+                            sslSocket.startHandshake();
+                        }
+                        //调用处理器
+                        var tcpSocket = new ScxTCPSocketImpl(socket);
+                        connectHandler.accept(tcpSocket);
+                    } catch (Exception e) {
+                        //暂时忽略
+                    }
+                });
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        }
+    }
+
+}
