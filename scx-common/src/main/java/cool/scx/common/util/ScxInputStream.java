303667dd33b510c956262e0d9c48fe7f731c3abd
scx567888 <scx567888@outlook.com>
2024-10-10 13:28:08 +0800

    dev

--- scx-common/src/main/java/cool/scx/common/util/ScxInputStream.java
+++ scx-common/src/main/java/cool/scx/common/util/ScxInputStream.java
@@ -1,31 +1,41 @@
 package cool.scx.common.util;
 
+import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-public class InputStreamHelper {
+public class ScxInputStream extends BufferedInputStream {
 
-    public static byte[] readMatch(InputStream inputStream, byte[] match) throws IOException {
+    public ScxInputStream(InputStream in) {
+        super(in);
+    }
+
+    public ScxInputStream(InputStream in, int size) {
+        super(in, size);
+    }
+
+    public byte[] readMatch(byte[] match) throws IOException {
         ByteArrayOutputStream buffer = new ByteArrayOutputStream();
         int matchIndex = 0;
         int b;
 
-        while ((b = inputStream.read()) != -1) {
-            
+        while ((b = this.read()) != -1) {
 
             if (b == match[matchIndex]) {
                 matchIndex++;
                 if (matchIndex == match.length) {
                     // 完全匹配，返回之前的内容
                     return buffer.toByteArray();
-                }else{
+                } else {
                     buffer.write(b);
                 }
             } else {
                 // 部分匹配失败，保存部分匹配的状态
                 matchIndex = matchFailure(match, matchIndex, b);
-                buffer.write(b);
+                if (matchIndex == 0) {
+                    buffer.write(b);
+                }
             }
         }
 
