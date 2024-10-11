3f00cc99fac9a07e050e6e96288cb97f889ee54f
scx567888 <scx567888@outlook.com>
2024-10-11 17:49:08 +0800

    dev

--- scx-io/src/main/java/cool/scx/io/NoMoreDataException.java
+++ scx-io/src/main/java/cool/scx/io/NoMoreDataException.java
@@ -1,9 +1,9 @@
 package cool.scx.io;
 
 public class NoMoreDataException extends RuntimeException {
-    
-    public NoMoreDataException(String message) {
-        super(message);
+
+    public NoMoreDataException() {
+
     }
-    
+
 }
