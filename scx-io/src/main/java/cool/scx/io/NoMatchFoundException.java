4bb61645dce37cef608eaf20bbb36eda0ffcd8d3
scx567888 <scx567888@outlook.com>
2024-10-11 18:30:45 +0800

    dev

--- scx-io/src/main/java/cool/scx/io/NoMatchFoundException.java
+++ scx-io/src/main/java/cool/scx/io/NoMatchFoundException.java
@@ -1,9 +1,12 @@
 package cool.scx.io;
 
+/**
+ * 寻找匹配内容 查找失败的异常
+ */
 public class NoMatchFoundException extends RuntimeException {
-    
+
     public NoMatchFoundException() {
-        
+
     }
-    
+
 }
--- scx-io/src/main/java/cool/scx/io/NoMoreDataException.java
+++ scx-io/src/main/java/cool/scx/io/NoMoreDataException.java
@@ -1,5 +1,8 @@
 package cool.scx.io;
 
+/**
+ * 读取数据时达到末尾的异常
+ */
 public class NoMoreDataException extends RuntimeException {
 
     public NoMoreDataException() {
