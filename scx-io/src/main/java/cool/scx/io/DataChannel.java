7ba1c05814cba8a2db8ebb1a27fd00c21bf2d44d
scx567888 <scx567888@outlook.com>
2024-10-18 17:36:37 +0800

    dev

--- scx-io/src/main/java/cool/scx/io/DataChannel.java
+++ scx-io/src/main/java/cool/scx/io/DataChannel.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.file.Path;
 
+/**
+ * 数据通道接口，提供读写数据的方法
+ */
 public interface DataChannel {
 
     void write(ByteBuffer buffer) throws IOException;
@@ -26,6 +29,15 @@ public interface DataChannel {
 
     void read(Path path) throws IOException;
 
+    /**
+     * 读取字节
+     * 当没有更多的数据时会抛出异常
+     *
+     * @param maxLength 最大长度
+     * @return 读取的字节
+     * @throws IOException         异常
+     * @throws NoMoreDataException 没有更多数据时抛出
+     */
     byte[] read(int maxLength) throws IOException, NoMoreDataException;
 
     void close() throws IOException;
