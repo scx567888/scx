6ea4080d47d2ffaf112aa62665fa1a577a22d1f6
scx567888 <scx567888@outlook.com>
2024-10-18 17:40:19 +0800

    dev

--- scx-net/src/main/java/cool/scx/net/ScxTCPSocket.java
+++ scx-net/src/main/java/cool/scx/net/ScxTCPSocket.java
@@ -1,37 +1,11 @@
 package cool.scx.net;
 
-import cool.scx.io.NoMoreDataException;
+import cool.scx.io.DataChannel;
 
 import java.io.IOException;
 import java.net.SocketAddress;
-import java.nio.ByteBuffer;
-import java.nio.file.Path;
 
-public interface ScxTCPSocket {
-
-    void write(ByteBuffer buffer) throws IOException;
-
-    void write(byte[] bytes, int offset, int length) throws IOException;
-
-    void write(byte[] bytes) throws IOException;
-
-    void write(Path path, long offset, long length) throws IOException;
-
-    void write(Path path) throws IOException;
-
-    int read(ByteBuffer buffer) throws IOException;
-
-    int read(byte[] bytes, int offset, int length) throws IOException;
-
-    int read(byte[] bytes) throws IOException;
-
-    void read(Path path, long offset, long length) throws IOException;
-
-    void read(Path path) throws IOException;
-
-    byte[] read(int maxLength) throws IOException, NoMoreDataException;
-
-    void close() throws IOException;
+public interface ScxTCPSocket extends DataChannel {
 
     SocketAddress getRemoteAddress() throws IOException;
 
