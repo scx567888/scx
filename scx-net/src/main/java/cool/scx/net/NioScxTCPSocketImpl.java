d3b40f45100b59cd9383a1d698db4f9fcb5b5171
scx567888 <scx567888@outlook.com>
2024-10-18 15:41:15 +0800

    dev

--- scx-net/src/main/java/cool/scx/net/NioScxTCPSocketImpl.java
+++ scx-net/src/main/java/cool/scx/net/NioScxTCPSocketImpl.java
@@ -10,6 +10,7 @@ import java.nio.channels.SocketChannel;
 import java.nio.file.Path;
 
 import static java.nio.file.StandardOpenOption.READ;
+import static java.nio.file.StandardOpenOption.WRITE;
 
 public class NioScxTCPSocketImpl implements ScxTCPSocket {
 
@@ -17,6 +18,83 @@ public class NioScxTCPSocketImpl implements ScxTCPSocket {
 
     public NioScxTCPSocketImpl(SocketChannel socketChannel) {
         this.socketChannel = socketChannel;
+    }
+
+    @Override
+    public void write(ByteBuffer buffer) throws IOException {
+        while (buffer.hasRemaining()) {
+            socketChannel.write(buffer);
+        }
+    }
+
+    @Override
+    public void write(byte[] bytes, int offset, int length) throws IOException {
+        write(ByteBuffer.wrap(bytes, offset, length));
+    }
+
+    @Override
+    public void write(byte[] bytes) throws IOException {
+        write(ByteBuffer.wrap(bytes));
+    }
+
+    @Override
+    public void write(Path path, long offset, long length) throws IOException {
+        try (var fileChannel = FileChannel.open(path, READ)) {
+            while (length > 0) {
+                // transferTo 不保证一次既可以全部传输完毕 所以我们需要循环调用 
+                var i = fileChannel.transferTo(offset, length, socketChannel);
+                offset += i;
+                length -= i;
+            }
+        }
+    }
+
+    @Override
+    public void write(Path path) throws IOException {
+        try (var fileChannel = FileChannel.open(path, READ)) {
+            long offset = 0;
+            long length = fileChannel.size();
+            while (length > 0) {
+                // transferTo 不保证一次既可以全部传输完毕 所以我们需要循环调用 
+                var i = fileChannel.transferTo(offset, length, socketChannel);
+                offset += i;
+                length -= i;
+            }
+        }
+    }
+
+    @Override
+    public int read(ByteBuffer buffer) throws IOException, NoMoreDataException {
+        return 0;
+    }
+
+    @Override
+    public int read(byte[] bytes, int offset, int length) throws IOException, NoMoreDataException {
+        var buffer = ByteBuffer.wrap(bytes, offset, length);
+        return this.socketChannel.read(buffer);
+    }
+
+    @Override
+    public int read(byte[] bytes) throws IOException, NoMoreDataException {
+        var buffer = ByteBuffer.wrap(bytes);
+        return this.socketChannel.read(buffer);
+    }
+
+    @Override
+    public int read(Path path, long offset, long length) throws IOException {
+        try (var fileChannel = FileChannel.open(path, WRITE)) {
+            while (length > 0) {
+                // transferTo 不保证一次既可以全部传输完毕 所以我们需要循环调用 
+                var i = fileChannel.transferFrom(socketChannel, offset, length);
+                offset += i;
+                length -= i;
+            }
+        }
+    }
+
+    @Override
+    public int read(Path path) throws IOException {
+        return 0;
     }
 
     @Override
@@ -37,24 +115,22 @@ public class NioScxTCPSocketImpl implements ScxTCPSocket {
         }
     }
 
+
     @Override
-    public void write(byte[] bytes) throws IOException {
-        var buffer = ByteBuffer.wrap(bytes);
-        while (buffer.hasRemaining()) {
-            socketChannel.write(buffer);
+    public void receiveFile(Path path, long offset, long length) throws IOException {
+        try (var fileChannel = FileChannel.open(path, WRITE)) {
+            while (length > 0) {
+                // transferTo 不保证一次既可以全部传输完毕 所以我们需要循环调用 
+                var i = fileChannel.transferFrom(socketChannel, offset, length);
+                offset += i;
+                length -= i;
+            }
         }
     }
 
     @Override
     public void sendFile(Path path, long offset, long length) throws IOException {
-        try (var fileChannel = FileChannel.open(path, READ)) {
-            while (length > 0) {
-                // transferTo 不保证一次既可以全部传输完毕 所以我们需要循环调用 
-                var i = fileChannel.transferTo(offset, length, socketChannel);
-                offset += i;
-                length -= i;
-            }
-        }
+
     }
 
     @Override
