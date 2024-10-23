2301cee37322519d63de93986183c425128684af
scx567888 <scx567888@outlook.com>
2024-10-23 11:00:45 +0800

    dev

--- scx-net/src/test/java/cool/scx/net/test/ClientTest.java
+++ scx-net/src/test/java/cool/scx/net/test/ClientTest.java
@@ -1,19 +1,8 @@
 package cool.scx.net.test;
 
-import cool.scx.net.ScxTCPClientImpl;
-import cool.scx.net.ScxTCPClientOptions;
-
-import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
-import java.util.concurrent.atomic.AtomicInteger;
-
-import static cool.scx.net.test.ServerTest.tls;
-
 public class ClientTest {
 
     public static void main(String[] args) {
-        
+
     }
 }
--- scx-net/src/test/java/cool/scx/net/test/ServerTest.java
+++ scx-net/src/test/java/cool/scx/net/test/ServerTest.java
@@ -1,15 +1,5 @@
 package cool.scx.net.test;
 
-import cool.scx.io.LinkedDataReader;
-import cool.scx.net.ScxTCPServerImpl;
-import cool.scx.net.ScxTCPServerImpl2;
-import cool.scx.net.ScxTCPServerOptions;
-import cool.scx.net.tls.TLS;
-
-import java.io.IOException;
-import java.nio.file.Path;
-import java.util.concurrent.atomic.AtomicInteger;
-
 public class ServerTest {
 
     public static void main(String[] args) {
