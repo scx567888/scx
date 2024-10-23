5160acf61e8d8649c8807a21b176d5d62bc05b8f
scx567888 <scx567888@outlook.com>
2024-10-23 09:48:21 +0800

    dev

--- scx-io/src/main/java/cool/scx/io/BufferHelper.java
+++ scx-io/src/main/java/cool/scx/io/BufferHelper.java
@@ -11,10 +11,30 @@ public class BufferHelper {
     }
 
     public static ByteBuffer expandBuffer(ByteBuffer oldBuffer) {
-        var newBuffer = ByteBuffer.allocate(oldBuffer.capacity() * 2);
+        return expandBuffer(oldBuffer, oldBuffer.capacity() * 2);
+    }
+
+    public static ByteBuffer expandBuffer(ByteBuffer oldBuffer, int size) {
+        var newBuffer = ByteBuffer.allocate(size);
         oldBuffer.flip();
         newBuffer.put(oldBuffer);
         return newBuffer;
+    }
+
+    /**
+     * 会自动扩容
+     *
+     * @param buffer b
+     * @param bytes  b
+     * @return b
+     */
+    public static ByteBuffer putBytes(ByteBuffer buffer, byte[] bytes) {
+        if (buffer.remaining() < bytes.length) {
+            int newCapacity = Math.max(buffer.capacity() * 2, buffer.capacity() + bytes.length);
+            buffer = expandBuffer(buffer, newCapacity);
+        }
+        buffer.put(bytes);
+        return buffer;
     }
 
 }
