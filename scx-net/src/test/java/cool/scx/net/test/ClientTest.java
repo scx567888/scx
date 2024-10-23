0f34b477e76a6e78ef5993be7695babda9af3d38
scx567888 <scx567888@outlook.com>
2024-10-23 11:00:36 +0800

    dev

--- scx-net/src/test/java/cool/scx/net/test/ClientTest.java
+++ scx-net/src/test/java/cool/scx/net/test/ClientTest.java
@@ -1,12 +1,10 @@
 package cool.scx.net.test;
 
 import cool.scx.net.ScxTCPClientImpl;
-import cool.scx.net.ScxTCPClientImpl2;
 import cool.scx.net.ScxTCPClientOptions;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -15,48 +13,7 @@ import static cool.scx.net.test.ServerTest.tls;
 
 public class ClientTest {
 
-    public static void main(String[] args) throws InterruptedException, IOException {
-//        test1();
-        for (int i = 0; i < 1; i++) {
-            Thread.ofVirtual().start(() -> {
-                test2();
-            });
-
-        }
-        Thread.sleep(9999999);
+    public static void main(String[] args) {
+        
     }
-
-    public static void test1() throws InterruptedException, IOException {
-        var tcpClient = new ScxTCPClientImpl2(new ScxTCPClientOptions().tls(tls));
-        var tcpSocket = tcpClient.connect(new InetSocketAddress(8899));
-        try {
-            AtomicInteger i = new AtomicInteger(0);
-            while (i.get() < 100) {
-//                tcpSocket.sendFile((i.getAndIncrement() + "\r\n").getBytes());
-//                sleep(50);
-            }
-            tcpSocket.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static void test2() {
-        var tcpClient = new ScxTCPClientImpl(new ScxTCPClientOptions().tls(tls));
-        var tcpSocket = tcpClient.connect(new InetSocketAddress(8899));
-        try {
-            Path path = Path.of("C:\\Users\\scx\\Desktop\\aaaa.iso");
-            long size = Files.size(path);
-            tcpSocket.read(Path.of("C:\\Users\\scx\\Desktop\\aaaa.iso"), size, Long.MAX_VALUE, StandardOpenOption.APPEND, StandardOpenOption.WRITE);
-            System.out.println("读取完成");
-//            while (true) {
-//
-//                byte[] read = tcpSocket.read(8192);
-////                System.out.println(read.length);
-//            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
 }
--- scx-net/src/test/java/cool/scx/net/test/ServerTest.java
+++ scx-net/src/test/java/cool/scx/net/test/ServerTest.java
@@ -12,69 +12,8 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class ServerTest {
 
-    public static TLS tls;
-
-    static {
-        tls = null;
-    }
-
-    public static void main(String[] args) throws InterruptedException, IOException {
-//        test1();
-        test2();
-    }
-
-    public static void test1() {
-        var tcpServer = new ScxTCPServerImpl2(new ScxTCPServerOptions().port(8899).tls(tls));
-        var i = new AtomicInteger(0);
-        tcpServer.onConnect(c -> {
-            System.out.println("客户端连接了 !!!");
-            int n = i.getAndIncrement();
-            var dataReader = new LinkedDataReader(() -> {
-                try {
-                    return c.read(8192);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            });
-            while (true) {
-                try {
-                    var s = dataReader.readUntil("\r\n".getBytes());
-                    if (s == null) {
-                        c.close();
-                        tcpServer.stop();
-                        return;
-                    }
-                    System.out.println(n + " " + c.getRemoteAddress() + " : " + new String(s));
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        });
-        tcpServer.start();
-        System.out.println("已监听端口号 : " + tcpServer.port());
-    }
-
-    public static void test2() {
-        var tcpServer = new ScxTCPServerImpl(new ScxTCPServerOptions().port(8899).tls(tls));
-        var i = new AtomicInteger(0);
-        tcpServer.onConnect(c -> {
-            System.out.println("客户端连接了 !!!");
-            long l = System.nanoTime();
-            var path = Path.of("C:\\Users\\scx\\Documents\\ISO\\ubuntu-24.04.1-desktop-amd64.iso");
-            try {
-                c.write(path);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            System.out.println("耗时 :" + (System.nanoTime() - l) / 1000_000);
-            try {
-                c.close();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        });
-        tcpServer.start();
-        System.out.println("已监听端口号 : " + tcpServer.port());
+    public static void main(String[] args) {
+        
     }
 
 }
