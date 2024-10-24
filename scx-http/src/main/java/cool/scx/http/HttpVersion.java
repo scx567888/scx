ca8f6361a7e745230254d2e90bef8e8ac6f1983a
scx567888 <scx567888@outlook.com>
2024-10-24 09:34:18 +0800

    dev

--- scx-http/src/main/java/cool/scx/http/HttpVersion.java
+++ scx-http/src/main/java/cool/scx/http/HttpVersion.java
@@ -18,9 +18,23 @@ public enum HttpVersion {
     /**
      * 
      * @param version v
-     * @return 未找到时返回 null
+     * @return 未找到时 抛出异常
      */
     public static HttpVersion of(String version) {
+        var upperCase = version.toUpperCase();
+        return switch (upperCase) {
+            case "HTTP/1.1" -> HTTP_1_1;
+            case "HTTP/2.0" -> HTTP_2;
+            default -> throw new IllegalArgumentException("Unsupported HTTP version: " + version);
+        };
+    }
+
+    /**
+     *
+     * @param version v
+     * @return 未找到时返回 null
+     */
+    public static HttpVersion find(String version) {
         var upperCase = version.toUpperCase();
         return switch (upperCase) {
             case "HTTP/1.1" -> HTTP_1_1;
