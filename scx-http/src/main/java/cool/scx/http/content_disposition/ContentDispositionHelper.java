7f785f1c7700ec524a2c413ce9fb23b96b8b04a2
scx567888 <scx567888@outlook.com>
2024-10-26 14:51:35 +0800

    dev

--- scx-http/src/main/java/cool/scx/http/content_disposition/ContentDispositionHelper.java
+++ scx-http/src/main/java/cool/scx/http/content_disposition/ContentDispositionHelper.java
@@ -4,7 +4,7 @@ import cool.scx.http.Parameters;
 import cool.scx.http.ParametersWritable;
 
 import static cool.scx.common.util.StringUtils.removeQuotes;
-import static cool.scx.http.content_type.ContentTypeHelper.encodeParams;
+import static cool.scx.http.content_type.ContentTypeHelper.*;
 
 public class ContentDispositionHelper {
 
@@ -12,14 +12,14 @@ public class ContentDispositionHelper {
         if (contentDispositionStr == null) {
             return null;
         }
-        var split = contentDispositionStr.split(";\\s*");
+        var split = SEMICOLON_PATTERN.split(contentDispositionStr);
         if (split.length == 0) {
             return null;
         }
         var type = split[0];
         ParametersWritable<String, String> params = Parameters.of();
         for (var i = 1; i < split.length; i = i + 1) {
-            var s = split[i].split("=", 2);
+            var s = EQUALS_SIGN_PATTERN.split(split[i], 2);
             if (s.length == 2) {
                 //移除两端的引号
                 params.add(s[0], removeQuotes(s[1]));
