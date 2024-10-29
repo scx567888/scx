93a58479a544d2fc01cd184c006217c03e130d2e
scx567888 <scx567888@outlook.com>
2024-10-29 15:05:24 +0800

    dev

--- scx-net/src/main/java/cool/scx/net/TCPClient.java
+++ scx-net/src/main/java/cool/scx/net/TCPClient.java
@@ -6,7 +6,11 @@ import java.io.UncheckedIOException;
 import java.net.Socket;
 import java.net.SocketAddress;
 
+import static java.lang.System.Logger.Level.ERROR;
+
 public class TCPClient implements ScxTCPClient {
+
+    private static final System.Logger logger = System.getLogger(TCPClient.class.getName());
 
     private final ScxTCPClientOptions options;
 
@@ -26,9 +30,9 @@ public class TCPClient implements ScxTCPClient {
         //todo 处理代理
         var proxy = options.proxy();
 
+        Socket socket;
         try {
 
-            Socket socket;
             if (tls != null && tls.enabled()) {
                 socket = tls.createSocket();
             } else {
@@ -37,14 +41,27 @@ public class TCPClient implements ScxTCPClient {
 
             socket.connect(endpoint);
 
+            //主动调用握手 快速检测 ssl 错误 防止等到调用用户处理程序时才发现 
             if (socket instanceof SSLSocket sslSocket) {
-                sslSocket.startHandshake();
+                try {
+                    sslSocket.startHandshake();
+                } catch (IOException e) {
+                    logger.log(ERROR, "SSL 握手失败 : " + e.getMessage());
+                    try {
+                        sslSocket.close();
+                    } catch (IOException ce) {
+                        e.addSuppressed(ce);
+                    }
+                    throw e;
+                }
             }
 
-            return new TCPSocket(socket);
         } catch (IOException e) {
+            logger.log(ERROR, "连接失败 : ", e);
             throw new UncheckedIOException(e);
         }
+
+        return new TCPSocket(socket);
 
     }
 
