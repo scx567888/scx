83dab616434171c416aa2e047f1cc7aeb2ac3a48
scx567888 <scx567888@outlook.com>
2024-11-03 10:30:55 +0800

    dev

---
+++ scx-scheduling/src/main/java/cool/scx/scheduling/cron/Result.java
@@ -0,0 +1,12 @@
+package cool.scx.scheduling.cron;
+
+class Result {
+    public int[] values;
+    public Type type;
+
+    public Result(Type type, int... values) {
+        this.values = values;
+        this.type = type;
+    }
+    
+}
