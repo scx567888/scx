64524e86d90c17eb26daf1da5191efd79af35257
scx567888 <scx567888@outlook.com>
2024-11-05 17:50:29 +0800

    dev

---
+++ scx-web/src/main/java/cool/scx/web/parameter_handler/from_upload/FromUploadParameterHandlerBuilder.java
@@ -0,0 +1,28 @@
+package cool.scx.web.parameter_handler.from_upload;
+
+import cool.scx.http.media.multi_part.MultiPartPart;
+import cool.scx.reflect.ParameterInfo;
+import cool.scx.web.parameter_handler.ParameterHandler;
+import cool.scx.web.parameter_handler.ParameterHandlerBuilder;
+
+import static cool.scx.common.util.ObjectUtils.convertValue;
+
+/**
+ * 处理 FileUpload 类型参数
+ *
+ * @author scx567888
+ * @version 1.11.8
+ */
+public final class FromUploadParameterHandlerBuilder implements ParameterHandlerBuilder {
+
+    @Override
+    public ParameterHandler tryBuildParameterHandler(ParameterInfo parameter) {
+        var isArray = parameter.type().isCollectionLikeType() || parameter.type().isArrayType();
+        var rawType = isArray ? parameter.type().getContentType().getRawClass() : parameter.type().getRawClass();
+        if (rawType != MultiPartPart.class) {
+            return null;
+        }
+        return new FromUploadParameterHandler(parameter);
+    }
+
+}
