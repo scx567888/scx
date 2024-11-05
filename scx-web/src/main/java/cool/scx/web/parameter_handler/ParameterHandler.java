aef2a2125d50e130d82ddd463c2ded4a1fa78126
scx567888 <scx567888@outlook.com>
2024-11-05 17:54:49 +0800

    dev

--- scx-web/src/main/java/cool/scx/web/parameter_handler/ParameterHandler.java
+++ scx-web/src/main/java/cool/scx/web/parameter_handler/ParameterHandler.java
@@ -1,7 +1,5 @@
 package cool.scx.web.parameter_handler;
 
-import cool.scx.reflect.ParameterInfo;
-
 /**
  * 参数处理器
  *
@@ -11,21 +9,12 @@ import cool.scx.reflect.ParameterInfo;
 public interface ParameterHandler {
 
     /**
-     * 判断是否可以处理这个参数类型
-     *
-     * @param parameter 参数实例
-     * @return 是否能够处理
-     */
-    boolean canHandle(ParameterInfo parameter);
-
-    /**
      * 将结果处理并返回
      *
-     * @param parameter   方法参数
      * @param requestInfo 包装后的 RoutingContext
      * @return 处理后的结果
      * @throws java.lang.Exception e
      */
-    Object handle(ParameterInfo parameter, RequestInfo requestInfo) throws Exception;
+    Object handle(RequestInfo requestInfo) throws Exception;
 
 }
---
+++ scx-web/src/main/java/cool/scx/web/parameter_handler/ParameterHandlerBuilder.java
@@ -0,0 +1,21 @@
+package cool.scx.web.parameter_handler;
+
+import cool.scx.reflect.ParameterInfo;
+
+/**
+ * 参数处理器
+ *
+ * @author scx567888
+ * @version 1.11.8
+ */
+public interface ParameterHandlerBuilder {
+
+    /**
+     * 判断是否可以处理这个参数类型 返回 null 不支持
+     *
+     * @param parameter 参数实例
+     * @return 参数处理器 (为 null 表示不支持)
+     */
+    ParameterHandler tryBuild(ParameterInfo parameter);
+
+}
