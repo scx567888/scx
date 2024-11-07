675ee598f6c8ebc31bd1d75eaf53c8c20be8e10c
scx567888 <scx567888@outlook.com>
2024-11-07 14:14:42 +0800

    dev

---
+++ scx-io/src/main/java/cool/scx/io/source2/InputStreamInputSource.java
@@ -0,0 +1,52 @@
+package cool.scx.io.source2;
+
+import cool.scx.io.InputSource;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.OpenOption;
+import java.nio.file.Path;
+
+public class InputStreamInputSource implements InputSource {
+
+    private final InputStream inputStream;
+
+    public InputStreamInputSource(InputStream inputStream) {
+        this.inputStream = inputStream;
+    }
+
+    @Override
+    public byte[] read(int length) throws IOException {
+        return inputStream.readNBytes(length);
+    }
+
+    @Override
+    public byte[] readAll() throws IOException {
+        return inputStream.readAllBytes();
+    }
+
+    @Override
+    public long transferTo(OutputStream out) throws IOException {
+        return inputStream.transferTo(out);
+    }
+
+    @Override
+    public void transferTo(Path outputPath, OpenOption... options) throws IOException {
+        try (var outputStream = Files.newOutputStream(outputPath, options)) {
+            inputStream.transferTo(outputStream);
+        }
+    }
+
+    @Override
+    public InputStream toInputStream() throws IOException {
+        return inputStream;
+    }
+
+    @Override
+    public void close() throws Exception {
+        inputStream.close();
+    }
+
+}
