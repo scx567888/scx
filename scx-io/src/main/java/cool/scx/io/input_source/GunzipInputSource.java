99542fc73a20f5489c75353c25e9fb88bf020a97
scx567888 <scx567888@outlook.com>
2024-11-08 09:38:36 +0800

    dev

--- scx-io/src/main/java/cool/scx/io/input_source/GunzipInputSource.java
+++ scx-io/src/main/java/cool/scx/io/input_source/GunzipInputSource.java
@@ -4,53 +4,22 @@ import cool.scx.io.InputSource;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.nio.file.Files;
-import java.nio.file.OpenOption;
-import java.nio.file.Path;
 import java.util.zip.GZIPInputStream;
 
 /**
  * 用来解压缩
  */
-public class GunzipInputSource implements InputSource {
+public class GunzipInputSource extends LazyInputStreamInputSource {
 
-    private final GZIPInputStream gzipInputStream;
+    private final InputSource inputSource;
 
-    public GunzipInputSource(InputSource inputSource) throws IOException {
-        this.gzipInputStream = new GZIPInputStream(inputSource.toInputStream());
+    public GunzipInputSource(InputSource inputSource) {
+        this.inputSource = inputSource;
     }
 
     @Override
-    public byte[] read(int length) throws IOException {
-        return gzipInputStream.readNBytes(length);
-    }
-
-    @Override
-    public byte[] readAll() throws IOException {
-        return gzipInputStream.readAllBytes();
-    }
-
-    @Override
-    public long transferTo(OutputStream out) throws IOException {
-        return gzipInputStream.transferTo(out);
-    }
-
-    @Override
-    public void transferTo(Path outputPath, OpenOption... options) throws IOException {
-        try (var out = Files.newOutputStream(outputPath)) {
-            gzipInputStream.transferTo(out);
-        }
-    }
-
-    @Override
-    public InputStream toInputStream() {
-        return gzipInputStream;
-    }
-
-    @Override
-    public void close() throws IOException {
-        gzipInputStream.close();
+    public InputStream toInputStream0() throws IOException {
+        return new GZIPInputStream(inputSource.toInputStream());
     }
 
 }
