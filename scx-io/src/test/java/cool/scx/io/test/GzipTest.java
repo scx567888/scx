658f2e9af86607fba2197383dd5418ab268eabff
scx567888 <scx567888@outlook.com>
2024-11-08 12:05:01 +0800

    dev

---
+++ scx-io/src/test/java/cool/scx/io/test/GzipTest.java
@@ -0,0 +1,29 @@
+package cool.scx.io.test;
+
+import cool.scx.io.gzip.GunzipInputSource;
+import cool.scx.io.gzip.GzipInputSource;
+import cool.scx.io.input_source.ByteArrayInputSource;
+import org.testng.Assert;
+
+import java.io.IOException;
+
+public class GzipTest {
+
+    public static void main(String[] args) throws IOException {
+        test1();
+    }
+
+    public static void test1() throws IOException {
+        var s = "Data";
+        for (int i = 0; i < 10; i++) {
+            s += s;
+        }
+        var b = s.getBytes();
+        for (int i = 0; i < 10; i++) {
+            b = new GzipInputSource(new ByteArrayInputSource(b)).readAll();
+            b = new GunzipInputSource(new ByteArrayInputSource(b)).readAll();
+        }
+        Assert.assertEquals(s, new String(b));
+    }
+
+}
