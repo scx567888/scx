086b19088167563ecfe6318179ce26762fb8c137
scx567888 <scx567888@outlook.com>
2024-11-13 17:42:08 +0800

    dev

--- scx-ansi/src/main/java/cool/scx/ansi/AnsiStyle.java
+++ scx-ansi/src/main/java/cool/scx/ansi/AnsiStyle.java
@@ -8,11 +8,17 @@ package cool.scx.ansi;
  */
 public enum AnsiStyle implements AnsiElement {
 
-    NORMAL("0"),
-    BOLD("1"),
-    FAINT("2"),
-    ITALIC("3"),
-    UNDERLINE("4");
+    RESET("0"),              // 重置所有样式
+    BOLD("1"),               // 粗体
+    FAINT("2"),              // 微弱
+    ITALIC("3"),             // 斜体
+    UNDERLINE("4"),          // 下划线
+    BLINK("5"),              // 闪烁
+    REVERSE("7"),            // 反转前景色和背景色
+    HIDDEN("8"),             // 隐藏文本
+    CROSSED_OUT("9"),        // 删除线
+    DOUBLE_UNDERLINE("21"),  // 双下划线
+    OVERLINE("53");          // 上划线
 
     private final String code;
 
