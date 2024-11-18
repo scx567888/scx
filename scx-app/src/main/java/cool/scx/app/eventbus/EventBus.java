7c2e4793684fc95b8c7a0baff53a5e46c6ede4d7
scx567888 <scx567888@outlook.com>
2024-11-19 03:38:39 +0800

    dev

--- scx-app/src/main/java/cool/scx/app/eventbus/EventBus.java
+++ scx-app/src/main/java/cool/scx/app/eventbus/EventBus.java
@@ -11,7 +11,7 @@ public class EventBus {
     private final MultiMap<String, Consumer<Object>> events = new MultiMap<>(ConcurrentHashMap::new, ArrayList::new);
 
     public void publish(String name, Object data) {
-        var consumers = events.get(name);
+        var consumers = events.getAll(name);
         //这里暂时并行处理
         for (var consumer : consumers) {
             Thread.ofVirtual().start(() -> consumer.accept(data));
@@ -19,7 +19,7 @@ public class EventBus {
     }
 
     public void consumer(String name, Consumer<Object> consumer) {
-        this.events.put(name, consumer);
+        this.events.add(name, consumer);
     }
 
 }
