61e1bb1203aeacf3dfe8df5cd40333498d9fc498
scx567888 <scx567888@outlook.com>
2024-11-22 10:04:52 +0800

    dev

--- scx-http/src/main/java/cool/scx/http/accept/Accepts.java
+++ scx-http/src/main/java/cool/scx/http/accept/Accepts.java
@@ -2,8 +2,12 @@ package cool.scx.http.accept;
 
 public interface Accepts extends Iterable<Accept> {
 
-    static AcceptsWritable of(String accepts) {
-        return AcceptHelper.decodeAccepts(accepts);
+    static AcceptsWritable of(String acceptsStr) {
+        return AcceptHelper.decodeAccepts(acceptsStr);
+    }
+
+    static AcceptsWritable of(Accepts oldAccepts) {
+        return new AcceptsImpl(oldAccepts);
     }
 
     long size();
