d10c4d2cb46404b03d7a71ccef0fe50eb0e37bcc
scx567888 <scx567888@outlook.com>
2024-11-27 23:10:39 +0800

    dev

--- scx-socket/src/test/java/cool/scx/socket/test/ScxSocketClientTest.java
+++ scx-socket/src/test/java/cool/scx/socket/test/ScxSocketClientTest.java
@@ -16,7 +16,7 @@ public class ScxSocketClientTest extends InitLogger {
     @Test
     public static void test1() {
         //启动服务器
-        ScxSocketServerTest.test1();
+//        ScxSocketServerTest.test1();
 
 //        var webSocketClient = new HelidonHttpClient();
         var webSocketClient = new UsagiHttpClient();
