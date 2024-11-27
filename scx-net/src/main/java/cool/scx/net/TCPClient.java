a47b474b091f7f11f70a42f315cc9b09cc46e8b4
scx567888 <scx567888@outlook.com>
2024-11-27 19:35:10 +0800

    dev

--- scx-net/src/main/java/cool/scx/net/TCPClient.java
+++ scx-net/src/main/java/cool/scx/net/TCPClient.java
@@ -6,11 +6,7 @@ import java.io.UncheckedIOException;
 import java.net.Socket;
 import java.net.SocketAddress;
 
-import static java.lang.System.Logger.Level.ERROR;
-
 public class TCPClient implements ScxTCPClient {
-
-    private static final System.Logger logger = System.getLogger(TCPClient.class.getName());
 
     private final ScxTCPClientOptions options;
 
@@ -24,7 +20,6 @@ public class TCPClient implements ScxTCPClient {
 
     @Override
     public ScxTCPSocket connect(SocketAddress endpoint) {
-
         var tls = options.tls();
 
         //todo 处理代理
@@ -32,33 +27,28 @@ public class TCPClient implements ScxTCPClient {
 
         Socket socket;
         try {
-
             if (tls != null && tls.enabled()) {
                 socket = tls.createSocket();
             } else {
                 socket = new Socket();
             }
-
             socket.connect(endpoint);
-
-            //主动调用握手 快速检测 ssl 错误 防止等到调用用户处理程序时才发现 
-            if (socket instanceof SSLSocket sslSocket) {
-                try {
-                    sslSocket.startHandshake();
-                } catch (IOException e) {
-                    logger.log(ERROR, "SSL 握手失败 : " + e.getMessage());
-                    try {
-                        sslSocket.close();
-                    } catch (IOException ce) {
-                        e.addSuppressed(ce);
-                    }
-                    throw e;
-                }
-            }
-
         } catch (IOException e) {
-            logger.log(ERROR, "连接失败 : ", e);
-            throw new UncheckedIOException(e);
+            throw new UncheckedIOException("客户端连接失败 !!!", e);
+        }
+
+        // 主动调用握手 快速检测 SSL 错误 防止等到调用用户处理程序时才发现 
+        if (socket instanceof SSLSocket sslSocket) {
+            try {
+                sslSocket.startHandshake();
+            } catch (IOException e) {
+                try {
+                    sslSocket.close();
+                } catch (IOException ce) {
+                    e.addSuppressed(ce);
+                }
+                throw new UncheckedIOException("客户端 SSL 握手失败 !!!", e);
+            }
         }
 
         return new TCPSocket(socket);
