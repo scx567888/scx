98b507cd901b07aa51cf46eee522661d671b7001
scx567888 <scx567888@outlook.com>
2024-12-02 16:30:03 +0800

    dev

--- scx-jdbc/src/main/java/cool/scx/jdbc/spy/wrapper/DataSourceWrapper.java
+++ scx-jdbc/src/main/java/cool/scx/jdbc/spy/wrapper/DataSourceWrapper.java
@@ -8,6 +8,12 @@ import java.io.PrintWriter;
 import java.sql.*;
 import java.util.logging.Logger;
 
+/**
+ * DataSourceWrapper
+ *
+ * @author scx567888
+ * @version 0.0.1
+ */
 public class DataSourceWrapper extends SpyWrapper<DataSource> implements DataSource {
 
     public DataSourceWrapper(DataSource dataSource, SpyEventListener eventListener) {
