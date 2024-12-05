28f980350790f87ac817bdff7e30abccd291c67c
scx567888 <scx567888@outlook.com>
2024-12-05 21:23:52 +0800

    dev

--- scx-http-usagi/src/main/java/cool/scx/http/usagi/UsagiHttpServer.java
+++ scx-http-usagi/src/main/java/cool/scx/http/usagi/UsagiHttpServer.java
@@ -2,7 +2,6 @@ package cool.scx.http.usagi;
 
 import cool.scx.http.ScxHttpServer;
 import cool.scx.http.ScxHttpServerRequest;
-import cool.scx.http.web_socket.ScxServerWebSocket;
 import cool.scx.tcp.ScxTCPServer;
 import cool.scx.tcp.ScxTCPSocket;
 import cool.scx.tcp.TCPServer;
@@ -10,7 +9,7 @@ import cool.scx.tcp.TCPServer;
 import java.util.function.Consumer;
 
 /**
- * todo 待完成
+ * Http 服务器
  *
  * @author scx567888
  * @version 0.0.1
@@ -20,8 +19,6 @@ public class UsagiHttpServer implements ScxHttpServer {
     private final ScxTCPServer tcpServer;
     private final UsagiHttpServerOptions options;
     Consumer<ScxHttpServerRequest> requestHandler;
-    Consumer<ScxServerWebSocket> webSocketHandler;
-    private Consumer<Throwable> errorHandler;
 
     public UsagiHttpServer(UsagiHttpServerOptions options) {
         this.options = options;
@@ -33,25 +30,13 @@ public class UsagiHttpServer implements ScxHttpServer {
         this(new UsagiHttpServerOptions());
     }
 
-    private void handle(ScxTCPSocket scxTCPSocket) {
-        new Http1ConnectionHandler(scxTCPSocket, this).start();
+    private void handle(ScxTCPSocket tcpSocket) {
+        new Http1xConnectionHandler(tcpSocket, this).start();
     }
 
     @Override
-    public ScxHttpServer requestHandler(Consumer<ScxHttpServerRequest> handler) {
-        this.requestHandler = handler;
-        return this;
-    }
-
-    @Override
-    public ScxHttpServer webSocketHandler(Consumer<ScxServerWebSocket> handler) {
-        this.webSocketHandler = handler;
-        return this;
-    }
-
-    @Override
-    public ScxHttpServer errorHandler(Consumer<Throwable> handler) {
-        this.errorHandler = handler;
+    public ScxHttpServer onRequest(Consumer<ScxHttpServerRequest> requestHandler) {
+        this.requestHandler = requestHandler;
         return this;
     }
 
