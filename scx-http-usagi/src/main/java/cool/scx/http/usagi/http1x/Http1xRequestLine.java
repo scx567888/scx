3aa1119d06efc3bbedd5428353d81454358fb68f
scx567888 <scx567888@outlook.com>
2024-12-05 22:11:01 +0800

    dev

--- scx-http-usagi/src/main/java/cool/scx/http/usagi/http1x/Http1xRequestLine.java
+++ scx-http-usagi/src/main/java/cool/scx/http/usagi/http1x/Http1xRequestLine.java
@@ -6,9 +6,16 @@ import cool.scx.http.ScxHttpMethod;
 /**
  * Http 1.x 的请求行
  *
+ * @param method
+ * @param path    注意这里都是未进行 URL 编码的格式
+ * @param version
  * @author scx567888
  * @version 0.0.1
  */
 public record Http1xRequestLine(ScxHttpMethod method, String path, HttpVersion version) {
+
+    public static Http1xRequestLine of(String requestLineStr) {
+        return Http1xRequestLineHelper.parseRequestLine(requestLineStr);
+    }
 
 }
---
+++ scx-http-usagi/src/main/java/cool/scx/http/usagi/http1x/Http1xRequestLineHelper.java
@@ -0,0 +1,35 @@
+package cool.scx.http.usagi.http1x;
+
+import cool.scx.http.HttpVersion;
+import cool.scx.http.ScxHttpMethod;
+
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+public final class Http1xRequestLineHelper {
+
+    public static Http1xRequestLine parseRequestLine(String requestLineStr) {
+        var split = requestLineStr.split(" ");
+        if (split.length != 3) {
+            throw new RuntimeException("Invalid request line: " + requestLineStr);
+        }
+        var methodStr = split[0];
+        var pathStr = split[1];
+        var versionStr = split[2];
+
+        var method = ScxHttpMethod.of(methodStr);
+        var path = URLDecoder.decode(pathStr, UTF_8);
+        var version = HttpVersion.of(versionStr);
+        return new Http1xRequestLine(method, path, version);
+    }
+
+    public static String encodeRequestLine(Http1xRequestLine requestLine) {
+        var method = requestLine.method().value();
+        var path = URLEncoder.encode(requestLine.path(), UTF_8);
+        var version = requestLine.version().value();
+        return method + " " + path + " " + version;
+    }
+
+}
