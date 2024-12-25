53566dc1020dfca36ba26c9d769554d506e0460b
scx567888 <scx567888@outlook.com>
2024-12-25 16:36:55 +0800

    dev

--- scx-io/src/main/java/cool/scx/io/SkipDataPuller.java
+++ scx-io/src/main/java/cool/scx/io/SkipDataPuller.java
@@ -1,7 +1,7 @@
 package cool.scx.io;
 
 public class SkipDataPuller implements DataPuller {
-    
+
     public static final SkipDataPuller SKIP_DATA_PULLER = new SkipDataPuller();
 
     //这个类不应该被用户实例化
@@ -13,5 +13,5 @@ public class SkipDataPuller implements DataPuller {
     public PullResult pull() {
         return PullResult.BREAK;
     }
-    
+
 }
