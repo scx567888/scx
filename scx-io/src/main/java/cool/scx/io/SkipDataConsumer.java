596f791ab8396bb0645dd9a1786b3f6565f32405
scx567888 <scx567888@outlook.com>
2024-12-25 10:47:02 +0800

    dev

---
+++ scx-io/src/main/java/cool/scx/io/SkipDataConsumer.java
@@ -0,0 +1,17 @@
+package cool.scx.io;
+
+public class SkipDataConsumer implements DataConsumer {
+    
+    public static final SkipDataConsumer SKIP_DATA_CONSUMER = new SkipDataConsumer();
+    
+    //这个类不应该被用户实例化
+    private SkipDataConsumer() {
+        
+    }
+
+    @Override
+    public void accept(byte[] bytes, int position, int length) {
+        //什么都不做
+    }
+    
+}
