f959dabe4a5f36de744a071260b4527f0abeb344
scx567888 <scx567888@outlook.com>
2024-12-27 18:48:55 +0800

    dev

--- scx-io/src/main/java/cool/scx/io/OutputStreamDataConsumer.java
+++ scx-io/src/main/java/cool/scx/io/OutputStreamDataConsumer.java
@@ -7,18 +7,27 @@ import java.io.UncheckedIOException;
 public class OutputStreamDataConsumer implements DataConsumer {
 
     private final OutputStream out;
+    
+    //记录总的长度 有时候会用到
+    private long byteCount;
 
     public OutputStreamDataConsumer(OutputStream out) {
         this.out = out;
+        this.byteCount = 0;
     }
 
     @Override
     public void accept(byte[] bytes, int position, int length) {
         try {
             out.write(bytes, position, length);
+            byteCount += length;
         } catch (IOException e) {
             throw new UncheckedIOException(e);
         }
+    }
+
+    public long byteCount() {
+        return byteCount;
     }
 
 }
