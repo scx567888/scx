4ddd41291823990023d934352f239e01d704a9a7
scx567888 <scx567888@outlook.com>
2025-01-03 10:55:25 +0800

    dev

--- scx-tcp/src/main/java/cool/scx/tcp/tls/TLSSocketChannel.java
+++ scx-tcp/src/main/java/cool/scx/tcp/tls/TLSSocketChannel.java
@@ -248,16 +248,17 @@ public class TLSSocketChannel extends AbstractSocketChannel {
                         if (unwrapResult.bytesProduced > 0) {
                             break _UW;
                         }
-                        int remaining = inboundNetData.remaining();
-                        // 2, 如果之前没有解密成功任何数据 可能是容量太小了 这里扩容
-                        if (remaining == 0) {
+                        //计算剩余 "可写的长度"
+                        var w = inboundNetData.capacity() - inboundNetData.remaining();
+                        // 2, 如果没有可写入的空间，则扩容
+                        if (w == 0) {
                             var newInboundNetData = ByteBuffer.allocate(inboundNetData.capacity() * 2);
                             // 原有的已经读取的数据别忘了放进去
                             newInboundNetData.put(inboundNetData);// 这里 netBuffer 已经是读状态 不需要 flip()
                             inboundNetData = newInboundNetData;
                         } else {
                             //3, 有剩余表示 只是上一次读取的少了 手动调整缓冲区：将未处理数据移动到缓冲区起始位置 然后重新读取
-                            inboundNetData.position(remaining);
+                            inboundNetData.position(inboundNetData.remaining());
                             // 设置位置到剩余数据的末尾 
                             inboundNetData.limit(inboundNetData.capacity());
                         }
