32b7374d5d4fece766877d8b961855728efeadfe
scx567888 <scx567888@outlook.com>
2025-01-07 11:28:13 +0800

    dev

---
+++ scx-tcp/src/main/java/cool/scx/tcp/ClassicTLSConfig.java
@@ -0,0 +1,20 @@
+package cool.scx.tcp;
+
+import javax.net.ssl.SSLSocket;
+import java.util.List;
+import java.util.function.BiFunction;
+
+public class ClassicTLSConfig implements ScxTLSConfig {
+
+    private final SSLSocket sslSocket;
+
+    public ClassicTLSConfig(SSLSocket sslSocket) {
+        this.sslSocket = sslSocket;
+    }
+
+    @Override
+    public void setHandshakeApplicationProtocolSelector(BiFunction<ScxTLSConfig, List<String>, String> selector) {
+        sslSocket.setHandshakeApplicationProtocolSelector((sslEngine, list) -> selector.apply(this, list));
+    }
+
+}
---
+++ scx-tcp/src/main/java/cool/scx/tcp/NioTLSConfig.java
@@ -0,0 +1,20 @@
+package cool.scx.tcp;
+
+import javax.net.ssl.SSLEngine;
+import java.util.List;
+import java.util.function.BiFunction;
+
+public class NioTLSConfig implements ScxTLSConfig {
+
+    private final SSLEngine sslEngine;
+
+    public NioTLSConfig(SSLEngine sslEngine) {
+        this.sslEngine = sslEngine;
+    }
+
+    @Override
+    public void setHandshakeApplicationProtocolSelector(BiFunction<ScxTLSConfig, List<String>, String> selector) {
+        sslEngine.setHandshakeApplicationProtocolSelector((sslEngine, list) -> selector.apply(this, list));
+    }
+
+}
