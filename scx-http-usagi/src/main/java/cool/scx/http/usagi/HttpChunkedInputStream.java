bbfbc91508007ed43e9ab47286749a796565f2f9
scx567888 <scx567888@outlook.com>
2025-01-08 13:36:27 +0800

    dev

--- scx-http-usagi/src/main/java/cool/scx/http/usagi/HttpChunkedInputStream.java
+++ scx-http-usagi/src/main/java/cool/scx/http/usagi/HttpChunkedInputStream.java
@@ -1,42 +1,39 @@
 package cool.scx.http.usagi;
 
 import cool.scx.io.LinkedDataReader;
-import cool.scx.io.NoMoreDataException;
+import cool.scx.io.PowerfulLinkedDataReader;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 
 /**
- * todo 待完成
+ * HttpChunkedInputStream
  *
  * @author scx567888
  * @version 0.0.1
  */
 public class HttpChunkedInputStream extends InputStream {
 
-    private final LinkedDataReader chunkedDataReader;
+    private final PowerfulLinkedDataReader chunkedDataReader;
 
     public HttpChunkedInputStream(LinkedDataReader dataReader) {
-        this.chunkedDataReader = new LinkedDataReader(new HttpChunkedDataSupplier(dataReader));
+        this.chunkedDataReader = new PowerfulLinkedDataReader(new HttpChunkedDataSupplier(dataReader));
     }
 
     @Override
     public int read() {
-        try {
-            return chunkedDataReader.read() & 0xFF;
-        } catch (NoMoreDataException e) {
-            return -1;
-        }
+        return chunkedDataReader.inputStreamRead();
     }
 
     @Override
     public int read(byte[] b, int off, int len) {
-        try {
-            var data = chunkedDataReader.read(len);
-            System.arraycopy(data, 0, b, off, data.length);
-            return data.length;
-        } catch (NoMoreDataException e) {
-            return -1;
-        }
+        return chunkedDataReader.inputStreamRead(b, off, len);
+    }
+
+    @Override
+    public long transferTo(OutputStream out) throws IOException {
+        return chunkedDataReader.inputStreamTransferTo(out);
     }
 
 }
