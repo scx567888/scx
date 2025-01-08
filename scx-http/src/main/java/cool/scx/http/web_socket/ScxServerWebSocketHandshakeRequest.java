04addfdc484e9a98a1dddd2d2497d808876acc65
scx567888 <scx567888@outlook.com>
2025-01-08 16:04:46 +0800

    dev

--- scx-http/src/main/java/cool/scx/http/web_socket/ScxServerWebSocketHandshakeRequest.java
+++ scx-http/src/main/java/cool/scx/http/web_socket/ScxServerWebSocketHandshakeRequest.java
@@ -21,14 +21,7 @@ public interface ScxServerWebSocketHandshakeRequest extends ScxHttpServerRequest
         return getHeader(SEC_WEBSOCKET_VERSION);
     }
 
-    default void acceptHandshake() {
-        // 实现握手接受逻辑，返回适当的响应头
-        var response = response();
-        response.setHeader(UPGRADE, "websocket");
-        response.setHeader(CONNECTION, "Upgrade");
-        response.setHeader(SEC_WEBSOCKET_ACCEPT, generateSecWebSocketAccept(secWebSocketKey()));
-        response.status(101).send();
-    }
+    ScxServerWebSocket acceptHandshake();
 
     ScxServerWebSocket webSocket();
 
