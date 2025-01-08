c9598035a6c63b4051e598b166d2c9f30d1f2866
scx567888 <scx567888@outlook.com>
2025-01-08 14:46:36 +0800

    dev

--- scx-http-usagi/src/main/java/cool/scx/http/usagi/http1x/Http1xConnection.java
+++ scx-http-usagi/src/main/java/cool/scx/http/usagi/http1x/Http1xConnection.java
@@ -3,14 +3,9 @@ package cool.scx.http.usagi.http1x;
 import cool.scx.http.*;
 import cool.scx.http.exception.InternalServerErrorException;
 import cool.scx.http.exception.ScxHttpException;
-import cool.scx.http.usagi.FixedLengthInputStream;
-import cool.scx.http.usagi.HttpChunkedInputStream;
 import cool.scx.http.usagi.UsagiHttpServerOptions;
 import cool.scx.http.usagi.exception.CloseConnectionException;
-import cool.scx.io.InputStreamDataSupplier;
-import cool.scx.io.LinkedDataReader;
-import cool.scx.io.NoMatchFoundException;
-import cool.scx.io.NoMoreDataException;
+import cool.scx.io.*;
 import cool.scx.tcp.ScxTCPSocket;
 
 import java.io.IOException;
@@ -31,27 +26,25 @@ public class Http1xConnection {
 
     private final static System.Logger LOGGER = System.getLogger(Http1xConnection.class.getName());
 
-    private static final byte[] CRLF_BYTES = "\r\n".getBytes();
-    private static final byte[] CRLF_CRLF_BYTES = "\r\n\r\n".getBytes();
-
     private final ScxTCPSocket tcpSocket;
     private final UsagiHttpServerOptions options;
     private final Consumer<ScxHttpServerRequest> requestHandler;
-    private final LinkedDataReader dataReader;
+    private final PowerfulLinkedDataReader dataReader;
     private final OutputStream dataWriter;
+    private boolean running;
 
     public Http1xConnection(ScxTCPSocket tcpSocket, UsagiHttpServerOptions options, Consumer<ScxHttpServerRequest> requestHandler) {
         this.tcpSocket = tcpSocket;
         this.options = options;
         this.requestHandler = requestHandler;
-        this.dataReader = new LinkedDataReader(new InputStreamDataSupplier(this.tcpSocket.inputStream()));
+        this.dataReader = new PowerfulLinkedDataReader(new InputStreamDataSupplier(this.tcpSocket.inputStream()));
         this.dataWriter = this.tcpSocket.outputStream();
+        this.running = true;
     }
 
 
     public void start() {
-        System.out.println("新连接");
-        while (true) {
+        while (running) {
             try {
                 // 1, 读取 请求行
                 var requestLine = readRequestLine();
@@ -69,8 +62,8 @@ public class Http1xConnection {
                 var isWebSocketHandshake = checkIsWebSocketHandshake(requestLine, headers);
 
                 var request = isWebSocketHandshake ?
-                        new Http1xServerWebSocketHandshakeRequest(requestLine, headers, body, tcpSocket, dataReader, dataWriter, isKeepAlive) :
-                        new Http1xServerRequest(requestLine, headers, body, tcpSocket, dataReader, dataWriter, isKeepAlive);
+                        new Http1xServerWebSocketHandshakeRequest(this, requestLine, headers, body, tcpSocket, dataReader, dataWriter, isKeepAlive) :
+                        new Http1xServerRequest(this, requestLine, headers, body, tcpSocket, dataReader, dataWriter, isKeepAlive);
 
                 // 5, 调用用户处理器
                 _callRequestHandler(request);
@@ -92,6 +85,10 @@ public class Http1xConnection {
         }
     }
 
+    public void stop() {
+        running = false;
+    }
+
     private void _callRequestHandler(Http1xServerRequest request) {
         if (requestHandler != null) {
             requestHandler.accept(request);
@@ -99,6 +96,7 @@ public class Http1xConnection {
     }
 
     private Http1xRequestLine readRequestLine() {
+        //尝试读取 请求行
         try {
             var requestLineBytes = dataReader.readUntil(CRLF_BYTES, options.maxRequestLineSize());
             var requestLineStr = new String(requestLineBytes);
@@ -116,6 +114,7 @@ public class Http1xConnection {
     }
 
     private ScxHttpHeadersWritable readHeaders() {
+        //尝试读取 headers
         try {
             var headerBytes = dataReader.readUntil(CRLF_CRLF_BYTES, options.maxHeaderSize());
             var headerStr = new String(headerBytes);
@@ -133,24 +132,26 @@ public class Http1xConnection {
     }
 
     private ScxHttpBody readBody(ScxHttpHeaders headers) {
+        // http1.1 本质上只有两种请求体格式 1, 分块传输 2, 指定长度 (当然也可以没有长度 那就表示没有请求体)
+
         //1, 判断请求体是不是分块传输
         var isChunkedTransfer = checkIsChunkedTransfer(headers);
         if (isChunkedTransfer) {
-            return new ScxHttpBodyImpl(new HttpChunkedInputStream(dataReader), headers, 65535);
+            return new ScxHttpBodyImpl(new DataReaderInputStream(new HttpChunkedDataSupplier(dataReader, options.maxPayloadSize())), headers, 65535);
         }
 
         //2, 判断请求体是不是有 长度
         var contentLength = headers.contentLength();
         if (contentLength != null) {
-            return new ScxHttpBodyImpl(new FixedLengthInputStream(dataReader, contentLength), headers, 65536);
+            // 请求体长度过大 这里抛出异常
+            if (contentLength > options.maxPayloadSize()) {
+                throw new ScxHttpException(HttpStatusCode.CONTENT_TOO_LARGE);
+            }
+            return new ScxHttpBodyImpl(new FixedLengthDataReaderInputStream(dataReader, contentLength), headers, 65536);
         }
 
         //3, 没有长度的空请求体
         return new ScxHttpBodyImpl(InputStream.nullInputStream(), headers, 65536);
-    }
-
-    public void stop() {
-        //todo 
     }
 
     private void handleHttpException(ScxHttpException e) {
