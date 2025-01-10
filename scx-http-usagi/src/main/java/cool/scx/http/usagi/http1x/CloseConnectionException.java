0f6a8e775e31e4bb17535231094d348abe551c5c
scx567888 <scx567888@outlook.com>
2025-01-10 17:20:17 +0800

    dev

---
+++ scx-http-usagi/src/main/java/cool/scx/http/usagi/http1x/CloseConnectionException.java
@@ -0,0 +1,20 @@
+package cool.scx.http.usagi.http1x;
+
+/**
+ * Socket 关闭异常 当捕获到此异常的时候 应该直接断开连接 (仅内部使用)
+ */
+public class CloseConnectionException extends RuntimeException {
+
+    public CloseConnectionException() {
+
+    }
+
+    public CloseConnectionException(String message) {
+        super(message);
+    }
+
+    public CloseConnectionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
