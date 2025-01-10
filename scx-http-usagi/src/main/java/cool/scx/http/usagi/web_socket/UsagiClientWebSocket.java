8414a1e097b1eda6b4254647475cdb5fefd90c6d
scx567888 <scx567888@outlook.com>
2025-01-10 13:47:26 +0800

    dev

--- scx-http-usagi/src/main/java/cool/scx/http/usagi/web_socket/UsagiClientWebSocket.java
+++ scx-http-usagi/src/main/java/cool/scx/http/usagi/web_socket/UsagiClientWebSocket.java
@@ -2,7 +2,6 @@ package cool.scx.http.usagi.web_socket;
 
 import cool.scx.common.util.RandomUtils;
 import cool.scx.http.web_socket.ScxClientWebSocket;
-import cool.scx.http.web_socket.WebSocketFrame;
 import cool.scx.http.web_socket.WebSocketOpCode;
 import cool.scx.io.DataReader;
 import cool.scx.tcp.ScxTCPSocket;
@@ -10,24 +9,25 @@ import cool.scx.tcp.ScxTCPSocket;
 import java.io.IOException;
 import java.io.OutputStream;
 
-import static cool.scx.http.web_socket.WebSocketFrameHelper.writeFrame;
+import static cool.scx.http.usagi.web_socket.WebSocketFrameHelper.writeFrame;
 
 /**
- * todo 待完成
+ * UsagiClientWebSocket
  *
  * @author scx567888
  * @version 0.0.1
  */
 public class UsagiClientWebSocket extends UsagiWebSocket implements ScxClientWebSocket {
 
-    public UsagiClientWebSocket(ScxTCPSocket connect, DataReader reader, OutputStream writer) {
-        super(connect, reader, writer);
+    public UsagiClientWebSocket(ScxTCPSocket connect, DataReader reader, OutputStream writer, UsagiWebSocketOptions webSocketOptions) {
+        super(connect, reader, writer, webSocketOptions);
     }
 
     @Override
-    public void sendFrame(byte[] payload, WebSocketOpCode opcode, boolean last) throws IOException {
+    public void sendFrame(WebSocketOpCode opcode, byte[] payload, boolean last) throws IOException {
         lock.lock();
         try {
+            // 和服务器端不同, 客户端的是需要发送掩码的
             var maskingKey = RandomUtils.randomBytes(4);
             var f = WebSocketFrame.of(last, opcode, maskingKey, payload);
             writeFrame(f, writer);
