a071b1da522417d725dc022eba1cc72e17df2040
scx567888 <scx567888@outlook.com>
2025-01-10 13:16:01 +0800

    dev

---
+++ scx-http-usagi/src/main/java/cool/scx/http/usagi/web_socket/WebSocketFrameTooBigException.java
@@ -0,0 +1,10 @@
+package cool.scx.http.usagi.web_socket;
+
+//WebSocket 帧 太大异常
+class WebSocketFrameTooBigException extends RuntimeException {
+
+    public WebSocketFrameTooBigException() {
+        
+    }
+
+}
