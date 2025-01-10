f8e8b668b34cd81a8c4d391fb504ae79d60aa286
scx567888 <scx567888@outlook.com>
2025-01-10 13:32:15 +0800

    dev

--- scx-http-usagi/src/main/java/cool/scx/http/usagi/UsagiHttpServerOptions.java
+++ scx-http-usagi/src/main/java/cool/scx/http/usagi/UsagiHttpServerOptions.java
@@ -1,5 +1,7 @@
 package cool.scx.http.usagi;
 
+import cool.scx.http.usagi.http1x.Http1xConnectionOptions;
+import cool.scx.http.usagi.web_socket.UsagiWebSocketOptions;
 import cool.scx.tcp.ScxTCPServerOptions;
 import cool.scx.tcp.tls.TLS;
 
@@ -14,47 +16,40 @@ import java.net.InetSocketAddress;
 public class UsagiHttpServerOptions {
 
     private final ScxTCPServerOptions tcpServerOptions; // TCP 服务器配置
+    private final Http1xConnectionOptions http1xConnectionOptions;// Http1 配置
+    private final UsagiWebSocketOptions webSocketOptions;// WebSocket 配置
     private TCPServerType tcpServerType; // TCP 服务器类型
-
-    private int maxRequestLineSize;// 最大请求行大小
-    private int maxHeaderSize;// 最大请求头大小
-    private int maxPayloadSize;// 最大请求体大小
-
-    private boolean mergeWebSocketFrame;//是否合并 WebSocket 帧
-    private int maxWebSocketFrameSize; // 最大单个 WebSocket 帧长度
-    private int maxWebSocketMessageSize;// 最大 WebSocket 消息长度 (可能由多个帧合并)
-
     private boolean enableHttp2; // 是否开启 Http2
 
     public UsagiHttpServerOptions() {
         //默认不自动握手
         this.tcpServerOptions = new ScxTCPServerOptions().autoUpgradeToTLS(true).autoHandshake(false);
+        this.http1xConnectionOptions = new Http1xConnectionOptions();
+        this.webSocketOptions = new UsagiWebSocketOptions();
         this.tcpServerType = TCPServerType.CLASSIC; // 默认 使用 CLASSIC 实现
-        this.maxRequestLineSize = 1024 * 64; // 默认 64 KB
-        this.maxHeaderSize = 1024 * 128; // 默认 128 KB
-        this.maxPayloadSize = 1024 * 1024 * 16; // 默认 16 MB
-        this.mergeWebSocketFrame = true; // 默认 合并 websocket 帧
-        this.maxWebSocketFrameSize = 1024 * 1024 * 16; // 默认 16 MB
-        this.maxWebSocketMessageSize = 1024 * 1024 * 16; // 默认 16 MB
         this.enableHttp2 = false;//默认不启用 http2
     }
 
     public UsagiHttpServerOptions(UsagiHttpServerOptions oldOptions) {
         //默认不自动握手
         this.tcpServerOptions = new ScxTCPServerOptions(oldOptions.tcpServerOptions()).autoUpgradeToTLS(true).autoHandshake(false);
+        this.http1xConnectionOptions = new Http1xConnectionOptions(oldOptions.http1xConnectionOptions());
+        this.webSocketOptions = new UsagiWebSocketOptions(oldOptions.webSocketOptions());
         tcpServerType(oldOptions.tcpServerType());
-        maxRequestLineSize(oldOptions.maxRequestLineSize());
-        maxHeaderSize(oldOptions.maxHeaderSize());
-        maxPayloadSize(oldOptions.maxPayloadSize());
-        mergeWebSocketFrame(oldOptions.mergeWebSocketFrame());
-        maxWebSocketFrameSize(oldOptions.maxWebSocketFrameSize());
-        maxWebSocketMessageSize(oldOptions.maxWebSocketMessageSize());
         enableHttp2(oldOptions.enableHttp2());
     }
 
-    //不允许外界访问
+    //因为涉及到一些底层实现, 所以不允许外界访问
     ScxTCPServerOptions tcpServerOptions() {
         return tcpServerOptions;
+    }
+
+    public Http1xConnectionOptions http1xConnectionOptions() {
+        return http1xConnectionOptions;
+    }
+
+    public UsagiWebSocketOptions webSocketOptions() {
+        return webSocketOptions;
     }
 
     public TCPServerType tcpServerType() {
@@ -67,57 +62,56 @@ public class UsagiHttpServerOptions {
     }
 
     public int maxRequestLineSize() {
-        return maxRequestLineSize;
+        return http1xConnectionOptions.maxRequestLineSize();
     }
 
     public UsagiHttpServerOptions maxRequestLineSize(int maxRequestLineSize) {
-        this.maxRequestLineSize = maxRequestLineSize;
+        http1xConnectionOptions.maxRequestLineSize(maxRequestLineSize);
         return this;
     }
 
     public int maxHeaderSize() {
-        return maxHeaderSize;
+        return http1xConnectionOptions.maxHeaderSize();
     }
 
     public UsagiHttpServerOptions maxHeaderSize(int maxHeaderSize) {
-        this.maxHeaderSize = maxHeaderSize;
+        http1xConnectionOptions.maxHeaderSize(maxHeaderSize);
         return this;
     }
 
     public int maxPayloadSize() {
-        return maxPayloadSize;
+        return http1xConnectionOptions.maxPayloadSize();
     }
 
     public UsagiHttpServerOptions maxPayloadSize(int maxPayloadSize) {
-        this.maxPayloadSize = maxPayloadSize;
+        http1xConnectionOptions.maxPayloadSize(maxPayloadSize);
         return this;
     }
 
-
     public boolean mergeWebSocketFrame() {
-        return mergeWebSocketFrame;
+        return webSocketOptions.mergeWebSocketFrame();
     }
 
     public UsagiHttpServerOptions mergeWebSocketFrame(boolean mergeWebSocketFrame) {
-        this.mergeWebSocketFrame = mergeWebSocketFrame;
+        webSocketOptions.mergeWebSocketFrame(mergeWebSocketFrame);
         return this;
     }
 
     public int maxWebSocketFrameSize() {
-        return maxWebSocketFrameSize;
+        return webSocketOptions.maxWebSocketFrameSize();
     }
 
     public UsagiHttpServerOptions maxWebSocketFrameSize(int maxWebSocketFrameSize) {
-        this.maxWebSocketFrameSize = maxWebSocketFrameSize;
+        webSocketOptions.maxWebSocketFrameSize(maxWebSocketFrameSize);
         return this;
     }
 
     public int maxWebSocketMessageSize() {
-        return maxWebSocketMessageSize;
+        return webSocketOptions.maxWebSocketMessageSize();
     }
 
     public UsagiHttpServerOptions maxWebSocketMessageSize(int maxWebSocketMessageSize) {
-        this.maxWebSocketMessageSize = maxWebSocketMessageSize;
+        webSocketOptions.maxWebSocketMessageSize(maxWebSocketMessageSize);
         return this;
     }
 
