0bf05da8f1d189f51422c7d81b986ffd57ab499c
scx567888 <scx567888@outlook.com>
2025-01-11 13:46:42 +0800

    dev

---
+++ scx-http-usagi/src/main/java/cool/scx/http/usagi/http1x/Http1xStatusLineHelper.java
@@ -0,0 +1,24 @@
+package cool.scx.http.usagi.http1x;
+
+import cool.scx.http.HttpVersion;
+
+public class Http1xStatusLineHelper {
+
+    public static Http1xStatusLine parseStatusLine(String statusLineStr) {
+        var parts = statusLineStr.split(" ", 3);
+
+        if (parts.length != 3) {
+            throw new RuntimeException("Invalid status line: " + statusLineStr);
+        }
+    
+        var versionStr = parts[0];
+        var codeStr = parts[1];
+        var reasonStr = parts[2];
+
+        var version = HttpVersion.of(versionStr);
+        var code = Integer.parseInt(codeStr);
+
+        return new Http1xStatusLine(version, code, reasonStr);
+    }
+
+}
