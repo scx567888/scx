4fa6e697063f354d53a68587922dd1e068bd61ea
scx567888 <scx567888@outlook.com>
2025-01-22 11:58:47 +0800

    dev

--- scx-http/src/main/java/cool/scx/http/ScxHttpServer.java
+++ scx-http/src/main/java/cool/scx/http/ScxHttpServer.java
@@ -15,9 +15,6 @@ public interface ScxHttpServer {
 
     ScxHttpServer onRequest(Consumer<ScxHttpServerRequest> requestHandler);
 
-    //todo 这个 api 设计的不合理 因为 此种方式相当于默认允许所有握手请求 但是目前为了兼容暂时用这种方式 (应该移除只保留 onRequest)
-    ScxHttpServer onWebSocket(Consumer<ScxServerWebSocket> handler);
-
     void start();
 
     void stop();
