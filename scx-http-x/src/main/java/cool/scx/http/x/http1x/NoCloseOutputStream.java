17d0043a7226afa3d7a148656d0be6bff496fefd
scx567888 <scx567888@outlook.com>
2025-01-22 16:26:21 +0800

    dev

--- scx-http-x/src/main/java/cool/scx/http/x/http1x/NoCloseOutputStream.java
+++ scx-http-x/src/main/java/cool/scx/http/x/http1x/NoCloseOutputStream.java
@@ -1,6 +1,7 @@
 package cool.scx.http.x.http1x;
 
 import java.io.FilterOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
 
 /**
@@ -9,15 +10,38 @@ import java.io.OutputStream;
  * @author scx567888
  * @version 0.0.1
  */
-public class NoCloseOutputStream extends FilterOutputStream {
+public class NoCloseOutputStream extends OutputStream {
+
+    private final OutputStream out;
 
     public NoCloseOutputStream(OutputStream out) {
-        super(out);
+        this.out=out;
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        this.out.write(b);
+    }
+
+    @Override
+    public void write(byte[] b) throws IOException {
+        this.out.write(b);
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        this.out.write(b, off, len);
+    }
+
+    @Override
+    public void flush() throws IOException {
+        this.out.flush();
     }
 
     @Override
     public void close() {
         //这里什么也不做
+        System.out.println();
     }
 
 }
