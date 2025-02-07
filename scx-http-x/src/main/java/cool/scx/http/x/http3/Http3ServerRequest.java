6ef18cdbba880b037faed17ae5c33d412601d87b
scx567888 <scx567888@outlook.com>
2025-02-07 11:37:53 +0800

    dev

--- scx-http-x/src/main/java/cool/scx/http/x/http3/Http3ServerRequest.java
+++ scx-http-x/src/main/java/cool/scx/http/x/http3/Http3ServerRequest.java
@@ -1,6 +1,49 @@
 package cool.scx.http.x.http3;
 
-//todo 待完成
-public class Http3ServerRequest {
+import cool.scx.http.*;
+import cool.scx.http.uri.ScxURI;
 
+//todo 待完成
+public class Http3ServerRequest implements ScxHttpServerRequest {
+
+    @Override
+    public ScxHttpServerResponse response() {
+        return null;
+    }
+
+    @Override
+    public ScxHttpMethod method() {
+        return null;
+    }
+
+    @Override
+    public ScxURI uri() {
+        return null;
+    }
+
+    @Override
+    public HttpVersion version() {
+        return null;
+    }
+
+    @Override
+    public ScxHttpHeaders headers() {
+        return null;
+    }
+
+    @Override
+    public ScxHttpBody body() {
+        return null;
+    }
+
+    @Override
+    public PeerInfo remotePeer() {
+        return null;
+    }
+
+    @Override
+    public PeerInfo localPeer() {
+        return null;
+    }
+    
 }
