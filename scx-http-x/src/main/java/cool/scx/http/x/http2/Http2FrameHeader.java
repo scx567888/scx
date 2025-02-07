689eb41ddfa18f67d121cfc8139409eef9b5ccf1
scx567888 <scx567888@outlook.com>
2025-02-07 12:03:18 +0800

    dev

--- scx-http-x/src/main/java/cool/scx/http/x/http2/Http2FrameHeader.java
+++ scx-http-x/src/main/java/cool/scx/http/x/http2/Http2FrameHeader.java
@@ -2,4 +2,8 @@ package cool.scx.http.x.http2;
 
 public record Http2FrameHeader(int length, Http2FrameType type, byte flags, int streamId) {
 
+    public static Http2FrameHeader of(byte[] data) {
+        return Http2FrameHeaderHelper.parseHttp2FrameHeader(data);
+    }
+    
 }
