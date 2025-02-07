f6240888ee60c24714fa5723e47aaa8db37b1802
scx567888 <scx567888@outlook.com>
2025-02-07 18:17:43 +0800

    dev

--- scx-http-x/src/main/java/cool/scx/http/x/http2/huffman/HuffmanHelper.java
+++ scx-http-x/src/main/java/cool/scx/http/x/http2/huffman/HuffmanHelper.java
@@ -5,6 +5,8 @@ import cool.scx.common.count_map.ICountMap;
 import cool.scx.common.util.$;
 
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.PriorityQueue;
 
 public class HuffmanHelper {
@@ -38,5 +40,61 @@ public class HuffmanHelper {
         // 队列中最后一个节点就是霍夫曼树的根节点
         return queue.poll();
     }
-    
+
+    public static <T> HashMap<T, String> buildHuffmanCodeTable(HuffmanNode<T> node) {
+        var huffmanCode = new HashMap<T, String>();
+        buildHuffmanCodeTable0(node, "", huffmanCode);
+        return huffmanCode;
+    }
+
+    public static <T> void buildHuffmanCodeTable0(HuffmanNode<T> node, String path, Map<T, String> huffmanCode) {
+        if (node.isLeaf()) {
+            huffmanCode.put(node.value, path);
+            return;
+        }
+        buildHuffmanCodeTable0(node.left, path + "0", huffmanCode);
+        buildHuffmanCodeTable0(node.right, path + "1", huffmanCode);
+    }
+
+    public static <T> HuffmanNode<T> buildHuffmanTreeFromCode(Map<T, String> huffmanCode) {
+        // 初始化根节点
+        var root = new HuffmanNode<T>(null, 0);
+
+        // 遍历编码表
+        for (var entry : huffmanCode.entrySet()) {
+            T symbol = entry.getKey();
+            String code = entry.getValue();
+
+            // 从根节点开始构建
+            HuffmanNode<T> current = root;
+
+            for (char c : code.toCharArray()) {
+                switch (c) {
+                    case '0' -> {
+                        // 如果当前位是 '0'，进入或创建左子节点
+                        if (current.left == null) {
+                            current.left = new HuffmanNode<>(null, 0);
+                        }
+                        current = current.left;
+                    }
+                    case '1' -> {
+                        // 如果当前位是 '1'，进入或创建右子节点
+                        if (current.right == null) {
+                            current.right = new HuffmanNode<>(null, 0);
+                        }
+                        current = current.right;
+                    }
+                    default -> {
+                        //忽略其他的 字符 比如为了方便维护添加的 分隔符
+                    }
+                }
+            }
+
+            // 到达编码末尾，设置叶子节点的值
+            current.value = symbol;
+        }
+
+        return root;
+    }
+
 }
