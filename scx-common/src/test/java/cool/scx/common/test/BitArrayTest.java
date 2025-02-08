acaa9409e74f9db0b779928e5d34efb309eaac60
scx567888 <scx567888@outlook.com>
2025-02-08 13:40:41 +0800

    dev

--- scx-common/src/test/java/cool/scx/common/test/BitArrayTest.java
+++ scx-common/src/test/java/cool/scx/common/test/BitArrayTest.java
@@ -10,21 +10,11 @@ public class BitArrayTest {
     }
 
     public static void test1() {
-        var ba = new BitArray();
-        ba.set(0, true);
-        ba.set(1, true);
-        ba.set(2, true);
-        ba.set(3, true);
-        var ba1 = new BitArray();
-        ba1.set(0, false);
-        ba1.set(1, false);
-        ba1.set(2, false);
-        ba1.set(3, false);
-        var ba2 = new CombinedBitArray(ba, ba1);
-        for (int i = 0; i < 100000; i++) {
-            byte[] bytes = ba2.toBytes();
-        }
-        System.out.println();
+        var a = new BitArray("abc".getBytes());
+        var b = new BitArray("def".getBytes());
+        var c = new CombinedBitArray(a, b);
+        String s = new String(c.toBytes());
+        System.out.println(s);
     }
 
 }
