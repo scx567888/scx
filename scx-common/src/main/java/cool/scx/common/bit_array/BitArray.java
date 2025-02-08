9a8b7e10822e466520194b1d02c5ef1d788d056b
scx567888 <scx567888@outlook.com>
2025-02-08 13:34:48 +0800

    dev

--- scx-common/src/main/java/cool/scx/common/bit_array/BitArray.java
+++ scx-common/src/main/java/cool/scx/common/bit_array/BitArray.java
@@ -2,8 +2,6 @@ package cool.scx.common.bit_array;
 
 import java.util.Arrays;
 
-import static cool.scx.common.bit_array.BitArrayHelper.setByBinaryString;
-
 public class BitArray implements IBitArray {
 
     // 掩码静态查找表
@@ -18,6 +16,13 @@ public class BitArray implements IBitArray {
 
     public BitArray() {
         this(new byte[]{});
+    }
+
+    public BitArray(int length) {
+        int byteLength = byteLength(length); // 计算所需字节数
+        this.data = new byte[byteLength];
+        this.capacity = byteLength << 3;
+        this.length = length;
     }
 
     public BitArray(byte[] data) {
@@ -63,7 +68,7 @@ public class BitArray implements IBitArray {
     @Override
     public byte[] toBytes() {
         // 计算实际需要的字节数
-        int actualByteLength = (length + 7) >> 3; // 向上取整
+        int actualByteLength = byteLength(length); // 向上取整
         // 创建一个新数组，仅复制有效数据部分
         return Arrays.copyOf(data, actualByteLength);
     }
@@ -79,7 +84,7 @@ public class BitArray implements IBitArray {
 
     private void ensureCapacity(int index) {
         if (index >= capacity) {
-            int newByteSize = Math.max((index + 8) >> 3, data.length + (data.length >> 1));
+            int newByteSize = Math.max((index + 8) >> 3, data.length + (data.length >> 1));// 1.5倍 扩容
             data = Arrays.copyOf(data, newByteSize);
             capacity = newByteSize << 3;
         }
@@ -91,12 +96,36 @@ public class BitArray implements IBitArray {
         }
     }
 
-    private int byteIndex(int index) {
+    private static int byteIndex(int index) {
         return index >> 3;
     }
 
-    private int bitIndex(int index) {
+    private static int bitIndex(int index) {
         return index & 7;
+    }
+
+    private static int byteLength(int bitLength) {
+        return (bitLength + 7) >> 3; // 向上取整，计算最小字节数
+    }
+
+    private static void setByBinaryString(BitArray bitArray, String binaryString) {
+        int bitIndex = 0; // 当前 BitSet 的索引
+        // 遍历字符串
+        for (var c : binaryString.toCharArray()) {
+            switch (c) {
+                case '1' -> {
+                    bitArray.set(bitIndex, true); // 仅当字符是 '1' 时设置为 true
+                    bitIndex++;
+                }
+                case '0' -> {
+                    bitArray.set(bitIndex, false);
+                    bitIndex++;
+                } // 仅当字符是 '1' 时设置为 true
+                default -> {
+                    // 其他字符（分隔符）直接跳过        
+                }
+            }
+        }
     }
 
 }
