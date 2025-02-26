931e9ee9e8f391eeca9383d9d2ac98f2d8b07a98
scx567888 <scx567888@outlook.com>
2025-02-26 22:17:05 +0800

    dev

--- scx-reflect/src/main/java/cool/scx/reflect/FieldInfo.java
+++ scx-reflect/src/main/java/cool/scx/reflect/FieldInfo.java
@@ -5,91 +5,78 @@ import com.fasterxml.jackson.databind.JavaType;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 
-import static cool.scx.reflect.FieldInfoHelper.*;
-import static cool.scx.reflect.ReflectHelper._findAccessModifier;
+import static cool.scx.reflect.Helper._findAccessModifier;
+import static cool.scx.reflect.Helper._findType;
 
 
 /// FieldInfo
 ///
 /// @author scx567888
 /// @version 0.0.1
-final class FieldInfo implements IFieldInfo {
+final class FieldInfo implements MemberInfo {
 
     private final Field field;
     private final ClassInfo classInfo;
-    private final String name;
     private final AccessModifier accessModifier;
+    private final String name;
     private final JavaType type;
     private final Annotation[] annotations;
 
     FieldInfo(Field field, ClassInfo classInfo) {
         this.field = field;
         this.classInfo = classInfo;
-        var accessFlags = field.accessFlags();
-        this.name = _findName(field);
-        this.accessModifier = _findAccessModifier(accessFlags);
-        this.type = _findType(this);
-        this.annotations = _findAnnotations(field);
+        this.accessModifier = _findAccessModifier(field.accessFlags());
+        this.name = field.getName();
+        this.type = _findType(field.getGenericType(), classInfo);
+        this.annotations = field.getDeclaredAnnotations();
     }
 
-    @Override
     public Field field() {
-        return this.field;
+        return field;
     }
 
-    @Override
-    public ClassInfo classInfo() {
-        return this.classInfo;
-    }
-
-    @Override
     public String name() {
         return name;
     }
 
-    @Override
-    public AccessModifier accessModifier() {
-        return this.accessModifier;
-    }
-
-    @Override
     public JavaType type() {
-        return this.type;
+        return type;
     }
 
-    @Override
     public Annotation[] annotations() {
         return this.annotations;
     }
 
-    @Override
     public <T extends Annotation> T getAnnotation(Class<T> annotationClass) {
-        return this.field.getAnnotation(annotationClass);
+        for (var annotation : this.annotations) {
+            if (annotationClass.isInstance(annotation)) {
+                return annotationClass.cast(annotation);
+            }
+        }
+        return null;
+    }
+
+    public void set(Object obj, Object value) throws IllegalAccessException {
+        field.set(obj, value);
+    }
+
+    public Object get(Object obj) throws IllegalAccessException {
+        return field.get(obj);
     }
 
     @Override
-    public Annotation[] getAnnotations() {
-        return this.field.getAnnotations();
+    public ClassInfo classInfo() {
+        return classInfo;
     }
 
     @Override
-    public <T extends Annotation> T[] getAnnotationsByType(Class<T> annotationClass) {
-        return this.field.getAnnotationsByType(annotationClass);
+    public AccessModifier accessModifier() {
+        return accessModifier;
     }
 
     @Override
     public void setAccessible(boolean flag) {
         this.field.setAccessible(flag);
-    }
-
-    @Override
-    public void set(Object obj, Object value) throws IllegalAccessException {
-        this.field.set(obj, value);
-    }
-
-    @Override
-    public Object get(Object obj) throws IllegalAccessException {
-        return this.field.get(obj);
     }
 
 }
