39b24822e0d3df9cee0d0f9ed9ff970d9b938d45
scx567888 <scx567888@outlook.com>
2025-02-27 11:40:02 +0800

    dev

--- scx-reflect/src/main/java/cool/scx/reflect/MethodInfo.java
+++ scx-reflect/src/main/java/cool/scx/reflect/MethodInfo.java
@@ -45,8 +45,18 @@ public final class MethodInfo implements ExecutableInfo {
         return method;
     }
 
+    @Override
+    public ClassInfo classInfo() {
+        return classInfo;
+    }
+
     public String name() {
         return name;
+    }
+
+    @Override
+    public AccessModifier accessModifier() {
+        return accessModifier;
     }
 
     public MethodType methodType() {
@@ -57,10 +67,21 @@ public final class MethodInfo implements ExecutableInfo {
         return returnType;
     }
 
+    @Override
+    public ParameterInfo[] parameters() {
+        return parameters;
+    }
+
     public MethodInfo superMethod() {
         return superMethod;
     }
 
+    @Override
+    public Annotation[] annotations() {
+        return annotations;
+    }
+
+    @Override
     public Annotation[] allAnnotations() {
         return allAnnotations;
     }
@@ -70,28 +91,8 @@ public final class MethodInfo implements ExecutableInfo {
     }
 
     @Override
-    public ClassInfo classInfo() {
-        return classInfo;
-    }
-
-    @Override
-    public AccessModifier accessModifier() {
-        return accessModifier;
-    }
-
-    @Override
     public void setAccessible(boolean flag) {
         method.setAccessible(flag);
-    }
-
-    @Override
-    public ParameterInfo[] parameters() {
-        return parameters;
-    }
-
-    @Override
-    public Annotation[] annotations() {
-        return annotations;
     }
 
 }
