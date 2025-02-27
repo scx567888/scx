20d61d487b745412beb22dde375b38d1e51fd9b0
scx567888 <scx567888@outlook.com>
2025-02-27 14:52:37 +0800

    dev

--- scx-ffm/src/main/java/cool/scx/ffm/platform/win32/helper/WindowsProxyHelper.java
+++ scx-ffm/src/main/java/cool/scx/ffm/platform/win32/helper/WindowsProxyHelper.java
@@ -7,29 +7,23 @@ import java.util.Map;
 import static cool.scx.ffm.platform.win32.WinReg.HKEY_CURRENT_USER;
 import static cool.scx.ffm.platform.win32.helper.Advapi32Helper.*;
 
-/**
- * 设置 windows 系统的代理 (使用前请检查当前操作系统是否为 Windows !!!)
- *
- * @author scx567888
- * @version 0.0.1
- * @see <a href="https://learn.microsoft.com/zh-cn/windows/win32/wininet/enabling-internet-functionality">https://learn.microsoft.com/zh-cn/windows/win32/wininet/enabling-internet-functionality</a>
- */
+/// 设置 windows 系统的代理 (使用前请检查当前操作系统是否为 Windows !!!)
+///
+/// @author scx567888
+/// @version 0.0.1
+/// @see <a href="https://learn.microsoft.com/zh-cn/windows/win32/wininet/enabling-internet-functionality">https://learn.microsoft.com/zh-cn/windows/win32/wininet/enabling-internet-functionality</a>
 public final class WindowsProxyHelper {
 
-    /**
-     * 注册表项地址
-     */
+    /// 注册表项地址
     public static final String INTERNET_SETTINGS_KEY_PATH = "Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings";
     public static final String PROXY_ENABLE = "ProxyEnable";
     public static final String PROXY_SERVER = "ProxyServer";
     public static final String PROXY_OVERRIDE = "ProxyOverride";
     public static final String LOCAL_HOST = "127.0.0.1";
 
-    /**
-     * 获取代理配置信息
-     *
-     * @return 代理配置信息
-     */
+    /// 获取代理配置信息
+    ///
+    /// @return 代理配置信息
     public static Map<String, Object> getInternetSettingsValues() {
         return registryGetValues(
                 HKEY_CURRENT_USER,
@@ -63,16 +57,12 @@ public final class WindowsProxyHelper {
         );
     }
 
-    /**
-     * 开启 代理
-     */
+    /// 开启 代理
     public static void enableProxy() {
         setProxyEnabled(true);
     }
 
-    /**
-     * 关闭 代理设置
-     */
+    /// 关闭 代理设置
     public static void disableProxy() {
         setProxyEnabled(false);
     }
@@ -93,20 +83,16 @@ public final class WindowsProxyHelper {
         );
     }
 
-    /**
-     * 设置代理服务器 (主机默认为本机)
-     *
-     * @param port 端口
-     */
+    /// 设置代理服务器 (主机默认为本机)
+    ///
+    /// @param port 端口
     public static void setProxyServer(int port) {
         setProxyServer(LOCAL_HOST + ":" + port);
     }
 
-    /**
-     * 设置代理服务器
-     *
-     * @param host 主机
-     */
+    /// 设置代理服务器
+    ///
+    /// @param host 主机
     public static void setProxyServer(String host) {
         registrySetStringValue(
                 HKEY_CURRENT_USER,
@@ -116,9 +102,7 @@ public final class WindowsProxyHelper {
         );
     }
 
-    /**
-     * 清空 代理设置
-     */
+    /// 清空 代理设置
     public static void clearProxyServer() {
         setProxyServer("");
     }
@@ -140,9 +124,7 @@ public final class WindowsProxyHelper {
         return value.split(";");
     }
 
-    /**
-     * 设置绕过代理的 主机 列表
-     */
+    /// 设置绕过代理的 主机 列表
     public static void setProxyOverride(String... list) {
         registrySetStringValue(
                 HKEY_CURRENT_USER,
@@ -152,9 +134,7 @@ public final class WindowsProxyHelper {
         );
     }
 
-    /**
-     * 清空 代理设置
-     */
+    /// 清空 代理设置
     public static void clearProxyOverride() {
         setProxyOverride();
     }
