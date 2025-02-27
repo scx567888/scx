efecaa86781fe4c06dfd018891fabb6eb866c6ff
scx567888 <scx567888@outlook.com>
2025-02-27 11:42:35 +0800

    dev

--- scx-reflect/src/main/java/cool/scx/reflect/MemberInfo.java
+++ scx-reflect/src/main/java/cool/scx/reflect/MemberInfo.java
@@ -1,26 +1,12 @@
 package cool.scx.reflect;
 
-import java.lang.annotation.Annotation;
-
 ///  类成员接口
-public interface MemberInfo {
+public interface MemberInfo extends AnnotatedElementInfo {
 
     ClassInfo classInfo();
 
     AccessModifier accessModifier();
 
     void setAccessible(boolean flag);
-
-    Annotation[] annotations();
-
-    default <T extends Annotation> T getAnnotation(Class<T> annotationClass) {
-        var annotations = annotations();
-        for (var annotation : annotations) {
-            if (annotationClass.isInstance(annotation)) {
-                return annotationClass.cast(annotation);
-            }
-        }
-        return null;
-    }
 
 }
