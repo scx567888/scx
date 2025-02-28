36a8cb44e80e3bedab7c58251de1c431f417b48c
scx567888 <scx567888@outlook.com>
2025-02-28 17:00:12 +0800

    dev

--- scx-scheduling/src/main/java/cool/scx/scheduling/cron/CronTaskImpl.java
+++ scx-scheduling/src/main/java/cool/scx/scheduling/cron/CronTaskImpl.java
@@ -2,10 +2,14 @@ package cool.scx.scheduling.cron;
 
 import com.cronutils.model.time.ExecutionTime;
 import com.cronutils.parser.CronParser;
-import cool.scx.scheduling.ScheduleStatus;
+import cool.scx.scheduling.ConcurrencyPolicy;
+import cool.scx.scheduling.ScheduleContext;
+import cool.scx.scheduling.Task;
+import cool.scx.scheduling.TaskStatus;
 
 import java.lang.System.Logger;
 import java.time.Duration;
+import java.time.Instant;
 import java.time.ZonedDateTime;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -14,9 +18,11 @@ import java.util.function.Consumer;
 
 import static com.cronutils.model.CronType.QUARTZ;
 import static com.cronutils.model.definition.CronDefinitionBuilder.instanceDefinitionFor;
+import static cool.scx.scheduling.ConcurrencyPolicy.CONCURRENCY;
+import static cool.scx.scheduling.ConcurrencyPolicy.NO_CONCURRENCY;
 import static java.lang.System.Logger.Level.ERROR;
 import static java.lang.System.getLogger;
-import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
 
 /// Cron 执行的任务
 ///
@@ -26,28 +32,31 @@ public class CronTaskImpl implements CronTask {
 
     private static final Logger logger = getLogger(CronTaskImpl.class.getName());
 
-    //这里默认用 QUARTZ 的格式
+    // 这里默认用 QUARTZ 的格式
     private static final CronParser CRON_PARSER = new CronParser(instanceDefinitionFor(QUARTZ));
 
     private final AtomicLong runCount;
     private final AtomicBoolean cancel;
     private ExecutionTime executionTime;
-    private boolean concurrent;
+    private ConcurrencyPolicy concurrencyPolicy;
     private long maxRunCount;
     private ScheduledExecutorService executor;
-    private Consumer<ScheduleStatus> task;
+    private Task task;
     private ZonedDateTime lastNext;
-
+    private Consumer<Throwable> errorHandler;
+    private ScheduleContext context;
 
     public CronTaskImpl() {
         this.runCount = new AtomicLong(0);
         this.cancel = new AtomicBoolean(false);
         this.executionTime = null;
-        this.concurrent = false;// 默认不允许并发
-        this.maxRunCount = -1;// 默认不限制运行次数
+        this.concurrencyPolicy = NO_CONCURRENCY; // 默认不允许并发
+        this.maxRunCount = -1; // 默认不限制运行次数
         this.executor = null;
         this.task = null;
         this.lastNext = null;
+        this.errorHandler = null;
+        this.context = null;
     }
 
     @Override
@@ -58,8 +67,8 @@ public class CronTaskImpl implements CronTask {
     }
 
     @Override
-    public CronTask concurrent(boolean concurrent) {
-        this.concurrent = concurrent;
+    public CronTask concurrencyPolicy(ConcurrencyPolicy concurrencyPolicy) {
+        this.concurrencyPolicy = concurrencyPolicy;
         return this;
     }
 
@@ -76,56 +85,91 @@ public class CronTaskImpl implements CronTask {
     }
 
     @Override
-    public CronTask task(Consumer<ScheduleStatus> task) {
+    public CronTask task(Task task) {
         this.task = task;
         return this;
     }
 
     @Override
-    public ScheduleStatus start() {
+    public CronTask onError(Consumer<Throwable> errorHandler) {
+        this.errorHandler = errorHandler;
+        return this;
+    }
+
+    @Override
+    public ScheduleContext start() {
         scheduleNext();
-        return new ScheduleStatus() {
+        this.context = new ScheduleContext() {
             @Override
             public long runCount() {
                 return runCount.get();
             }
 
             @Override
+            public Instant nextRunTime() {
+                return lastNext != null ? lastNext.toInstant() : null;
+            }
+
+            @Override
+            public Instant nextRunTime(int count) {
+                ZonedDateTime nextTime = lastNext;
+                for (int i = 0; i < count; i++) {
+                    nextTime = executionTime.nextExecution(nextTime).orElse(null);
+                }
+                return nextTime != null ? nextTime.toInstant() : null;
+            }
+
+            @Override
             public void cancel() {
                 cancel.set(true);
             }
+
+            @Override
+            public Status status() {
+                return cancel.get() ? Status.CANCELED : (runCount.get() >= maxRunCount ? Status.DONE : Status.RUNNING);
+            }
         };
+        return context;
     }
 
     public void run() {
         var l = runCount.incrementAndGet();
-        //已经取消了 或者 达到了最大次数
+        // 已经取消了 或者 达到了最大次数
         if (cancel.get() || maxRunCount != -1 && l > maxRunCount) {
             return;
         }
         //如果是并发的 直接处理下一次
-        if (concurrent) {
+        if (concurrencyPolicy == CONCURRENCY) {
             scheduleNext();
         }
         try {
-            task.accept(new ScheduleStatus() {
+            task.run(new TaskStatus() {
 
                 @Override
-                public long runCount() {
+                public long currentRunCount() {
                     return l;
                 }
 
                 @Override
-                public void cancel() {
-                    cancel.set(true);
+                public ScheduleContext context() {
+                    return context;
                 }
 
             });
         } catch (Throwable e) {
-            logger.log(ERROR, "调度任务时发生错误 !!!", e);
+            if (errorHandler != null) {
+                try {
+                    errorHandler.accept(e);
+                } catch (Throwable ex) {
+                    e.addSuppressed(ex);
+                    logger.log(ERROR, "errorHandler 发生错误 !!!", e);
+                }
+            } else {
+                logger.log(ERROR, "调度任务时发生错误 !!!", e);
+            }
         }
         //如果不是并发 就需要等待当前任务完成在处理下一次任务
-        if (!concurrent) {
+        if (concurrencyPolicy != CONCURRENCY) {
             scheduleNext();
         }
     }
@@ -140,10 +184,9 @@ public class CronTaskImpl implements CronTask {
 
         lastNext = executionTime.nextExecution(lastNext).orElse(null);
 
-        var delay = Duration.between(now, lastNext).toMillis();
+        var delay = Duration.between(now, lastNext).toNanos();
 
-        //此处精度不需要太高
-        executor.schedule(this::run, delay, MILLISECONDS);
+        executor.schedule(this::run, delay, NANOSECONDS);
 
     }
 
