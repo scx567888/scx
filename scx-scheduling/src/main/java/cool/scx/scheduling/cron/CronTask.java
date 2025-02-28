8f6ad2b18c8a0be8058deca1e5ed83e5aea1390b
scx567888 <scx567888@outlook.com>
2025-02-28 15:05:59 +0800

    dev

--- scx-scheduling/src/main/java/cool/scx/scheduling/cron/CronTask.java
+++ scx-scheduling/src/main/java/cool/scx/scheduling/cron/CronTask.java
@@ -8,7 +8,7 @@ import cool.scx.scheduling.ScheduleTask;
 ///
 /// @author scx567888
 /// @version 0.0.1
-public interface CronTask extends ScheduleTask {
+public interface CronTask extends ScheduleTask<CronTask> {
 
     /// cron 表达式
     ///
