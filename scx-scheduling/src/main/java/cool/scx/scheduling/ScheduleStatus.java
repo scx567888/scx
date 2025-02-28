b4a22a6941877c7762f806e023625d2fb67575ed
scx567888 <scx567888@outlook.com>
2025-02-28 12:59:04 +0800

    dev

--- scx-scheduling/src/main/java/cool/scx/scheduling/ScheduleStatus.java
+++ scx-scheduling/src/main/java/cool/scx/scheduling/ScheduleStatus.java
@@ -14,17 +14,27 @@ public interface ScheduleStatus {
     long runCount();
 
     /// 下一次运行的时间
+    ///  如果下一次不会运行任何任务 则返回 null
     Instant nextRunTime();
 
     /// 指定次数后运行的时间
+    /// 假设 当前调度器拥有 maxRunCount 限制 那么当超出限制之后 会返回 null
     Instant nextRunTime(int count);
 
     /// 取消任务 (有两种情况)
     /// 1, 当作为 task 回调参数时可用来取消下一次未执行的任务
     /// 2, 当作为 start 返回值时可以取消任何时候的任务 包括任务从未开始也可以取消
     void cancel();
-    
-    /// 是否已经取消了
-    void isCanceled();
+
+    /// 调度器状态 这表示宏观的调度器的状态而不是 子任务的状态
+    /// 比如在两个任务的间歇期间 仍会返回 进行中
+    Status status();
+
+    enum Status {
+        READY,       // 未开始
+        RUNNING,     // 正在进行中
+        DONE,        // 已完成
+        CANCELED,    // 已取消
+    }
 
 }
