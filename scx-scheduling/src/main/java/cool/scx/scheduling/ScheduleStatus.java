83e9363cd4669682e7ed98808d8835c99553213d
scx567888 <scx567888@outlook.com>
2025-02-28 12:32:17 +0800

    dev

--- scx-scheduling/src/main/java/cool/scx/scheduling/ScheduleStatus.java
+++ scx-scheduling/src/main/java/cool/scx/scheduling/ScheduleStatus.java
@@ -11,23 +11,20 @@ public interface ScheduleStatus {
     /// 任务运行的次数 (有两种情况)
     /// 1, 当作为 task 回调参数时表示当前任务的运行次数(包括当前次数) 不会变化
     /// 2, 当作为 start 返回值时表示当前任务的运行次数 会动态变化
-    ///
-    /// @return c
     long runCount();
 
     /// 下一次运行的时间
-    ///
-    /// @return c
     Instant nextRunTime();
 
     /// 指定次数后运行的时间
-    ///
-    /// @return c
     Instant nextRunTime(int count);
 
     /// 取消任务 (有两种情况)
     /// 1, 当作为 task 回调参数时可用来取消下一次未执行的任务
     /// 2, 当作为 start 返回值时可以取消任何时候的任务 包括任务从未开始也可以取消
     void cancel();
+    
+    /// 是否已经取消了
+    void isCanceled();
 
 }
