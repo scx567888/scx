90919831a4e7624886aa8e854ccef7db42944f43
scx567888 <scx567888@outlook.com>
2025-02-28 13:51:12 +0800

    dev

---
+++ scx-scheduling/src/main/java/cool/scx/scheduling/TaskStatus.java
@@ -0,0 +1,15 @@
+package cool.scx.scheduling;
+
+public interface TaskStatus {
+
+    /// 当前运行次数
+    long currentRunCount();
+    
+    /// 调度上下文
+    ScheduleContext context();
+
+    default void cancelSchedule(){
+        context().cancel();
+    }
+    
+}
