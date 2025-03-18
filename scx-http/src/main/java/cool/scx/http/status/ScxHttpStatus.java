248ba41b7e5b487d11795b0db85813044b48b8e7
scx567888 <scx567888@outlook.com>
2025-03-18 15:50:07 +0800

    dev

--- scx-http/src/main/java/cool/scx/http/status/ScxHttpStatus.java
+++ scx-http/src/main/java/cool/scx/http/status/ScxHttpStatus.java
@@ -6,10 +6,14 @@ package cool.scx.http.status;
 /// @version 0.0.1
 public sealed interface ScxHttpStatus permits HttpStatus, ScxHttpStatusImpl {
 
-    static ScxHttpStatus of(int v) {
+    static ScxHttpStatus of(int code) {
         // 优先使用 HttpStatus
-        var m = HttpStatus.find(v);
-        return m != null ? m : new ScxHttpStatusImpl(v);
+        var m = HttpStatus.find(code);
+        return m != null ? m : new ScxHttpStatusImpl(code);
+    }
+
+    static ScxHttpStatus of(int code, String description) {
+        return new ScxHttpStatusImpl(code, description);
     }
 
     /// 状态码
@@ -17,5 +21,10 @@ public sealed interface ScxHttpStatus permits HttpStatus, ScxHttpStatusImpl {
 
     /// 状态码描述
     String description();
+
+    /// 是否相等
+    default boolean equals(ScxHttpStatus other) {
+        return this.code() == other.code();
+    }
 
 }
