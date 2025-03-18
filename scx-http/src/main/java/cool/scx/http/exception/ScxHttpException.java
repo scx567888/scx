0fd00b6494f3e80e6def51db1f0d058089fd74a9
scx567888 <scx567888@outlook.com>
2025-03-18 16:11:49 +0800

    dev

--- scx-http/src/main/java/cool/scx/http/exception/ScxHttpException.java
+++ scx-http/src/main/java/cool/scx/http/exception/ScxHttpException.java
@@ -1,12 +1,12 @@
 package cool.scx.http.exception;
 
-import cool.scx.http.status.HttpStatus;
+import cool.scx.http.status.ScxHttpStatus;
 
 /// ScxHttpException
-/// 这是一个基于 [HttpStatus] 的运行时异常类，用于表示 HTTP 请求处理过程中的异常情况。
+/// 这是一个基于 [ScxHttpStatus] 的运行时异常类，用于表示 HTTP 请求处理过程中的异常情况。
 ///
 /// 该异常旨在处理 HTTP 规范中 4xx（客户端错误）和 5xx（服务器错误）序列的状态码，表示请求失败的各种可能原因。
-/// 通过 [HttpStatus] 提供了详细的状态码和描述信息，使异常的语义更加清晰。
+/// 通过 [ScxHttpStatus] 提供了详细的状态码和描述信息，使异常的语义更加清晰。
 ///
 /// ### 注意：
 ///
@@ -15,7 +15,7 @@ import cool.scx.http.status.HttpStatus;
 /// ### 适用场景：
 ///
 ///   - 在 HTTP 请求处理失败时，抛出此异常以便调用方捕获并作出相应处理。
-///   - 结合 [HttpStatus] 使用，可以快速标识具体的错误类型并生成标准化的响应。
+///   - 结合 [ScxHttpStatus] 使用，可以快速标识具体的错误类型并生成标准化的响应。
 ///
 /// ### 使用示例：
 /// ```java
@@ -26,29 +26,29 @@ import cool.scx.http.status.HttpStatus;
 /// @version 0.0.1
 public class ScxHttpException extends RuntimeException {
 
-    private final HttpStatus status;
+    private final ScxHttpStatus status;
 
-    public ScxHttpException(HttpStatus status) {
+    public ScxHttpException(ScxHttpStatus status) {
         super();
         this.status = status;
     }
 
-    public ScxHttpException(HttpStatus status, String message) {
+    public ScxHttpException(ScxHttpStatus status, String message) {
         super(message);
         this.status = status;
     }
 
-    public ScxHttpException(HttpStatus status, Throwable cause) {
+    public ScxHttpException(ScxHttpStatus status, Throwable cause) {
         super(cause);
         this.status = status;
     }
 
-    public ScxHttpException(HttpStatus status, String message, Throwable cause) {
+    public ScxHttpException(ScxHttpStatus status, String message, Throwable cause) {
         super(message, cause);
         this.status = status;
     }
 
-    public final HttpStatus status() {
+    public final ScxHttpStatus status() {
         return this.status;
     }
 
