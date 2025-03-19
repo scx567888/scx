821b0f4a015721d3c99f79dcf410d2f9386028d2
scx567888 <scx567888@outlook.com>
2025-03-19 12:16:21 +0800

    dev

--- scx-io/src/main/java/cool/scx/io/io_stream/DataReaderInputStream.java
+++ scx-io/src/main/java/cool/scx/io/io_stream/DataReaderInputStream.java
@@ -10,6 +10,13 @@ import java.io.OutputStream;
 public class DataReaderInputStream extends InputStream {
 
     private final PowerfulLinkedDataReader dataReader;
+    private volatile boolean closed;
+
+    private void ensureOpen() throws IOException {
+        if (closed) {
+            throw new IOException("Stream closed");
+        }
+    }
 
     public DataReaderInputStream(PowerfulLinkedDataReader dataReader) {
         this.dataReader = dataReader;
@@ -21,21 +28,29 @@ public class DataReaderInputStream extends InputStream {
 
     @Override
     public int read() throws IOException {
+        ensureOpen();
         return dataReader.inputStreamRead();
     }
 
     @Override
     public int read(byte[] b, int off, int len) throws IOException {
+        ensureOpen();
         return dataReader.inputStreamRead(b, off, len);
     }
 
     @Override
     public long transferTo(OutputStream out) throws IOException {
+        ensureOpen();
         return dataReader.inputStreamTransferTo(out);
     }
 
     public PowerfulLinkedDataReader dataReader() {
         return dataReader;
+    }
+
+    @Override
+    public void close() throws IOException {
+        closed = true;
     }
 
 }
