7a5638d260b179b16e531cbf5ccef2989bd3170e
scx567888 <scx567888@outlook.com>
2025-03-25 18:03:47 +0800

    dev

--- scx-http-x/src/main/java/cool/scx/http/x/http1/Http1Helper.java
+++ scx-http-x/src/main/java/cool/scx/http/x/http1/Http1Helper.java
@@ -1,12 +1,10 @@
 package cool.scx.http.x.http1;
 
 import cool.scx.http.exception.BadRequestException;
-import cool.scx.http.headers.ScxHttpHeaders;
 import cool.scx.http.headers.ScxHttpHeadersWritable;
 import cool.scx.http.peer_info.PeerInfo;
 import cool.scx.http.peer_info.PeerInfoWritable;
 import cool.scx.http.status.ScxHttpStatus;
-import cool.scx.http.version.HttpVersion;
 import cool.scx.http.x.http1.headers.Http1Headers;
 import cool.scx.http.x.http1.request_line.Http1RequestLine;
 import cool.scx.tcp.ScxTCPSocket;
@@ -17,55 +15,27 @@ import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 
 import static cool.scx.common.util.StringUtils.isBlank;
-import static cool.scx.http.headers.HttpFieldName.*;
+import static cool.scx.http.headers.HttpFieldName.HOST;
 import static cool.scx.http.method.HttpMethod.GET;
 import static cool.scx.http.status.HttpStatus.*;
+import static cool.scx.http.x.http1.headers.connection.Connection.UPGRADE;
+import static cool.scx.http.x.http1.headers.upgrade.Upgrade.WEB_SOCKET;
 
 public final class Http1Helper {
 
-    public static final byte[] CONTINUE_100 = "HTTP/1.1 100 Continue\r\n\r\n".getBytes(StandardCharsets.UTF_8);
+    private static final byte[] CONTINUE_100 = "HTTP/1.1 100 Continue\r\n\r\n".getBytes(StandardCharsets.UTF_8);
     public static final byte[] CRLF_BYTES = "\r\n".getBytes();
     public static final byte[] CRLF_CRLF_BYTES = "\r\n\r\n".getBytes();
     public static final byte[] CHUNKED_END_BYTES = "0\r\n\r\n".getBytes();
 
-    public static boolean checkIsWebSocketHandshake(Http1RequestLine requestLine, ScxHttpHeaders headers) {
-        if (requestLine.method() == GET) {
-            var connection = headers.get(CONNECTION);
-            if ("upgrade".equalsIgnoreCase(connection)) {
-                var upgrade = headers.get(UPGRADE);
-                return "websocket".equalsIgnoreCase(upgrade);
-            }
-        }
-        return false;
-    }
-
-    public static boolean checkIsKeepAlive(Http1RequestLine requestLine, ScxHttpHeaders headers) {
-        // Http 1.1 以下的均不支持持久连接  
-        if (requestLine.version() == HttpVersion.HTTP_1_1) {
-            var connection = headers.get(CONNECTION);
-            // Http 1.1 中 不指定或者显示指定都表示持久连接 
-            if (connection == null || "keep-alive".equalsIgnoreCase(connection)) {
-                return true;
-            }
-            //如果显示指定 则需要关闭
-            if ("close".equalsIgnoreCase(connection)) {
-                return false;
-            }
-        }
-        return false;
-    }
-
-    public static boolean checkIs100ContinueExpected(ScxHttpHeaders headers) {
-        var expect = headers.get(EXPECT);
-        return "100-continue".equalsIgnoreCase(expect);
+    public static boolean checkIsWebSocketHandshake(Http1RequestLine requestLine, Http1Headers headers) {
+        return requestLine.method() == GET &&
+                headers.connection() == UPGRADE &&
+                headers.upgrade() == WEB_SOCKET;
     }
 
     public static void sendContinue100(OutputStream out) throws IOException {
         out.write(CONTINUE_100);
-    }
-
-    public static void sendChunkedEnd(OutputStream out) throws IOException {
-        out.write(CHUNKED_END_BYTES);
     }
 
     public static void consumeInputStream(InputStream inputStream) {
@@ -88,9 +58,8 @@ public final class Http1Helper {
         return PeerInfo.of().address(address).host(address.getHostString()).port(address.getPort());
     }
 
-    //验证 http1.x 中的 host
-    public static void validateHeaders(ScxHttpHeadersWritable headers) {
-        //我们只验证是否存在 HOST 字段 (有且只能有一个)
+    /// 验证 Http/1.1 中的 Host, 这里我们只校验是否存在且只有一个值
+    public static void validateHost(ScxHttpHeadersWritable headers) {
         var all = headers.getAll(HOST);
         int size = all.size();
         if (size == 0) {
