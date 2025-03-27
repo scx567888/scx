f1a0970607fb35762d8b1153b4efbc3a01a61f22
scx567888 <scx567888@outlook.com>
2025-03-27 19:02:55 +0800

    dev

--- scx-http-x/src/main/java/cool/scx/http/x/http1/status_line/Http1StatusLine.java
+++ scx-http-x/src/main/java/cool/scx/http/x/http1/status_line/Http1StatusLine.java
@@ -1,10 +1,11 @@
 package cool.scx.http.x.http1.status_line;
 
 import cool.scx.http.version.HttpVersion;
+import cool.scx.http.x.http1.request_line.InvalidHttpVersion;
 
 public record Http1StatusLine(HttpVersion version, int code, String reason) {
 
-    public static Http1StatusLine of(String statusLineStr) {
+    public static Http1StatusLine of(String statusLineStr) throws InvalidHttpStatusLineException, InvalidHttpStatusException, InvalidHttpVersion {
         return Http1StatusLineHelper.parseStatusLine(statusLineStr);
     }
 
