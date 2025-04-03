a8b06f1f5b7d9d5b56072db54c0b4689d55dc564
scx567888 <scx567888@outlook.com>
2025-04-03 18:26:21 +0800

    dev

--- scx-jdbc/src/main/java/cool/scx/jdbc/sql/NamedSQLListParameter.java
+++ scx-jdbc/src/main/java/cool/scx/jdbc/sql/NamedSQLListParameter.java
@@ -1,26 +1,30 @@
 package cool.scx.jdbc.sql;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-/// 用来表示 特殊的列表形式命名 SQL 参数
-/// 可以用在如 in 中
-/// 注意 !!! 此参数只能用于 NamedSQL
+/// 代表一种特殊的 SQL 参数类型，用于处理带有列表形式的命名参数。
+///
+/// 通常用于 SQL 查询中，如 IN 子句 例如：`SELECT * FROM table WHERE field IN (:values)``
+/// 该类允许用户将多个值传递给 SQL 查询，而不是仅仅传递一个单一值。
+///
+/// 注意：此参数类型只能用于带有命名参数的 SQL 查询，且仅适用于 NamedSQL。
+///
+/// 示例：
+/// ```java
+/// var inList = new NamedSQLListParameter(1, 2, 3, 4);
+///```
+/// 对应 SQL 查询：`SELECT * FROM table WHERE field IN (:inList)`
+///
+/// @author scx567888
+/// @version 0.0.1
+/// @see NamedSQL
 public class NamedSQLListParameter {
 
     private final Collection<?> values;
 
     public NamedSQLListParameter(Object... values) {
         this.values = List.of(values);
-    }
-
-    public NamedSQLListParameter(Iterable<?> values) {
-        var temp = new ArrayList<>();
-        for (var value : values) {
-            temp.add(value);
-        }
-        this.values = temp;
     }
 
     public NamedSQLListParameter(Collection<?> values) {
