4d55d0b10950b5f8068ffcb51f8203171a0dae32
scx567888 <scx567888@outlook.com>
2025-04-13 17:33:14 +0800

    v3.5.3

--- scx-http/src/main/java/cool/scx/http/routing/TypeMatcherImpl.java
+++ scx-http/src/main/java/cool/scx/http/routing/TypeMatcherImpl.java
@@ -1,35 +1,13 @@
 package cool.scx.http.routing;
 
 import cool.scx.http.ScxHttpServerRequest;
-//import cool.scx.http.web_socket.ScxServerWebSocketHandshakeRequest;
-
-import static cool.scx.http.routing.TypeMatcher.Type.*;
 
 public class TypeMatcherImpl implements TypeMatcher {
 
-    static TypeMatcher ANY_TYPE = new TypeMatcherImpl(ANY);
-    static TypeMatcher NORMAL_TYPE = new TypeMatcherImpl(NORMAL);
-    static TypeMatcher WEB_SOCKET_HANDSHAKE_TYPE = new TypeMatcherImpl(WEB_SOCKET_HANDSHAKE);
-
-    private final Type type;
-
-    private TypeMatcherImpl(TypeMatcher.Type type) {
-        this.type = type;
-    }
-
-    @Override
-    public Type type() {
-        return type;
-    }
+    static TypeMatcher ANY_TYPE = new TypeMatcherImpl();
 
     @Override
     public boolean matches(ScxHttpServerRequest request) {
-        // todo 这里如何处理 不同类型?
-//        return switch (type) {
-//            case ANY -> true;
-//            case NORMAL -> !(request instanceof ScxServerWebSocketHandshakeRequest);
-//            case WEB_SOCKET_HANDSHAKE -> request instanceof ScxServerWebSocketHandshakeRequest;
-//        };
         return true;
     }
 
