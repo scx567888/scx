39180d39be9dcedd96b198349e9a505016d930ad
scx567888 <scx567888@outlook.com>
2025-04-19 18:17:39 +0800

    dev

--- scx-data/src/main/java/cool/scx/data/Repository.java
+++ scx-data/src/main/java/cool/scx/data/Repository.java
@@ -26,14 +26,6 @@ public interface Repository<Entity, ID> {
     /// @return 主键 ID (若数据没有主键, 则为 null)
     ID add(Entity entity, FieldPolicy fieldPolicy);
 
-    default ID add(Entity entity) {
-        return add(entity, includedAll());
-    }
-
-    default ID add(FieldPolicy fieldPolicy) {
-        return add((Entity) null, fieldPolicy);
-    }
-
     /// 添加多条数据
     ///
     /// 因为无法判断 entityList 中每个成员的情况, 所以所有插入字段均由 fieldPolicy 提供
@@ -43,16 +35,43 @@ public interface Repository<Entity, ID> {
     /// @return 主键 ID 列表 (若数据没有主键, 则为 null 列表)
     List<ID> add(Collection<Entity> entityList, FieldPolicy fieldPolicy);
 
-    default List<ID> add(Collection<Entity> entityList) {
-        return add(entityList, includedAll());
-    }
-
-    /// 查询
+    /// 创建一个查询器
     ///
     /// @param query       查询条件
     /// @param fieldPolicy 字段策略
-    /// @return 查询执行器
+    /// @return 查询器
     Finder<Entity> finder(Query query, FieldPolicy fieldPolicy);
+
+    /// 更新数据
+    ///
+    /// 当 entity 为 null 时, 将使用 fieldPolicy 进行纯表达式更新, 此时要求 fieldPolicy 必须包含至少一个字段表达式
+    ///
+    /// @param entity      需要更新的数据 (可以为 null)
+    /// @param query       查询条件
+    /// @param fieldPolicy 字段策略
+    /// @return 更新成功的条数
+    long update(Entity entity, FieldPolicy fieldPolicy, Query query);
+
+    /// 删除数据
+    ///
+    /// @param query 查询条件
+    /// @return 删除成功的条数
+    long delete(Query query);
+
+    /// 清空整个数据源 (慎用)
+    void clear();
+
+    default ID add(Entity entity) {
+        return add(entity, includedAll());
+    }
+
+    default ID add(FieldPolicy fieldPolicy) {
+        return add((Entity) null, fieldPolicy);
+    }
+
+    default List<ID> add(Collection<Entity> entityList) {
+        return add(entityList, includedAll());
+    }
 
     default Finder<Entity> finder(Query query) {
         return finder(query, includedAll());
@@ -90,16 +109,6 @@ public interface Repository<Entity, ID> {
         return finder(query).first();
     }
 
-    /// 更新数据
-    ///
-    /// 当 entity 为 null 时, 将使用 fieldPolicy 进行纯表达式更新, 此时要求 fieldPolicy 必须包含至少一个字段表达式
-    ///
-    /// @param entity      需要更新的数据 (可以为 null)
-    /// @param query       查询条件
-    /// @param fieldPolicy 字段策略
-    /// @return 更新成功的条数
-    long update(Entity entity, FieldPolicy fieldPolicy, Query query);
-
     default long update(Entity entity, Query query) {
         return update(entity, includedAll(), query);
     }
@@ -115,14 +124,5 @@ public interface Repository<Entity, ID> {
     default long count() {
         return finder().count();
     }
-
-    /// 删除数据
-    ///
-    /// @param query 查询条件
-    /// @return 删除成功的条数
-    long delete(Query query);
-
-    /// 清空整个数据源 (慎用)
-    void clear();
 
 }
