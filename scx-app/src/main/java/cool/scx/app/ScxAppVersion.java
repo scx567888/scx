f2a3f525e505499ab5647686e9ceb451f5233de3
scx567888 <scx567888@outlook.com>
2025-04-21 00:45:34 +0800

    dev

--- scx-app/src/main/java/cool/scx/app/ScxAppVersion.java
+++ scx-app/src/main/java/cool/scx/app/ScxAppVersion.java
@@ -9,7 +9,7 @@ import cool.scx.ansi.Ansi;
 public class ScxAppVersion {
 
     /// SCX 版本号
-    public static final String SCX_VERSION = "3.5.8";
+    public static final String SCX_VERSION = "3.5.9";
 
     /// 在控制台上打印 banner
     static void printBanner() {
