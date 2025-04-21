69a89f3b744fd4739057e1bf0f399172f4223dcf
scx567888 <scx567888@outlook.com>
2025-04-21 11:03:18 +0800

    dev

---
+++ scx-jdbc/src/main/java/cool/scx/jdbc/result_handler/map_builder/DefaultMapBuilder.java
@@ -0,0 +1,27 @@
+package cool.scx.jdbc.result_handler.map_builder;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+record DefaultMapBuilder(Supplier<Map<String, Object>> mapSupplier,
+                         Function<String, String> columnNameMapping) implements MapBuilder {
+
+    public static final MapBuilder MAP_BUILDER = new DefaultMapBuilder(HashMap::new, c -> c);
+
+    public Map<String, Object> createMap(ResultSet rs, String[] columnLabelIndex) throws SQLException {
+        var map = mapSupplier.get();
+
+        for (int i = 1; i <= columnLabelIndex.length; i = i + 1) {
+            var key = columnNameMapping.apply(columnLabelIndex[i]);
+            var value = rs.getObject(i);
+            map.put(key, value);
+        }
+
+        return map;
+    }
+
+}
