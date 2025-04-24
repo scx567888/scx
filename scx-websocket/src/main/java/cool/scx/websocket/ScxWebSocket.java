c493863cb67177abecc879a38f211053a754a840
scx567888 <scx567888@outlook.com>
2025-04-24 18:27:44 +0800

    dev

--- scx-websocket/src/main/java/cool/scx/websocket/ScxWebSocket.java
+++ scx-websocket/src/main/java/cool/scx/websocket/ScxWebSocket.java
@@ -1,12 +1,7 @@
 package cool.scx.websocket;
 
-import cool.scx.websocket.handler.BinaryMessageHandler;
-import cool.scx.websocket.handler.CloseHandler;
-import cool.scx.websocket.handler.TextMessageHandler;
-
-import java.util.function.Consumer;
-
-import static cool.scx.websocket.WebSocketCloseInfo.NORMAL_CLOSE;
+import static cool.scx.websocket.WebSocketHelper.createClosePayload;
+import static cool.scx.websocket.WebSocketOpCode.*;
 
 /// ScxWebSocket
 ///
@@ -14,49 +9,44 @@ import static cool.scx.websocket.WebSocketCloseInfo.NORMAL_CLOSE;
 /// @version 0.0.1
 public interface ScxWebSocket {
 
-    ScxWebSocket onTextMessage(TextMessageHandler textMessageHandler);
+    WebSocketFrame readFrame();
 
-    ScxWebSocket onBinaryMessage(BinaryMessageHandler binaryMessageHandler);
-
-    ScxWebSocket onPing(Consumer<byte[]> pingHandler);
-
-    ScxWebSocket onPong(Consumer<byte[]> pongHandler);
-
-    ScxWebSocket onClose(CloseHandler closeHandler);
-
-    ScxWebSocket onError(Consumer<Throwable> errorHandler);
-
-    //以上回调设置完成之后调用以便启动 websocket 监听
-    void start();
-
-    ScxWebSocket send(String textMessage, boolean last);
-
-    ScxWebSocket send(byte[] binaryMessage, boolean last);
-
-    ScxWebSocket ping(byte[] data);
-
-    ScxWebSocket pong(byte[] data);
-
-    ScxWebSocket close(int code, String reason);
+    ScxWebSocket sendFrame(WebSocketFrame frame);
 
     ScxWebSocket terminate();
 
     boolean isClosed();
 
-    default ScxWebSocket send(String textMessage) {
-        return send(textMessage, true);
+    default ScxWebSocket send(String textMessage, boolean last) {
+        var payload = textMessage != null ? textMessage.getBytes() : new byte[]{};
+        var frame = WebSocketFrame.of(TEXT, payload, last);
+        sendFrame(frame);
+        return this;
     }
 
-    default ScxWebSocket send(byte[] binaryMessage) {
-        return send(binaryMessage, true);
+    default ScxWebSocket send(byte[] binaryMessage, boolean last) {
+        var frame = WebSocketFrame.of(BINARY, binaryMessage, last);
+        sendFrame(frame);
+        return this;
     }
 
-    default ScxWebSocket close(WebSocketCloseInfo closeInfo) {
-        return close(closeInfo.code(), closeInfo.reason());
+    default ScxWebSocket ping(byte[] data) {
+        var frame = WebSocketFrame.of(PING, data);
+        sendFrame(frame);
+        return this;
     }
 
-    default ScxWebSocket close() {
-        return close(NORMAL_CLOSE);
+    default ScxWebSocket pong(byte[] data) {
+        var frame = WebSocketFrame.of(PONG, data, false);
+        sendFrame(frame);
+        return this;
+    }
+
+    default ScxWebSocket close(int code, String reason) {
+        var closePayload = createClosePayload(code, reason);
+        var frame = WebSocketFrame.of(CLOSE, closePayload, true);
+        sendFrame(frame);
+        return this;
     }
 
 }
