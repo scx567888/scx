bdc1d6778a5f387a316dac0110b005296da425c5
scx567888 <scx567888@outlook.com>
2025-04-25 15:32:50 +0800

    dev

--- scx-http-x/src/main/java/cool/scx/http/x/http1/headers/upgrade/ScxUpgradeImpl.java
+++ scx-http-x/src/main/java/cool/scx/http/x/http1/headers/upgrade/ScxUpgradeImpl.java
@@ -2,4 +2,13 @@ package cool.scx.http.x.http1.headers.upgrade;
 
 record ScxUpgradeImpl(String value) implements ScxUpgrade {
 
+    ScxUpgradeImpl {
+        value = value.toLowerCase();
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+    
 }
