54636290024020cca62384811df3f34a0162fda6
scx567888 <scx567888@outlook.com>
2025-04-30 17:28:47 +0800

    dev

--- scx-http/src/main/java/cool/scx/http/version/HttpVersion.java
+++ scx-http/src/main/java/cool/scx/http/version/HttpVersion.java
@@ -1,22 +1,26 @@
 package cool.scx.http.version;
 
 /// HttpVersion
-/// 
+///
 /// 这里我们只保留流行的 Http 版本,较旧的版本如 HTTP/0.9 和 HTTP/1.0 不再做保留
 ///
 /// @author scx567888
 /// @version 0.0.1
 public enum HttpVersion {
 
-    HTTP_1_1("http/1.1"),
-    HTTP_2("h2"),
-    HTTP_3("h3");
+    HTTP_1_1("HTTP/1.1", "http/1.1"),
+    HTTP_2("HTTP/2", "h2"),
+    HTTP_3("HTTP/3", "h3");
 
-    private final String value;
+    private final String protocolVersion;
 
-    HttpVersion(String value) {
-        this.value = value;
+    private final String alpnValue;
+
+    HttpVersion(String protocolVersion, String alpnValue) {
+        this.protocolVersion = protocolVersion;
+        this.alpnValue = alpnValue;
     }
+
 
     /// @param version v
     /// @return 未找到时 抛出异常
@@ -31,23 +35,26 @@ public enum HttpVersion {
     /// @param version v
     /// @return 未找到时返回 null
     public static HttpVersion find(String version) {
-        var upperCase = version.toLowerCase();
-        return switch (upperCase) {
+        var lowerCase = version.toLowerCase();
+        return switch (lowerCase) {
             case "http/1.1" -> HTTP_1_1;
-            case "h2" -> HTTP_2;
-            case "h3" -> HTTP_3;
+            case "http/2", "h2" -> HTTP_2;
+            case "http/3", "h3" -> HTTP_3;
             default -> null;
         };
     }
 
-    /// 协议名称
-    public String value() {
-        return this.value;
+    public String protocolVersion() {
+        return protocolVersion;
+    }
+
+    public String alpnValue() {
+        return alpnValue;
     }
 
     @Override
     public String toString() {
-        return value;
+        return protocolVersion;
     }
 
 }
