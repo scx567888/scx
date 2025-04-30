ffa4b59a6c70138166c01a0d7be2ec6b74cfcce8
scx567888 <scx567888@outlook.com>
2025-04-30 10:02:33 +0800

    dev

--- scx-tcp/src/main/java/cool/scx/tcp/ScxTCPClient.java
+++ scx-tcp/src/main/java/cool/scx/tcp/ScxTCPClient.java
@@ -8,13 +8,10 @@ import java.net.SocketAddress;
 /// @version 0.0.1
 public interface ScxTCPClient {
 
-    /// 默认 10 秒
-    int DEFAULT_TIMEOUT = 10 * 1000;
-
     ScxTCPSocket connect(SocketAddress endpoint, int timeout);
 
     default ScxTCPSocket connect(SocketAddress endpoint) {
-        return connect(endpoint, DEFAULT_TIMEOUT);
+        return connect(endpoint, 0);
     }
 
 }
