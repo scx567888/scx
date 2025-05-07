c8fcada1c172980fc6c39d122fb114adee229c15
scx567888 <scx567888@outlook.com>
2025-05-07 16:17:01 +0800

    dev

---
+++ scx-bean/src/main/java/cool/scx/bean/x/InstanceBeanContext.java
@@ -0,0 +1,43 @@
+package cool.scx.bean.x;
+
+import java.util.List;
+
+/// 根据一个 已经存在的 bean 创建而来
+public final class InstanceBeanContext implements BeanContext {
+
+    private final Object bean;
+    private boolean alreadyInjected;
+
+    public InstanceBeanContext(Object bean) {
+        this.bean = bean;
+        this.alreadyInjected = false;
+    }
+
+    @Override
+    public Object create(BeanFactory beanFactory) {
+        return bean;
+    }
+
+    @Override
+    public Object createAndInject(BeanFactory beanFactory, List<BeanInjector> injectors) {
+        if (alreadyInjected) {
+            return bean;
+        }
+        for (var injector : injectors) {
+            injector.inject(bean);
+        }
+        alreadyInjected = true;
+        return bean;
+    }
+
+    @Override
+    public Class<?> beanClass() {
+        return bean.getClass();
+    }
+
+    @Override
+    public boolean singleton() {
+        return true;
+    }
+
+}
