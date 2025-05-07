c9d94652863741f018806dc75d4902ae616d3361
scx567888 <scx567888@outlook.com>
2025-05-07 11:43:52 +0800

    dev

--- scx-bean/src/main/java/cool/scx/bean/x/BeanCreator.java
+++ scx-bean/src/main/java/cool/scx/bean/x/BeanCreator.java
@@ -2,8 +2,10 @@ package cool.scx.bean.x;
 
 public interface BeanCreator {
     
+    /// 创建 bean
     Object create();
 
+    /// bean 类型
     Class<?> beanClass();
     
 }
--- scx-bean/src/main/java/cool/scx/bean/x/BeanDefinition.java
+++ scx-bean/src/main/java/cool/scx/bean/x/BeanDefinition.java
@@ -1,9 +1,16 @@
 package cool.scx.bean.x;
 
+import cool.scx.reflect.ConstructorInfo;
+
 public interface BeanDefinition {
 
+    /// 是否单例模式
     boolean singleton();
 
+    /// bean 类型
     Class<?> beanClass();
-    
+
+    /// 首选构造函数
+    ConstructorInfo preferredConstructor();
+
 }
