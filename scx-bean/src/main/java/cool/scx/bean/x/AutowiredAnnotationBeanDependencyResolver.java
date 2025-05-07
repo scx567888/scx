83bf7c46ffb924f6f0914a9372a09e375e93488c
scx567888 <scx567888@outlook.com>
2025-05-07 21:08:22 +0800

    dev

---
+++ scx-bean/src/main/java/cool/scx/bean/x/AutowiredAnnotationBeanDependencyResolver.java
@@ -0,0 +1,49 @@
+package cool.scx.bean.x;
+
+import cool.scx.reflect.FieldInfo;
+import cool.scx.reflect.MethodInfo;
+import cool.scx.reflect.ParameterInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.lang.annotation.Annotation;
+
+public class AutowiredAnnotationBeanDependencyResolver implements BeanDependencyResolver {
+
+    private final BeanFactory beanFactory;
+
+    public AutowiredAnnotationBeanDependencyResolver(BeanFactory beanFactory) {
+        this.beanFactory = beanFactory;
+    }
+
+    @Override
+    public Object resolveConstructorArgument(ParameterInfo parameter) {
+        Annotation[] annotations = parameter.allAnnotations();
+        // 没有任何其他注解我们就当作是必须设置的, 强制获取
+        if (annotations.length == 0) {
+            return beanFactory.getBean(parameter.parameter().getType());
+        } else {
+            // 只有一个注解 并且这个注解 还是 Autowired, 一样强制获取
+            if (annotations.length == 1 && annotations[0] instanceof Autowired autowired) {
+                return beanFactory.getBean(parameter.parameter().getType());
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Object resolveFieldValue(FieldInfo fieldInfo) {
+        //只处理有 Autowired 注解的
+        var annotation = fieldInfo.findAnnotation(Autowired.class);
+        if (annotation == null) {
+            return null;
+        }
+        var type = fieldInfo.field().getType();
+        return beanFactory.getBean(type);
+    }
+
+    @Override
+    public boolean resolveMethod(MethodInfo methodInfo) {
+        return false;
+    }
+
+}
