5d5999183d5586672fb2ec4a137e377d5b4c5b7a
scx567888 <scx567888@outlook.com>
2025-05-08 14:26:25 +0800

    dev

--- scx-bean/src/main/java/cool/scx/bean/BeanFactoryImpl.java
+++ scx-bean/src/main/java/cool/scx/bean/BeanFactoryImpl.java
@@ -1,90 +1,118 @@
 package cool.scx.bean;
 
+import cool.scx.bean.exception.DuplicateBeanNameException;
+import cool.scx.bean.exception.NoSuchBeanException;
+import cool.scx.bean.exception.NoUniqueBeanException;
+import cool.scx.bean.provider.*;
+import cool.scx.bean.resolver.BeanResolver;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
+import static cool.scx.bean.provider.InjectingBeanProvider.InjectionPolicy.ALWAYS;
+import static cool.scx.bean.provider.InjectingBeanProvider.InjectionPolicy.ONCE;
+
+// todo 根据 class getBean 速度优化
 @SuppressWarnings("unchecked")
 public class BeanFactoryImpl implements BeanFactory {
 
-    private final Map<String, BeanContext> beanContextMap = new ConcurrentHashMap<>();
-    private final List<BeanDependencyResolver> beanDependencyResolvers = new ArrayList<>();
+    private final Map<String, BeanProvider> beanProviderMap = new ConcurrentHashMap<>();
+    private final List<BeanResolver> beanResolvers = new ArrayList<>();
 
     @Override
-    public Object getBean(String name) {
-        var beanContext = getBeanContext(name);
-        return beanContext.getBean(this);
+    public Object getBean(String name) throws NoSuchBeanException {
+        return findBeanProvider(name).getBean(this);
     }
 
     @Override
-    public <T> T getBean(Class<T> requiredType) {
-        var beanContext = getBeanContext(requiredType);
-        return (T) beanContext.getBean(this);
+    public <T> T getBean(Class<T> requiredType) throws NoSuchBeanException, NoUniqueBeanException {
+        return (T) findBeanProvider(requiredType).getBean(this);
     }
 
     @Override
-    public void registerBean(String name, Object instance) {
-        registerBeanContext(name, new BeanContextImpl(new InstanceBeanCreator(instance), true));
+    public <T> T getBean(String name, Class<T> requiredType) throws NoSuchBeanException {
+        return (T) findBeanProvider(name, requiredType).getBean(this);
     }
 
     @Override
-    public void registerBeanClass(String name, Class<?> beanClass) {
-        // todo 这里是否单例 需要 通过注解判断 暂时全部采用 单例
-        registerBeanContext(name, new BeanContextImpl(new AnnotationConfigBeanCreator(beanClass), true));
+    public void registerBean(String name, Object bean) {
+        registerBeanProvider(name, new InjectingBeanProvider(new InstanceBeanProvider(bean), ONCE));
     }
 
     @Override
-    public void registerBeanContext(String name, BeanContext beanContext) {
-        beanContextMap.put(name, beanContext);
+    public void registerBeanClass(String name, Class<?> beanClass, boolean singleton) throws DuplicateBeanNameException {
+        if (singleton) {
+            registerBeanProvider(name, new InjectingBeanProvider(new SingletonBeanProvider(new AnnotationConfigBeanProvider(beanClass)), ONCE));
+        } else {
+            registerBeanProvider(name, new InjectingBeanProvider(new AnnotationConfigBeanProvider(beanClass), ALWAYS));
+        }
     }
 
     @Override
-    public void addBeanDependencyResolver(BeanDependencyResolver beanDependencyResolver) {
-        this.beanDependencyResolvers.add(beanDependencyResolver);
+    public void registerBeanProvider(String name, BeanProvider beanProvider) throws DuplicateBeanNameException {
+        var oldValue = beanProviderMap.putIfAbsent(name, beanProvider);
+        if (oldValue != null) {
+            throw new DuplicateBeanNameException("重复的 bean name, name = [" + name + "]");
+        }
     }
 
     @Override
-    public List<BeanDependencyResolver> beanDependencyResolvers() {
-        return beanDependencyResolvers;
+    public void addBeanResolver(BeanResolver beanResolver) {
+        this.beanResolvers.add(beanResolver);
+    }
+
+    @Override
+    public List<BeanResolver> beanResolvers() {
+        return beanResolvers;
     }
 
     @Override
     public void initializeBeans() {
-        for (var entry : beanContextMap.values()) {
+        for (var entry : beanProviderMap.values()) {
             entry.getBean(this);
         }
     }
 
     @Override
     public String[] getBeanNames() {
-        return beanContextMap.keySet().toArray(String[]::new);
+        return beanProviderMap.keySet().toArray(String[]::new);
     }
 
-    private BeanContext getBeanContext(String name) {
-        var beanContext = beanContextMap.get(name);
-        if (beanContext == null) {
-            throw new IllegalArgumentException("未找到任何符合名称的 bean !!! name = " + name);
+    private BeanProvider findBeanProvider(String name) throws NoSuchBeanException {
+        var beanProvider = beanProviderMap.get(name);
+        if (beanProvider == null) {
+            throw new NoSuchBeanException("未找到任何符合名称的 bean, name = [" + name + "]");
         }
-        return beanContext;
+        return beanProvider;
     }
 
-    private BeanContext getBeanContext(Class<?> requiredType) {
-        var beanContexts = beanContextMap.values();
-        var list = new ArrayList<BeanContext>();
-        for (var beanContext : beanContexts) {
-            if (requiredType.isAssignableFrom(beanContext.beanClass())) {
-                list.add(beanContext);
+    private BeanProvider findBeanProvider(Class<?> requiredType) throws NoSuchBeanException, NoUniqueBeanException {
+        var beanProviders = beanProviderMap.values();
+        var list = new ArrayList<BeanProvider>();
+        for (var beanProvider : beanProviders) {
+            if (requiredType.isAssignableFrom(beanProvider.beanClass())) {
+                list.add(beanProvider);
             }
         }
         var size = list.size();
         if (size == 0) {
-            throw new IllegalArgumentException("未找到任何符合类型的 bean !!! class = " + requiredType.getName());
+            throw new NoSuchBeanException("未找到任何符合类型的 bean, class = [" + requiredType.getName() + "]");
         }
         if (size > 1) {
-            throw new IllegalArgumentException("找到多个符合类型的 bean !!! class = " + requiredType.getName() + " 已找到 = " + list.stream().map(c -> c.beanClass().getName()).toList());
+            throw new NoUniqueBeanException("找到多个符合类型的 bean, class = [" + requiredType.getName() + "], 已找到 = [" + list.stream().map(c -> c.beanClass().getName()).collect(Collectors.joining(", ")) + "]");
         }
         return list.get(0);
+    }
+
+    private BeanProvider findBeanProvider(String name, Class<?> requiredType) throws NoSuchBeanException, NoUniqueBeanException {
+        var beanProvider = findBeanProvider(name);
+        if (requiredType.isAssignableFrom(beanProvider.beanClass())) {
+            return beanProvider;
+        }
+        throw new NoSuchBeanException("未找到任何符合名称的 bean, name = [" + name + "], class = [" + requiredType.getName() + "]");
     }
 
 }
