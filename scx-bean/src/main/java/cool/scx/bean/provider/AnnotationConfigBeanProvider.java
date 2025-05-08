7c21e96bc230d7f3c3d2d4b467eb9cd010b2da9f
scx567888 <scx567888@outlook.com>
2025-05-08 16:30:56 +0800

    dev

--- scx-bean/src/main/java/cool/scx/bean/provider/AnnotationConfigBeanProvider.java
+++ scx-bean/src/main/java/cool/scx/bean/provider/AnnotationConfigBeanProvider.java
@@ -5,17 +5,11 @@ import cool.scx.reflect.ClassInfoFactory;
 import cool.scx.reflect.ConstructorInfo;
 
 import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayList;
-import java.util.List;
 
 import static cool.scx.reflect.AccessModifier.PUBLIC;
 
-/// 根据 class 创建 bean
-/// todo 此类待优化
+/// 根据 class 创建 Bean
 public class AnnotationConfigBeanProvider implements BeanProvider {
-
-    // 保存依赖链路
-    private static final ThreadLocal<List<AnnotationConfigBeanProvider>> CURRENTLY_CREATING = ThreadLocal.withInitial(ArrayList::new);
 
     private final Class<?> beanClass;
     private final ConstructorInfo constructor;
@@ -34,16 +28,6 @@ public class AnnotationConfigBeanProvider implements BeanProvider {
             }
         }
         throw new IllegalArgumentException("No constructor found for " + beanClass);
-    }
-
-    public static String buildCycleText(List<AnnotationConfigBeanProvider> creatingList, Class<?> beanClass) {
-        // 如果已存在，组装一条依赖链
-        var cycle = new ArrayList<String>();
-        for (var creator : creatingList) {
-            cycle.add(creator.beanClass.getName());
-        }
-        cycle.add(beanClass.getName()); // 再加上自己形成完整回环
-        return String.join(" -> ", cycle);
     }
 
     public Object newInstance(BeanFactory beanFactory) throws InvocationTargetException, InstantiationException, IllegalAccessException {
@@ -72,24 +56,17 @@ public class AnnotationConfigBeanProvider implements BeanProvider {
 
     @Override
     public Object getBean(BeanFactory beanFactory) {
-        var creatingList = CURRENTLY_CREATING.get();
-
-        // 检测循环依赖
-        if (creatingList.contains(this)) {
-            var message = buildCycleText(creatingList, this.beanClass);
-            throw new IllegalStateException("检测到构造函数循环依赖, 依赖链 = [" + message + "]");
-        }
-
-        creatingList.add(this); // 加入正在创建列表
-
         // 创建实例
         try {
             return newInstance(beanFactory);
         } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
             throw new RuntimeException(e);
-        } finally {
-            creatingList.remove(creatingList.size() - 1); // 创建结束，移除自己
         }
+    }
+
+    @Override
+    public boolean singleton() {
+        return false;
     }
 
 }
