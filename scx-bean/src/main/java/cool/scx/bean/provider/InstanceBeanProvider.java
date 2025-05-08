7575c92027105a73ce6218de082b6bde35f68d6d
scx567888 <scx567888@outlook.com>
2025-05-08 13:17:17 +0800

    dev

--- scx-bean/src/main/java/cool/scx/bean/provider/InstanceBeanProvider.java
+++ scx-bean/src/main/java/cool/scx/bean/provider/InstanceBeanProvider.java
@@ -3,7 +3,7 @@ package cool.scx.bean.provider;
 import cool.scx.bean.BeanFactory;
 
 /// 根据一个 已经存在的 Bean 创建
-public record InstanceBeanCreator(Object bean) implements BeanProvider {
+public record InstanceBeanProvider(Object bean) implements BeanProvider {
 
     @Override
     public Object getBean(BeanFactory beanFactory) {
