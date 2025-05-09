cf98b1e9f48f49b9542486e71171507acb686ec7
scx567888 <scx567888@outlook.com>
2025-05-09 12:00:46 +0800

    dev

--- scx-bean/src/main/java/cool/scx/bean/annotation/PreferredConstructor.java
+++ scx-bean/src/main/java/cool/scx/bean/annotation/PreferredConstructor.java
@@ -1,10 +1,13 @@
 package cool.scx.bean.annotation;
 
-import java.lang.annotation.*;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /// 标识注册时优先构造函数
 @Target({ElementType.CONSTRUCTOR})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface PreferredConstructor {
-    
+
 }
