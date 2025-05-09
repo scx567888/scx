064b00bd96b3b9820f7cccbde8c63dcc33d28863
scx567888 <scx567888@outlook.com>
2025-05-09 12:45:53 +0800

    dev

--- scx-bean/src/main/java/cool/scx/bean/provider/annotation_config/Helper.java
+++ scx-bean/src/main/java/cool/scx/bean/provider/annotation_config/Helper.java
@@ -1,33 +1,21 @@
-package cool.scx.bean.provider;
+package cool.scx.bean.provider.annotation_config;
 
 import cool.scx.bean.BeanFactory;
 import cool.scx.bean.annotation.PreferredConstructor;
-import cool.scx.bean.exception.BeanCreationException;
 import cool.scx.bean.exception.NoSuchConstructorException;
 import cool.scx.bean.exception.NoUniqueConstructorException;
 import cool.scx.reflect.ClassInfoFactory;
 import cool.scx.reflect.ConstructorInfo;
+import cool.scx.reflect.ParameterInfo;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
 
 import static cool.scx.reflect.AccessModifier.PUBLIC;
 
-/// 根据 class 创建 Bean
-public class AnnotationConfigBeanProvider implements BeanProvider {
+class Helper {
 
-    // 保存依赖链路
-    private static final ThreadLocal<List<Class<?>>> CURRENTLY_CREATING = ThreadLocal.withInitial(ArrayList::new);
-
-    private final Class<?> beanClass;
-    private final ConstructorInfo constructor;
-
-    public AnnotationConfigBeanProvider(Class<?> beanClass) {
-        this.beanClass = beanClass;
-        this.constructor = findPreferredConstructor(beanClass);
-    }
-
+    /// 查找构造函数
     public static ConstructorInfo findPreferredConstructor(Class<?> beanClass) {
         // 我们只使用 public 的 构造函数
         var classInfo = ClassInfoFactory.getClassInfo(beanClass);
@@ -76,6 +64,7 @@ public class AnnotationConfigBeanProvider implements BeanProvider {
 
     }
 
+    /// 美化构造函数输出
     private static String formatConstructors(List<ConstructorInfo> constructors) {
         var builder = new StringBuilder();
         for (var constructor : constructors) {
@@ -86,66 +75,15 @@ public class AnnotationConfigBeanProvider implements BeanProvider {
         return builder.toString();
     }
 
-    public static String buildCycleText(List<Class<?>> creatingList, Class<?> beanClass) {
-        // 如果已存在，组装一条依赖链
-        var cycle = new ArrayList<String>();
-        for (var creator : creatingList) {
-            cycle.add(creator.getName());
-        }
-        cycle.add(beanClass.getName()); // 再加上自己形成完整回环
-        return String.join(" -> ", cycle);
-    }
-
-    public Object newInstance(BeanFactory beanFactory) throws InvocationTargetException, InstantiationException, IllegalAccessException {
-        //1, 获取参数
-        var parameters = constructor.parameters();
-        //2, 创建参数数组
-        var objects = new Object[parameters.length];
-        for (int i = 0; i < parameters.length; i++) {
-            var parameter = parameters[i];
-            //3, 尝试从 beanResolver 中获取 
-            for (var beanResolver : beanFactory.beanResolvers()) {
-                var value = beanResolver.resolveConstructorArgument(parameter);
-                if (value != null) {
-                    objects[i] = value;
-                    break;
-                }
+    /// 解析构造函数参数
+    public static Object resolveConstructorArgument(BeanFactory beanFactory, ParameterInfo parameter) {
+        for (var beanResolver : beanFactory.beanResolvers()) {
+            var value = beanResolver.resolveConstructorArgument(parameter);
+            if (value != null) {
+                return value;
             }
         }
-        return constructor.newInstance(objects);
-    }
-
-    @Override
-    public Class<?> beanClass() {
-        return beanClass;
-    }
-
-    @Override
-    public Object getBean(BeanFactory beanFactory) {
-        var creatingList = CURRENTLY_CREATING.get();
-
-        // 检测循环依赖
-        if (creatingList.contains(this.beanClass)) {
-            var message = buildCycleText(creatingList, this.beanClass);
-            throw new BeanCreationException("在类 " + this.beanClass.getName() + " 中, 检测到构造函数循环依赖, 依赖链 = [" + message + "]");
-        }
-
-        creatingList.add(this.beanClass); // 加入正在创建列表
-        // 创建实例
-        try {
-            return newInstance(beanFactory);
-        } catch (InstantiationException | IllegalAccessException e) {
-            throw new BeanCreationException("在类 " + this.beanClass.getName() + "中, 创建 bean 时发生异常 ", e);
-        } catch (InvocationTargetException e) {
-            throw new BeanCreationException("在类 " + this.beanClass.getName() + "中, 创建 bean 时发生异常 ", e.getCause());
-        } finally {
-            creatingList.removeLast();
-        }
-    }
-
-    @Override
-    public boolean singleton() {
-        return false;
+        return null;
     }
 
 }
