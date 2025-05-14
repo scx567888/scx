e82319ba931229012edef460eb3fd0e5653406cb
scx567888 <scx567888@outlook.com>
2025-05-14 19:07:53 +0800

    dev

--- scx-data/src/main/java/cool/scx/data/query/Junction.java
+++ scx-data/src/main/java/cool/scx/data/query/Junction.java
@@ -5,29 +5,23 @@ import java.util.List;
 
 import static cool.scx.data.query.WhereType.*;
 
-/// Logic
+/// Junction
 ///
 /// @author scx567888
 /// @version 0.0.1
-public class Logic extends QueryLike<Logic> {
+public abstract sealed class Junction extends QueryLike<Junction> permits And, Or {
 
-    private final LogicType logicType;
     private final List<Object> clauses;
 
-    public Logic(LogicType logicType) {
-        this.logicType = logicType;
+    protected Junction() {
         this.clauses = new ArrayList<>();
-    }
-
-    public LogicType logicType() {
-        return logicType;
     }
 
     public Object[] clauses() {
         return clauses.toArray();
     }
 
-    public Logic add(Object... logicCauses) {
+    public Junction add(Object... logicCauses) {
         for (var logicCause : logicCauses) {
             if (logicCause == null) {
                 continue;
@@ -44,80 +38,80 @@ public class Logic extends QueryLike<Logic> {
         return this;
     }
 
-    public Logic clear() {
+    public Junction clear() {
         clauses.clear();
         return this;
     }
 
-    public final Logic eq(String fieldName, Object value, QueryOption... options) {
+    public final Junction eq(String fieldName, Object value, QueryOption... options) {
         return add(new Where(fieldName, EQUAL, value, null, options));
     }
 
-    public final Logic ne(String fieldName, Object value, QueryOption... options) {
+    public final Junction ne(String fieldName, Object value, QueryOption... options) {
         return add(new Where(fieldName, NOT_EQUAL, value, null, options));
     }
 
-    public final Logic lt(String fieldName, Object value, QueryOption... options) {
+    public final Junction lt(String fieldName, Object value, QueryOption... options) {
         return add(new Where(fieldName, LESS_THAN, value, null, options));
     }
 
-    public final Logic le(String fieldName, Object value, QueryOption... options) {
+    public final Junction le(String fieldName, Object value, QueryOption... options) {
         return add(new Where(fieldName, LESS_THAN_OR_EQUAL, value, null, options));
     }
 
-    public final Logic gt(String fieldName, Object value, QueryOption... options) {
+    public final Junction gt(String fieldName, Object value, QueryOption... options) {
         return add(new Where(fieldName, GREATER_THAN, value, null, options));
     }
 
-    public final Logic ge(String fieldName, Object value, QueryOption... options) {
+    public final Junction ge(String fieldName, Object value, QueryOption... options) {
         return add(new Where(fieldName, GREATER_THAN_OR_EQUAL, value, null, options));
     }
 
-    public final Logic isNull(String fieldName, QueryOption... options) {
+    public final Junction isNull(String fieldName, QueryOption... options) {
         return add(new Where(fieldName, IS_NULL, null, null, options));
     }
 
-    public final Logic isNotNull(String fieldName, QueryOption... options) {
+    public final Junction isNotNull(String fieldName, QueryOption... options) {
         return add(new Where(fieldName, IS_NOT_NULL, null, null, options));
     }
 
-    public final Logic like(String fieldName, Object value, QueryOption... options) {
+    public final Junction like(String fieldName, Object value, QueryOption... options) {
         return add(new Where(fieldName, LIKE, value, null, options));
     }
 
-    public final Logic notLike(String fieldName, Object value, QueryOption... options) {
+    public final Junction notLike(String fieldName, Object value, QueryOption... options) {
         return add(new Where(fieldName, NOT_LIKE, value, null, options));
     }
 
-    public final Logic likeRegex(String fieldName, String value, QueryOption... options) {
+    public final Junction likeRegex(String fieldName, String value, QueryOption... options) {
         return add(new Where(fieldName, LIKE_REGEX, value, null, options));
     }
 
-    public final Logic notLikeRegex(String fieldName, String value, QueryOption... options) {
+    public final Junction notLikeRegex(String fieldName, String value, QueryOption... options) {
         return add(new Where(fieldName, NOT_LIKE_REGEX, value, null, options));
     }
 
-    public final Logic in(String fieldName, Object value, QueryOption... options) {
+    public final Junction in(String fieldName, Object value, QueryOption... options) {
         return add(new Where(fieldName, IN, value, null, options));
     }
 
-    public final Logic notIn(String fieldName, Object value, QueryOption... options) {
+    public final Junction notIn(String fieldName, Object value, QueryOption... options) {
         return add(new Where(fieldName, NOT_IN, value, null, options));
     }
 
-    public final Logic between(String fieldName, Object value1, Object value2, QueryOption... options) {
+    public final Junction between(String fieldName, Object value1, Object value2, QueryOption... options) {
         return add(new Where(fieldName, BETWEEN, value1, value2, options));
     }
 
-    public final Logic notBetween(String fieldName, Object value1, Object value2, QueryOption... options) {
+    public final Junction notBetween(String fieldName, Object value1, Object value2, QueryOption... options) {
         return add(new Where(fieldName, NOT_BETWEEN, value1, value2, options));
     }
 
-    public final Logic jsonContains(String fieldName, Object value, QueryOption... options) {
+    public final Junction jsonContains(String fieldName, Object value, QueryOption... options) {
         return add(new Where(fieldName, JSON_CONTAINS, value, null, options));
     }
 
-    public final Logic jsonOverlaps(String fieldName, Object value, QueryOption... options) {
+    public final Junction jsonOverlaps(String fieldName, Object value, QueryOption... options) {
         return add(new Where(fieldName, JSON_OVERLAPS, value, null, options));
     }
 
