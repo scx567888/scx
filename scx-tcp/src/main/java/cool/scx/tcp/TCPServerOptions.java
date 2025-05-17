8998571351c8befc63f0707411e4dd04a86fa073
scx567888 <scx567888@outlook.com>
2025-05-17 17:25:30 +0800

    dev

--- scx-tcp/src/main/java/cool/scx/tcp/TCPServerOptions.java
+++ scx-tcp/src/main/java/cool/scx/tcp/TCPServerOptions.java
@@ -1,7 +1,5 @@
 package cool.scx.tcp;
 
-import cool.scx.tcp.tls.TLS;
-
 /// TCP 服务器 配置
 ///
 /// @author scx567888
@@ -9,22 +7,13 @@ import cool.scx.tcp.tls.TLS;
 public class TCPServerOptions {
 
     private int backlog;
-    private TLS tls;
-    private boolean autoUpgradeToTLS;
-    private boolean autoHandshake;
 
     public TCPServerOptions() {
         this.backlog = 128; // 默认背压大小 128
-        this.tls = null; // 默认没有 tls
-        this.autoUpgradeToTLS = true; // 自动升级到 TLS
-        this.autoHandshake = true; // 自动握手
     }
 
     public TCPServerOptions(TCPServerOptions oldOptions) {
         backlog(oldOptions.backlog());
-        tls(oldOptions.tls());
-        autoUpgradeToTLS(oldOptions.autoUpgradeToTLS());
-        autoHandshake(oldOptions.autoHandshake());
     }
 
     public int backlog() {
@@ -33,33 +22,6 @@ public class TCPServerOptions {
 
     public TCPServerOptions backlog(int backlog) {
         this.backlog = backlog;
-        return this;
-    }
-
-    public TLS tls() {
-        return tls;
-    }
-
-    public TCPServerOptions tls(TLS tls) {
-        this.tls = tls;
-        return this;
-    }
-
-    public boolean autoUpgradeToTLS() {
-        return autoUpgradeToTLS;
-    }
-
-    public TCPServerOptions autoUpgradeToTLS(boolean autoUpgradeToTLS) {
-        this.autoUpgradeToTLS = autoUpgradeToTLS;
-        return this;
-    }
-
-    public boolean autoHandshake() {
-        return autoHandshake;
-    }
-
-    public TCPServerOptions autoHandshake(boolean autoHandshake) {
-        this.autoHandshake = autoHandshake;
         return this;
     }
 
