34e641185bcafc1e8a2ba1cde83e31c8d8f85109
scx567888 <scx567888@outlook.com>
2025-05-19 14:15:47 +0800

    dev

---
+++ scx-data/src/main/java/cool/scx/data/aggregation_definition/AggregationDefinitionBuilder.java
@@ -0,0 +1,17 @@
+package cool.scx.data.aggregation_definition;
+
+public class AggregationDefinitionBuilder {
+
+    public static AggregationDefinition groupBy(String fieldName) {
+        return new AggregationDefinitionImpl().groupBy(fieldName);
+    }
+
+    public static AggregationDefinition groupBy(String fieldName, String expression) {
+        return new AggregationDefinitionImpl().groupBy(fieldName, expression);
+    }
+
+    public static AggregationDefinition aggregateColumn(String fieldName, String expression) {
+        return new AggregationDefinitionImpl().aggregateColumn(fieldName, expression);
+    }
+
+}
