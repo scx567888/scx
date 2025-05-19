86fbcd1bae9eb6933b1fcf9dc5689a50156880b4
scx567888 <scx567888@outlook.com>
2025-05-19 18:53:24 +0800

    dev

--- scx-data/src/main/java/cool/scx/data/query/serializer/QueryDeserializer.java
+++ scx-data/src/main/java/cool/scx/data/query/serializer/QueryDeserializer.java
@@ -3,8 +3,11 @@ package cool.scx.data.query.serializer;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import cool.scx.common.util.ObjectUtils;
-import cool.scx.data.query.Query;
-import cool.scx.data.query.QueryImpl;
+import cool.scx.data.query.*;
+
+import java.util.ArrayList;
+
+import static cool.scx.common.util.ObjectUtils.convertValue;
 
 /// QueryDeserializer
 ///
@@ -14,27 +17,19 @@ public class QueryDeserializer {
 
     public static final QueryDeserializer QUERY_DESERIALIZER = new QueryDeserializer();
 
-    private final WhereDeserializer whereDeserializer;
-    private final OrderByDeserializer orderByDeserializer;
-
-    public QueryDeserializer() {
-        this.whereDeserializer = new WhereDeserializer();
-        this.orderByDeserializer = new OrderByDeserializer();
-    }
-
     public Query fromJson(String json) throws JsonProcessingException {
         var v = ObjectUtils.jsonMapper().readTree(json);
         return deserializeQuery(v);
     }
 
-    public Object deserialize(JsonNode v) {
+    public Query deserialize(JsonNode v) {
         if (v.isObject()) {
             var type = v.get("@type").asText();
             if (type.equals("Query")) {
                 return deserializeQuery(v);
             }
         }
-        return null;
+        throw new IllegalArgumentException("Unknown query type: " + v);
     }
 
     public Query deserializeQuery(JsonNode objectNode) {
@@ -42,21 +37,117 @@ public class QueryDeserializer {
         if (objectNode == null) {
             return query;
         }
-        if (objectNode.get("where") != null && !objectNode.get("where").isNull()) {
-            var where = whereDeserializer.deserialize(objectNode.get("where"));
+
+        var whereNode = objectNode.get("where");
+        var orderByNode = objectNode.get("orderBy");
+        var offsetNode = objectNode.get("offset");
+        var limitNode = objectNode.get("limit");
+        if (whereNode!= null && !whereNode.isNull()) {
+            var where = deserializeWhere(whereNode);
             query.where(where);
         }
-        if (objectNode.get("orderBy") != null && !objectNode.get("orderBy").isNull()) {
-            var orderBy = orderByDeserializer.deserialize(objectNode.path("orderBy"));
+        if (orderByNode != null && !orderByNode.isNull()) {
+            var orderBy = deserializeOrderBy(orderByNode);
             query.orderBy(orderBy);
         }
-        if (objectNode.get("offset") != null && !objectNode.get("offset").isNull()) {
-            query.offset(objectNode.get("offset").asLong());
+        if (offsetNode != null && !offsetNode.isNull()) {
+            query.offset(offsetNode.asLong());
         }
-        if (objectNode.get("limit") != null && !objectNode.get("limit").isNull()) {
-            query.limit(objectNode.get("limit").asLong());
+        if (limitNode != null && !limitNode.isNull()) {
+            query.limit(limitNode.asLong());
         }
         return query;
+    }
+
+    public Object deserializeOrderBy(JsonNode v) {
+        if (v.isObject()) {
+            var type = v.get("@type").asText();
+            return switch (type) {
+                case "OrderBy" -> deserializeOrderBy0(v);
+                default -> v;
+            };
+        } else if (v.isTextual()) {
+            return deserializeString(v);
+        } else if (v.isArray()) {
+            return deserializeOrderByAll(v);
+        }
+        return null;
+    }
+
+    private OrderBy deserializeOrderBy0(JsonNode v) {
+        var name = v.get("name").asText();
+        var orderByType = convertValue(v.get("orderByType"), OrderByType.class);
+        var info = convertValue(v.path("info"), QueryOption.Info.class);
+        return new OrderBy(name, orderByType, info);
+    }
+
+    private String deserializeString(JsonNode v) {
+        return v.textValue();
+    }
+
+    private Object[] deserializeOrderByAll(JsonNode v) {
+        var s = new ArrayList<>();
+        for (var jsonNode : v) {
+            s.add(deserializeOrderBy(jsonNode));
+        }
+        return s.toArray();
+    }
+
+    public Object deserializeWhere(JsonNode v) {
+        if (v.isObject()) {
+            var type = v.get("@type").asText();
+            return switch (type) {
+                case "And" -> deserializeAnd(v);
+                case "Or" -> deserializeOr(v);
+                case "Not" -> deserializeNot(v);
+                case "WhereClause" -> deserializeWhereClause(v);
+                case "Where" -> deserializeWhere0(v);
+                default -> v;
+            };
+        } else if (v.isTextual()) {
+            return deserializeString(v);
+        } else if (v.isArray()) {
+            return deserializeWhereAll(v);
+        }
+        return null;
+    }
+
+    private Junction deserializeAnd(JsonNode v) {
+        var clauses = deserializeWhereAll(v.get("clauses"));
+        return new And().add(clauses);
+    }
+
+    private Junction deserializeOr(JsonNode v) {
+        var clauses = deserializeWhereAll(v.get("clauses"));
+        return new Or().add(clauses);
+    }
+
+    private Not deserializeNot(JsonNode v) {
+        var clause = deserializeWhere(v.get("clause"));
+        return new Not(clause);
+    }
+
+    private WhereClause deserializeWhereClause(JsonNode v) {
+        var whereClause = v.get("whereClause").asText();
+        var params = convertValue(v.get("params"), Object[].class);
+        return new WhereClause(whereClause, params);
+    }
+
+    private Where deserializeWhere0(JsonNode v) {
+        var name = v.get("name").asText();
+        var whereType = convertValue(v.get("whereType"), WhereType.class);
+        var value1 = convertValue(v.get("value1"), Object.class);
+        var value2 = convertValue(v.get("value2"), Object.class);
+        var info = convertValue(v.get("info"), QueryOption.Info.class);
+        return new Where(name, whereType, value1, value2, info);
+    }
+
+    private Object[] deserializeWhereAll(JsonNode v) {
+        var s = new ArrayList<>();
+        for (var jsonNode : v) {
+            s.add(deserializeWhere(jsonNode));
+        }
+        return s.toArray();
     }
 
 }
