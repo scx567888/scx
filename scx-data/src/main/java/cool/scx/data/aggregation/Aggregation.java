85e9bc705964c8538cfbb0bc48bd5fe1d54b562e
scx567888 <scx567888@outlook.com>
2025-05-19 18:03:55 +0800

    dev

---
+++ scx-data/src/main/java/cool/scx/data/aggregation/Aggregation.java
@@ -0,0 +1,38 @@
+package cool.scx.data.aggregation;
+
+import java.util.Map;
+
+/// 聚合定义
+public interface Aggregation {
+
+    /// 设置分组
+    Aggregation groupBy(String fieldName);
+
+    /// 设置分组
+    Aggregation groupBy(String fieldName, GroupByOption... groupByOptions);
+
+    /// 设置分组
+    Aggregation groupBy(String fieldName, String expression);
+
+    /// 获取 分组列
+    Map<String, GroupBy> groupBys();
+
+    /// 移除 某个分组
+    Aggregation removeGroupBy(String fieldName);
+
+    /// 清除 所有分组
+    Aggregation clearGroupBys();
+
+    /// 设置聚合列
+    Aggregation agg(String fieldName, String expression);
+
+    /// 获取聚合列
+    Map<String, String> aggs();
+
+    /// 清除 某个聚合列
+    Aggregation removeAgg(String fieldName);
+
+    /// 清除 所有聚合列
+    Aggregation clearAggs();
+
+}
