2de7033224a972b6458470aeaa1d15d9b2247855
scx567888 <scx567888@outlook.com>
2025-05-20 11:04:38 +0800

    dev

--- scx-data/src/main/java/cool/scx/data/query/OrderBy.java
+++ scx-data/src/main/java/cool/scx/data/query/OrderBy.java
@@ -1,8 +1,10 @@
 package cool.scx.data.query;
 
+import cool.scx.data.build_control.BuildControl;
+import cool.scx.data.build_control.BuildControlInfo;
+
 import static cool.scx.common.util.StringUtils.isBlank;
-import static cool.scx.data.query.QueryOption.Info;
-import static cool.scx.data.query.QueryOption.ofInfo;
+import static cool.scx.data.build_control.BuildControlInfo.ofInfo;
 
 /// OrderBy
 ///
@@ -10,40 +12,41 @@ import static cool.scx.data.query.QueryOption.ofInfo;
 /// @version 0.0.1
 public final class OrderBy extends QueryLike<OrderBy> {
 
-    private final String name;
+    // fieldName 或者 表达式
+    private final String selector;
     private final OrderByType orderByType;
-    private final Info info;
+    private final BuildControlInfo info;
 
-    public OrderBy(String name, OrderByType orderByType, Info info) {
-        if (isBlank(name)) {
-            throw new IllegalArgumentException("OrderBy 参数错误 : 名称 不能为空 !!!");
+    public OrderBy(String selector, OrderByType orderByType, BuildControlInfo info) {
+        if (isBlank(selector)) {
+            throw new IllegalArgumentException("OrderBy 参数错误 : selector 不能为空 !!!");
         }
         if (orderByType == null) {
             throw new IllegalArgumentException("OrderBy 参数错误 : orderByType 不能为空 !!!");
         }
-        this.name = name;
+        this.selector = selector;
         this.orderByType = orderByType;
         this.info = info;
     }
 
     /// 添加一个排序字段
     ///
-    /// @param name        排序字段的名称 (默认是实体类的字段名 , 不是数据库中的字段名)
+    /// @param selector    selector
     /// @param orderByType 排序类型 正序或倒序
-    /// @param options     配置
-    public OrderBy(String name, OrderByType orderByType, QueryOption... options) {
-        this(name, orderByType, ofInfo(options));
+    /// @param controls    配置
+    public OrderBy(String selector, OrderByType orderByType, BuildControl... controls) {
+        this(selector, orderByType, ofInfo(controls));
     }
 
-    public String name() {
-        return name;
+    public String selector() {
+        return selector;
     }
 
     public OrderByType orderByType() {
         return orderByType;
     }
 
-    public Info info() {
+    public BuildControlInfo info() {
         return info;
     }
 
