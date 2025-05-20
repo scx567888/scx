9be35292235c4710c6242b1078ef286e113c2c1b
scx567888 <scx567888@outlook.com>
2025-05-20 19:37:31 +0800

    dev

--- scx-data/src/test/java/cool/scx/data/test/FieldPolicyTest.java
+++ scx-data/src/test/java/cool/scx/data/test/FieldPolicyTest.java
@@ -4,9 +4,9 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import static cool.scx.data.field_policy.FieldPolicyBuilder.exclude;
-import static cool.scx.data.field_policy.serializer.FieldPolicyDeserializer.FIELD_POLICY_DESERIALIZER;
-import static cool.scx.data.field_policy.serializer.FieldPolicySerializer.FIELD_POLICY_SERIALIZER;
+import static cool.scx.data.field_policy.FieldPolicyBuilder.updateExclude;
+import static cool.scx.data.field_policy.serializer.UpdateFieldPolicyDeserializer.UPDATE_FIELD_POLICY_DESERIALIZER;
+import static cool.scx.data.field_policy.serializer.UpdateFieldPolicySerializer.UPDATE_FIELD_POLICY_SERIALIZER;
 
 public class FieldPolicyTest {
 
@@ -16,17 +16,17 @@ public class FieldPolicyTest {
 
     @Test
     public static void test1() throws JsonProcessingException {
-        var fieldPolicy = exclude("a", "b", "c", "d", "e", "f", "g", "h", "i")
+        var fieldPolicy = updateExclude("a", "b", "c", "d", "e", "f", "g", "h", "i")
                 .ignoreNull(false)
                 .ignoreNull("name", true)
                 .expression("w", "w * 2");
-        var json = FIELD_POLICY_SERIALIZER.toJson(fieldPolicy);
-        var newFieldPolicy = FIELD_POLICY_DESERIALIZER.fromJson(json);
-        Assert.assertEquals(fieldPolicy.fieldNames(), newFieldPolicy.fieldNames());
-        Assert.assertEquals(fieldPolicy.filterMode(), newFieldPolicy.filterMode());
-        Assert.assertEquals(fieldPolicy.ignoreNull(), newFieldPolicy.ignoreNull());
-        Assert.assertEquals(fieldPolicy.ignoreNulls(), newFieldPolicy.ignoreNulls());
-        Assert.assertEquals(fieldPolicy.expressions(), newFieldPolicy.expressions());
+        var json = UPDATE_FIELD_POLICY_SERIALIZER.toJson(fieldPolicy);
+        var newFieldPolicy = UPDATE_FIELD_POLICY_DESERIALIZER.fromJson(json);
+        Assert.assertEquals(fieldPolicy.getFieldNames(), newFieldPolicy.getFieldNames());
+        Assert.assertEquals(fieldPolicy.getFilterMode(), newFieldPolicy.getFilterMode());
+        Assert.assertEquals(fieldPolicy.getIgnoreNull(), newFieldPolicy.getIgnoreNull());
+        Assert.assertEquals(fieldPolicy.getIgnoreNulls(), newFieldPolicy.getIgnoreNulls());
+        Assert.assertEquals(fieldPolicy.getExpressions(), newFieldPolicy.getExpressions());
     }
 
 }
