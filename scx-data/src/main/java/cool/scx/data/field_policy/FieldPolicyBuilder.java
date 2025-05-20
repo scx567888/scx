cf12087cbd9ff49401c4382e7bf6bb7d307fab35
scx567888 <scx567888@outlook.com>
2025-05-20 19:19:20 +0800

    dev

--- scx-data/src/main/java/cool/scx/data/field_policy/FieldPolicyBuilder.java
+++ scx-data/src/main/java/cool/scx/data/field_policy/FieldPolicyBuilder.java
@@ -9,35 +9,59 @@ import static cool.scx.data.field_policy.FilterMode.INCLUDED;
 /// @version 0.0.1
 public class FieldPolicyBuilder {
 
-    public static FieldPolicy includeAll() {
-        return new FieldPolicyImpl(EXCLUDED);
+    public static QueryFieldPolicy queryIncludeAll() {
+        return new QueryFieldPolicyImpl(EXCLUDED);
     }
 
-    public static FieldPolicy excludeAll() {
-        return new FieldPolicyImpl(INCLUDED);
+    public static QueryFieldPolicy queryExcludeAll() {
+        return new QueryFieldPolicyImpl(INCLUDED) {};
     }
 
-    public static FieldPolicy include(String... fieldNames) {
-        return excludeAll().include(fieldNames);
+    public static QueryFieldPolicy queryInclude(String... fieldNames) {
+        return queryExcludeAll().include(fieldNames);
     }
 
-    public static FieldPolicy exclude(String... fieldNames) {
-        return includeAll().exclude(fieldNames);
+    public static QueryFieldPolicy queryExclude(String... fieldNames) {
+        return queryIncludeAll().exclude(fieldNames);
+    }
+
+    public static UpdateFieldPolicy updateIncludeAll() {
+        return new UpdateFieldPolicyImpl(EXCLUDED);
+    }
+
+    public static UpdateFieldPolicy updateExcludeAll() {
+        return new UpdateFieldPolicyImpl(INCLUDED);
+    }
+
+    public static UpdateFieldPolicy updateInclude(String... fieldNames) {
+        return updateExcludeAll().include(fieldNames);
+    }
+
+    public static UpdateFieldPolicy updateExclude(String... fieldNames) {
+        return updateIncludeAll().exclude(fieldNames);
     }
 
     /// 默认包含所有
-    public static FieldPolicy ignoreNull(boolean ignoreNull) {
-        return includeAll().ignoreNull(ignoreNull);
+    public static UpdateFieldPolicy ignoreNull(boolean ignoreNull) {
+        return updateIncludeAll().ignoreNull(ignoreNull);
     }
 
     /// 默认包含所有
-    public static FieldPolicy ignoreNull(String fieldName, boolean ignoreNull) {
-        return includeAll().ignoreNull(fieldName, ignoreNull);
+    public static UpdateFieldPolicy ignoreNull(String fieldName, boolean ignoreNull) {
+        return updateIncludeAll().ignoreNull(fieldName, ignoreNull);
     }
 
     /// 默认包含所有
-    public static FieldPolicy expression(String fieldName, String expression) {
-        return includeAll().expression(fieldName, expression);
+    public static UpdateFieldPolicy expression(String fieldName, String expression) {
+        return updateIncludeAll().expression(fieldName, expression);
+    }
+
+    public static QueryFieldPolicy virtualField(String expression, String virtualFieldName) {
+        return queryIncludeAll().virtualField(expression, virtualFieldName);
+    }
+    
+    public static QueryFieldPolicy virtualField(String expression) {
+        return queryIncludeAll().virtualField(expression);
     }
 
 }
