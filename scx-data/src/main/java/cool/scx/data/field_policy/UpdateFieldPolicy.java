e6dbf03febe67faacf6b42121b1109a1ab3fb0ec
scx567888 <scx567888@outlook.com>
2025-05-20 19:08:41 +0800

    dev

---
+++ scx-data/src/main/java/cool/scx/data/field_policy/UpdateFieldPolicy.java
@@ -0,0 +1,38 @@
+package cool.scx.data.field_policy;
+
+import java.util.Map;
+
+/// 更新字段策略
+public interface UpdateFieldPolicy extends FieldPolicy<UpdateFieldPolicy> {
+
+    /// 设置 全局 忽略空值
+    UpdateFieldPolicy ignoreNull(boolean ignoreNull);
+
+    /// 设置 忽略空值
+    UpdateFieldPolicy ignoreNull(String fieldName, boolean ignoreNull);
+
+    /// 设置 字段表达式 (用于插入和更新)
+    UpdateFieldPolicy expression(String fieldName, String expression);
+
+    /// 获取 全局是否忽略 空值
+    boolean getIgnoreNull();
+
+    /// 获取 忽略 空值
+    Map<String, Boolean> getIgnoreNulls();
+
+    /// 获取 字段表达式
+    Map<String, String> getExpressions();
+
+    /// 清除 所有忽略空值
+    UpdateFieldPolicy clearIgnoreNulls();
+
+    /// 清除 所有表达式
+    UpdateFieldPolicy clearExpressions();
+
+    /// 移除 是否忽略 空值
+    UpdateFieldPolicy removeIgnoreNull(String fieldName);
+
+    /// 清除 某个表达式
+    UpdateFieldPolicy removeExpression(String fieldName);
+
+}
