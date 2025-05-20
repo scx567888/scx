07e14f7a744bea77212b10fce680e4e5f5a1ef2e
scx567888 <scx567888@outlook.com>
2025-05-20 19:34:26 +0800

    dev

---
+++ scx-data/src/main/java/cool/scx/data/field_policy/serializer/QueryFieldPolicySerializer.java
@@ -0,0 +1,37 @@
+package cool.scx.data.field_policy.serializer;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import cool.scx.common.util.ObjectUtils;
+import cool.scx.data.field_policy.QueryFieldPolicy;
+import cool.scx.data.field_policy.UpdateFieldPolicy;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/// FieldPolicySerializer
+///
+/// @author scx567888
+/// @version 0.0.1
+public class QueryFieldPolicySerializer {
+
+    public static final QueryFieldPolicySerializer QUERY_FIELD_POLICY_SERIALIZER = new QueryFieldPolicySerializer();
+
+    public String toJson(QueryFieldPolicy fieldPolicy) throws JsonProcessingException {
+        var v = serialize(fieldPolicy);
+        return ObjectUtils.jsonMapper().writeValueAsString(v);
+    }
+
+    public Map<String, Object> serialize(QueryFieldPolicy fieldPolicy) {
+        return serializeQueryFieldPolicy(fieldPolicy);
+    }
+
+    private Map<String, Object> serializeQueryFieldPolicy(QueryFieldPolicy fieldPolicy) {
+        var m = new LinkedHashMap<String, Object>();
+        m.put("@type", "QueryFieldPolicy");
+        m.put("filterMode", fieldPolicy.getFilterMode());
+        m.put("fieldNames", fieldPolicy.getFieldNames());
+        m.put("virtualFields", fieldPolicy.getVirtualFields());
+        return m;
+    }
+
+}
