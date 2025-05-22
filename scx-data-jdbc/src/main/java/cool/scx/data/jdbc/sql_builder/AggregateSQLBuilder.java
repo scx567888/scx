62d716c6a9ad2aaad70509330007d8b90e943b39
scx567888 <scx567888@outlook.com>
2025-05-22 09:31:44 +0800

    dev

--- scx-data-jdbc/src/main/java/cool/scx/data/jdbc/sql_builder/AggregateSQLBuilder.java
+++ scx-data-jdbc/src/main/java/cool/scx/data/jdbc/sql_builder/AggregateSQLBuilder.java
@@ -37,43 +37,46 @@ public class AggregateSQLBuilder {
         Agg[] aggs = aggregation.getAggs();
         // 需要 分组的列
         GroupBy[] groupBys = aggregation.getGroupBys();
-        //1, 过滤查询列
-        var selectColumns = filterByQueryFieldPolicy(fieldPolicy, table);
-        //2, 创建虚拟查询列
-        var virtualSelectColumns = createVirtualSelectColumns(fieldPolicy, dialect);
-        //3, 创建最终查询列
-        var finalSelectColumns = tryConcatAny(selectColumns, (Object[]) virtualSelectColumns);
-        //4, 创建 where 子句
-        var whereClause = whereParser.parse(query.getWhere());
-        //6, 创建 orderBy 子句
-        var orderByClauses = orderByParser.parse(query.getOrderBys());
-        //7, 创建 SQL
-        var sql = GetAggregateSQL(finalSelectColumns, whereClause.whereClause(), orderByClauses, query.getOffset(), query.getLimit());
-        return sql(sql, whereClause.params());
+//        //1, 过滤查询列
+//        var selectColumns = filterByQueryFieldPolicy(fieldPolicy, table);
+//        //2, 创建虚拟查询列
+//        var virtualSelectColumns = createVirtualSelectColumns(fieldPolicy, dialect);
+//        //3, 创建最终查询列
+//        var finalSelectColumns = tryConcatAny(selectColumns, (Object[]) virtualSelectColumns);
+//        //4, 创建 where 子句
+//        var whereClause = whereParser.parse(query.getWhere());
+//        //6, 创建 orderBy 子句
+//        var orderByClauses = orderByParser.parse(query.getOrderBys());
+//        //7, 创建 SQL
+//        var sql = GetAggregateSQL(finalSelectColumns, whereClause.whereClause(), orderByClauses, query.getOffset(), query.getLimit());
+//        return sql(sql, whereClause.params());
+        return null;
     }
 
     public SQL buildAggregateFirstSQL(Query beforeAggregateQuery, Aggregation aggregation, Query afterAggregateQuery) {
-        //1, 过滤查询列
-        var selectColumns = filterByQueryFieldPolicy(fieldPolicy, table);
-        //2, 创建虚拟查询列
-        var virtualSelectColumns = createVirtualSelectColumns(fieldPolicy, dialect);
-        //3, 创建最终查询列
-        var finalSelectColumns = tryConcatAny(selectColumns, (Object[]) virtualSelectColumns);
-        //4, 创建 where 子句
-        var whereClause = whereParser.parse(query.getWhere());
-        //6, 创建 orderBy 子句
-        var orderByClauses = orderByParser.parse(query.getOrderBys());
-        //7, 创建 SQL
-        var sql = GetAggregateSQL(finalSelectColumns, whereClause.whereClause(), orderByClauses, null, 1L);
-        return sql(sql, whereClause.params());
+//        //1, 过滤查询列
+//        var selectColumns = filterByQueryFieldPolicy(fieldPolicy, table);
+//        //2, 创建虚拟查询列
+//        var virtualSelectColumns = createVirtualSelectColumns(fieldPolicy, dialect);
+//        //3, 创建最终查询列
+//        var finalSelectColumns = tryConcatAny(selectColumns, (Object[]) virtualSelectColumns);
+//        //4, 创建 where 子句
+//        var whereClause = whereParser.parse(query.getWhere());
+//        //6, 创建 orderBy 子句
+//        var orderByClauses = orderByParser.parse(query.getOrderBys());
+//        //7, 创建 SQL
+//        var sql = GetAggregateSQL(finalSelectColumns, whereClause.whereClause(), orderByClauses, null, 1L);
+//        return sql(sql, whereClause.params());
+        return null;
     }
 
     private String GetAggregateSQL(Object[] selectColumns, String whereClause, String[] orderByClauses, Long offset, Long limit) {
         if (selectColumns.length == 0) {
             throw new IllegalArgumentException("Select 子句错误 : 待查询的数据列 不能为空 !!!");
         }
-        var sql = "SELECT " + getSelectColumns(selectColumns) + " FROM " + getTableName() + getWhereClause(whereClause) + getOrderByClause(orderByClauses);
-        return dialect.getLimitSQL(sql, offset, limit);
+//        var sql = "SELECT " + getSelectColumns(selectColumns) + " FROM " + getTableName() + getWhereClause(whereClause) + getOrderByClause(orderByClauses);
+//        return dialect.getLimitSQL(sql, offset, limit);
+        return null;
     }
 
 }
