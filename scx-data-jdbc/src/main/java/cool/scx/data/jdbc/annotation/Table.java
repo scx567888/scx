c2b6ce56bddec5f8332b7e85fbe21e10b2ba1c84
scx567888 <scx567888@outlook.com>
2025-05-25 21:18:43 +0800

    dev

--- scx-data-jdbc/src/main/java/cool/scx/data/jdbc/annotation/Table.java
+++ scx-data-jdbc/src/main/java/cool/scx/data/jdbc/annotation/Table.java
@@ -16,11 +16,11 @@ import static cool.scx.common.constant.AnnotationValue.NULL;
 public @interface Table {
 
     /// 用于标注实体类对应的数据库表名.
-    /// 
+    ///
     /// 如果未显式指定表名 (即 `value` 为空), 将默认使用类名作为表名.
-    /// 
+    ///
     /// 本注解不会进行任何隐式的命名转换, 例如 `UserInfo` 不会自动转换为 `user_info`.
-    /// 
+    ///
     /// 这种设计经过明确考量, 旨在保持命名一致性, 减少认知负担, 并避免因自动转换导致的歧义或错误.
     ///
     /// @return 表的名称（全限定），默认为类名
