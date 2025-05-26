47a29d90439789ee335235840b803a0afd47f742
scx567888 <scx567888@outlook.com>
2025-05-26 11:54:26 +0800

    dev

--- scx-data/src/main/java/cool/scx/data/query/SkipIfInfo.java
+++ scx-data/src/main/java/cool/scx/data/query/SkipIfInfo.java
@@ -3,31 +3,25 @@ package cool.scx.data.query;
 import java.lang.reflect.Array;
 import java.util.Collection;
 
-record BuildControlInfo(boolean skipIfNull,
-                               boolean skipIfEmptyList,
-                               boolean skipIfEmptyString,
-                               boolean skipIfBlankString,
-                               boolean useExpression,
-                               boolean useExpressionValue) {
+public record SkipIfInfo(boolean skipIfNull,
+                         boolean skipIfEmptyList,
+                         boolean skipIfEmptyString,
+                         boolean skipIfBlankString) {
 
-    public static BuildControlInfo ofInfo(BuildControl... controls) {
+    public static SkipIfInfo ofInfo(BuildControl... controls) {
         var skipIfNull = false;
         var skipIfEmptyList = false;
         var skipIfEmptyString = false;
         var skipIfBlankString = false;
-        var useExpression = false;
-        var useExpressionValue = false;
         for (var c : controls) {
             switch (c) {
                 case SKIP_IF_NULL -> skipIfNull = true;
                 case SKIP_IF_EMPTY_LIST -> skipIfEmptyList = true;
                 case SKIP_IF_EMPTY_STRING -> skipIfEmptyString = true;
                 case SKIP_IF_BLANK_STRING -> skipIfBlankString = true;
-                case USE_EXPRESSION -> useExpression = true;
-                case USE_EXPRESSION_VALUE -> useExpressionValue = true;
             }
         }
-        return new BuildControlInfo(skipIfNull, skipIfEmptyList, skipIfEmptyString, skipIfBlankString, useExpression, useExpressionValue);
+        return new SkipIfInfo(skipIfNull, skipIfEmptyList, skipIfEmptyString, skipIfBlankString);
     }
 
     public boolean shouldSkip(Object value1) {
