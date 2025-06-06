3143c7866f76f2b2ce13153c6c586b0b803f4fa2
scx567888 <scx567888@outlook.com>
2025-06-06 15:59:18 +0800

    代码优化

--- scx-common/src/main/java/cool/scx/common/util/ScopedValue.java
+++ scx-common/src/main/java/cool/scx/common/util/ScopedValue.java
@@ -1,11 +1,11 @@
 package cool.scx.common.util;
 
-import cool.scx.common.exception.ScxRuntimeException;
+import cool.scx.common.functional.ScxCallable;
+import cool.scx.common.functional.ScxRunnable;
+import cool.scx.common.functional.ScxSupplier;
 
-import java.util.concurrent.Callable;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReference;
-import java.util.function.Supplier;
 
 /// 使用 threadLocal 模拟的 ScopedValue
 /// todo ScopedValue 正式版本发布时 移除此类
@@ -44,65 +44,67 @@ public final class ScopedValue<T> {
             this.value = value;
         }
 
-        public void run(Runnable op) {
-            var exception = new AtomicReference<Exception>();
+        @SuppressWarnings("unchecked")
+        public <E extends Throwable> void run(ScxRunnable<E> op) throws E {
+            var exception = new AtomicReference<E>();
             var w = Thread.ofPlatform().name("scx-scoped-value-thread-", THREAD_NUMBER.getAndIncrement()).start(() -> {
                 key.bind(value);
                 try {
                     op.run();
-                } catch (Exception e) {
-                    exception.set(e);
+                } catch (Throwable e) {
+                    exception.set((E) e);
                 }
             });
             try {
                 w.join();
             } catch (InterruptedException e) {
-                throw new ScxRuntimeException(e);
+                throw new IllegalStateException(e);
             }
             if (exception.get() != null) {
-                throw new ScxRuntimeException(exception.get());
+                throw exception.get();
             }
         }
 
-        public <R> R get(Supplier<? extends R> op) {
+        @SuppressWarnings("unchecked")
+        public <R, E extends Throwable> R get(ScxSupplier<? extends R, E> op) throws E {
             var result = new AtomicReference<R>();
-            var exception = new AtomicReference<Exception>();
+            var exception = new AtomicReference<E>();
             var w = Thread.ofPlatform().name("scx-scoped-value-thread-", THREAD_NUMBER.getAndIncrement()).start(() -> {
                 key.bind(value);
                 try {
                     var r = op.get();
                     result.set(r);
-                } catch (Exception e) {
-                    exception.set(e);
+                } catch (Throwable e) {
+                    exception.set((E) e);
                 }
             });
             try {
                 w.join();
             } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+                throw new IllegalStateException(e);
             }
             if (exception.get() != null) {
-                throw new ScxRuntimeException(exception.get());
+                throw exception.get();
             }
             return result.get();
         }
 
-        public <R> R call(Callable<? extends R> op) throws Exception {
+        public <R, E extends Throwable> R call(ScxCallable<? extends R, E> op) throws E {
             var result = new AtomicReference<R>();
-            var exception = new AtomicReference<Exception>();
+            var exception = new AtomicReference<E>();
             var w = Thread.ofPlatform().name("scx-scoped-value-thread-", THREAD_NUMBER.getAndIncrement()).start(() -> {
                 key.bind(value);
                 try {
                     var r = op.call();
                     result.set(r);
-                } catch (Exception e) {
-                    exception.set(e);
+                } catch (Throwable e) {
+                    exception.set((E) e);
                 }
             });
             try {
                 w.join();
             } catch (InterruptedException e) {
-                throw new ScxRuntimeException(e);
+                throw new IllegalStateException(e);
             }
             if (exception.get() != null) {
                 throw exception.get();
