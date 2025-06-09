76b68494ed014a7fc34433b7035d7969858f17bd
scx567888 <scx567888@outlook.com>
2025-06-09 21:37:11 +0800

    dev

--- scx-io/src/main/java/cool/scx/io/data_supplier/BoundaryDataSupplier.java
+++ scx-io/src/main/java/cool/scx/io/data_supplier/BoundaryDataSupplier.java
@@ -1,19 +1,13 @@
-package cool.scx.http.media.multi_part;
+package cool.scx.io.data_supplier;
 
 import cool.scx.io.data_indexer.KMPDataIndexer;
 import cool.scx.io.data_node.DataNode;
 import cool.scx.io.data_reader.DataReader;
-import cool.scx.io.data_reader.LinkedDataReader;
-import cool.scx.io.data_supplier.DataSupplier;
-import cool.scx.io.data_supplier.InputStreamDataSupplier;
 import cool.scx.io.exception.DataSupplierException;
 import cool.scx.io.exception.NoMatchFoundException;
 import cool.scx.io.exception.NoMoreDataException;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.util.Arrays;
 
 public class BoundaryDataSupplier implements DataSupplier {
 
@@ -23,21 +17,13 @@ public class BoundaryDataSupplier implements DataSupplier {
     private boolean isFinish = false;
 
     public BoundaryDataSupplier(DataReader dataReader, byte[] boundaryBytes) {
-        this.dataReader = dataReader;
-        this.dataIndexer = new KMPDataIndexer(boundaryBytes);
-        this.bufferLength = Math.max(boundaryBytes.length, 1);
+        this(dataReader, boundaryBytes, 8192);
     }
 
-    public static void main(String[] args) {
-        try (var in = Files.newInputStream(Path.of("C:\\Users\\scx\\Desktop\\AI 桥计划.txt"));) {
-            var s = new LinkedDataReader(new InputStreamDataSupplier(in, 1));
-            var my = new LinkedDataReader(new BoundaryDataSupplier(s, "AI学生打分".getBytes(StandardCharsets.UTF_8)));
-            byte[] bytes = my.read(Integer.MAX_VALUE);
-            var str = new String(bytes, StandardCharsets.UTF_8);
-            System.out.println(str);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    public BoundaryDataSupplier(DataReader dataReader, byte[] boundaryBytes, int bufferLength) {
+        this.dataReader = dataReader;
+        this.dataIndexer = new KMPDataIndexer(boundaryBytes);
+        this.bufferLength = bufferLength;
     }
 
     @Override
@@ -51,13 +37,30 @@ public class BoundaryDataSupplier implements DataSupplier {
             var index = dataReader.indexOf(dataIndexer, bufferLength, Long.MAX_VALUE);
             // 找到了就全部返回 
             var read = dataReader.read((int) index);
-            // 找到了就没必要标识为完成
+            // 找到了就要标识为完成
             isFinish = true;
             return new DataNode(read);
         } catch (NoMatchFoundException e) {
             // 没找到 说明可能还有 继续读取
-            byte[] read = dataReader.read(bufferLength);
-            return new DataNode(read);
+            // 为了防止误读这里检查 patternIndex
+            // 没有匹配任何 可以安全读
+            if (dataIndexer.patternIndex() == 0) {
+                var read = dataReader.read(bufferLength);
+                return new DataNode(read);
+            } else {
+                // 已经匹配到了一部分 检查是否是 真正的匹配
+                var peek = dataReader.peek(dataIndexer.pattern().length);
+                // 完全匹配 返回 null
+                var match = Arrays.equals(peek, dataIndexer.pattern());
+                if (match) {
+                    isFinish = true;
+                    return null;
+                } else {
+                    // 不匹配 继续读 这里注意 skip 索引
+                    dataReader.skip(dataIndexer.pattern().length);
+                    return new DataNode(peek);
+                }
+            }
         } catch (NoMoreDataException e) {
             // 如果底层 DataReader 没数据了，也返回 null
             return null;
