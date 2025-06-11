180b887220fbdc7566d0643191ec7523f237869c
scx567888 <scx567888@outlook.com>
2025-06-11 11:48:22 +0800

    dev

--- scx-byte-reader/src/main/java/cool/scx/byte_reader/consumer/ByteConsumer.java
+++ scx-byte-reader/src/main/java/cool/scx/byte_reader/consumer/ByteConsumer.java
@@ -1,15 +1,14 @@
 package cool.scx.byte_reader.consumer;
 
+import cool.scx.byte_reader.ByteChunk;
+
 /// DataConsumer
 ///
 /// @author scx567888
 /// @version 0.0.1
 public interface ByteConsumer {
 
-    /// @param bytes    字节
-    /// @param position 起始位置
-    /// @param length   长度
     /// @return 是否需要更多数据
-    boolean accept(byte[] bytes, int position, int length);
+    boolean accept(ByteChunk chunk);
 
 }
