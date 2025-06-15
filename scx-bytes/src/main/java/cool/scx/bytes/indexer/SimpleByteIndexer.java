bb834a75623b85d6cb7d0e46557cb3b5b32c7faa
scx567888 <scx567888@outlook.com>
2025-06-15 10:05:22 +0800

    dev

--- scx-bytes/src/main/java/cool/scx/bytes/indexer/SimpleByteIndexer.java
+++ scx-bytes/src/main/java/cool/scx/bytes/indexer/SimpleByteIndexer.java
@@ -1,7 +1,6 @@
 package cool.scx.bytes.indexer;
 
 import cool.scx.bytes.ByteChunk;
-import cool.scx.common.util.ArrayUtils;
 
 /// 单字节查找器
 ///
@@ -17,8 +16,16 @@ public class SimpleByteIndexer implements ByteIndexer {
 
     @Override
     public int indexOf(ByteChunk chunk) {
-        int i = ArrayUtils.indexOf(chunk.bytes, chunk.startPosition, chunk.length, b);
-        return i == -1 ? Integer.MIN_VALUE : i;
+        var bytes = chunk.bytes;
+        var start = chunk.startPosition;
+        var end = chunk.endPosition;
+        //普通 查找
+        for (var i = start; i < end; i = i + 1) {
+            if (bytes[i] == b) {
+                return i - start;
+            }
+        }
+        return Integer.MIN_VALUE;
     }
 
 }
