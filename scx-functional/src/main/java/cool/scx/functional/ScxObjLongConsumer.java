28e711ca107132801e6ca5da010ed3ad4d8ea4d7
scx567888 <scx567888@outlook.com>
2025-06-16 17:19:32 +0800

    dev

---
+++ scx-functional/src/main/java/cool/scx/functional/ScxObjLongConsumer.java
@@ -0,0 +1,19 @@
+package cool.scx.functional;
+
+/// 支持显式抛出异常的 ObjLongConsumer.
+///
+/// @param <T> 参数1类型
+/// @param <E> 异常类型
+/// @author scx567888
+/// @version 0.0.1
+/// @see java.util.function.ObjLongConsumer
+@FunctionalInterface
+public interface ScxObjLongConsumer<T,E extends Throwable> {
+
+    /// Performs this operation on the given arguments.
+    ///
+    /// @param t the first input argument
+    /// @param value the second input argument
+    void accept(T t, long value)  throws E;
+
+}
