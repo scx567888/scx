2551b786dbda9e0234afacf5d9aee42f0bab9c22
scx567888 <scx567888@outlook.com>
2025-06-17 15:13:45 +0800

    dev

--- scx-collections/src/main/java/cool/scx/collections/ArrayIterable.java
+++ scx-collections/src/main/java/cool/scx/collections/ArrayIterable.java
@@ -6,14 +6,22 @@ import java.util.Iterator;
 public class ArrayIterable<T> implements Iterable<T> {
 
     private final T[] array;
+    private final int start;
+    private final int end;
 
-    public ArrayIterable(T... array) {
+    public ArrayIterable(T[] array) {
+        this(array, 0, array.length);
+    }
+
+    public ArrayIterable(T[] array, int start, int end) {
         this.array = array;
+        this.start = start;
+        this.end = end;
     }
 
     @Override
     public Iterator<T> iterator() {
-        return new ArrayIterator<>(array);
+        return new ArrayIterator<>(array, start, end);
     }
 
 }
