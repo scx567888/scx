e948e0ad80008b3240f66938cacd7aeea6d3c8fa
scx567888 <scx567888@outlook.com>
2025-06-17 12:50:43 +0800

    dev

--- scx-http/src/main/java/cool/scx/http/parameters/ParametersIterator.java
+++ scx-http/src/main/java/cool/scx/http/parameters/ParametersIterator.java
@@ -4,12 +4,12 @@ import cool.scx.collections.multi_map.IMultiMapEntry;
 
 import java.util.Iterator;
 
-public class ParametersIterator<K,V> implements Iterator<ParameterEntry<K, V>> {
+public class ParametersIterator<K, V> implements Iterator<ParameterEntry<K, V>> {
 
     private final Iterator<IMultiMapEntry<K, V>> iterator;
 
-    public  ParametersIterator(Iterator<IMultiMapEntry<K, V>> iterator) {
-        this.iterator=iterator;
+    public ParametersIterator(Iterator<IMultiMapEntry<K, V>> iterator) {
+        this.iterator = iterator;
     }
 
     @Override
@@ -22,5 +22,5 @@ public class ParametersIterator<K,V> implements Iterator<ParameterEntry<K, V>> {
         var next = iterator.next();
         return new ParameterEntryImpl<>(next);
     }
-    
+
 }
