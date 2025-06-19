4cb1bd9d5f08afcf1c08156c5b12c9748cf9ad9d
scx567888 <scx567888@outlook.com>
2025-06-19 16:10:28 +0800

    dev

--- scx-data/src/main/java/cool/scx/data/DataManager.java
+++ scx-data/src/main/java/cool/scx/data/DataManager.java
@@ -2,20 +2,12 @@ package cool.scx.data;
 
 import cool.scx.data.exception.DataAccessException;
 import cool.scx.functional.ScxCallable;
-import cool.scx.functional.ScxConsumer;
-import cool.scx.functional.ScxFunction;
 import cool.scx.functional.ScxRunnable;
 
 public interface DataManager<C extends DataContext> {
 
-    C createDataContext();
+    <T, E extends Throwable> T withContext(ScxCallable<T, E> handler) throws DataAccessException, E;
 
-    <T, E extends Throwable> T withContext(ScxFunction<C, T, E> handler) throws DataAccessException, E;
-
-    <E extends Throwable> void withContext(ScxConsumer<C, E> handler) throws DataAccessException, E;
-
-    <T, E extends Throwable> T autoContext(ScxCallable<T, E> handler) throws DataAccessException, E;
-
-    <E extends Throwable> void autoContext(ScxRunnable<E> handler) throws DataAccessException, E;
+    <E extends Throwable> void withContext(ScxRunnable<E> handler) throws DataAccessException, E;
 
 }
