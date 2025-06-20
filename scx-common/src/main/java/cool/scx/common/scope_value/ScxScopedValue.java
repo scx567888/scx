e130ec507c33c51171c78e1fa26c2409304e7555
scx567888 <scx567888@outlook.com>
2025-06-20 15:03:48 +0800

    dev

--- scx-common/src/main/java/cool/scx/common/scope_value/ScxScopedValue.java
+++ scx-common/src/main/java/cool/scx/common/scope_value/ScxScopedValue.java
@@ -3,7 +3,10 @@ package cool.scx.common.scope_value;
 import cool.scx.functional.ScxCallable;
 import cool.scx.functional.ScxRunnable;
 
-/// 使用 threadLocal 模拟的 ScopedValue
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/// 使用 threadLocal 模拟的 ScopedValue (不支持线程池线程复用和复杂异步线程切换)
 /// todo ScopedValue 正式版本发布时 移除此类
 ///
 /// @param <T> a
@@ -42,27 +45,43 @@ public final class ScxScopedValue<T> {
             this.prev = prev;
         }
 
+        private void bindAll(Deque<Object> oldValues) {
+            if (prev != null) {
+                prev.bindAll(oldValues);
+            }
+            oldValues.addLast(key.get());
+            key.bind(value);
+        }
+
+        private void restoreAll(Deque<Object> oldValues) {
+            var old = oldValues.pollLast();
+            key.bind(old);
+            if (prev != null) {
+                prev.restoreAll(oldValues);
+            }
+        }
+
         public <T> Carrier where(ScxScopedValue<T> key, T value) {
             return new Carrier(key, value, this);
         }
 
         public <E extends Throwable> void run(ScxRunnable<E> op) throws E {
-            var old = key.get();
-            key.bind(value);
+            var oldValues = new ArrayDeque<>();
+            bindAll(oldValues);
             try {
                 op.run();
             } finally {
-                key.bind(old);
+                restoreAll(oldValues);
             }
         }
 
         public <R, E extends Throwable> R call(ScxCallable<? extends R, E> op) throws E {
-            var old = key.get();
-            key.bind(value);
+            var oldValues = new ArrayDeque<>();
+            bindAll(oldValues);
             try {
                 return op.call();
             } finally {
-                key.bind(old);
+                restoreAll(oldValues);
             }
         }
 
