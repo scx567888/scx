0540a3170f980f8f1c431894af4a27e5a24ea731
scx567888 <scx567888@outlook.com>
2025-06-25 17:19:12 +0800

    dev

--- scx-data/src/main/java/cool/scx/data/LockMode.java
+++ scx-data/src/main/java/cool/scx/data/LockMode.java
@@ -1,15 +1,12 @@
 package cool.scx.data;
 
-///  锁模式
+/// LockMode
 ///
 /// @author scx567888
 /// @version 0.0.1
 public enum LockMode {
 
-    /// 共享锁, 允许多个读取, 阻止写入
     SHARED,
-
-    /// 排他锁, 阻止读取和写入
     EXCLUSIVE
 
 }
