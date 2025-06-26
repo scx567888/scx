83c21ee922b018613ef66a2f26f6ca87abc3ff5a
scx567888 <scx567888@outlook.com>
2025-06-26 13:27:44 +0800

    dev

--- scx-bytes/src/main/java/cool/scx/bytes/consumer/SkipByteConsumer.java
+++ scx-bytes/src/main/java/cool/scx/bytes/consumer/SkipByteConsumer.java
@@ -2,22 +2,16 @@ package cool.scx.bytes.consumer;
 
 import cool.scx.bytes.ByteChunk;
 
-/// 直接跳过
+/// SkipByteConsumer
 ///
 /// @author scx567888
 /// @version 0.0.1
 public class SkipByteConsumer implements ByteConsumer {
 
-    public static final SkipByteConsumer SKIP_DATA_CONSUMER = new SkipByteConsumer();
-
-    //这个类不应该被用户实例化
-    private SkipByteConsumer() {
-
-    }
+    public static final SkipByteConsumer SKIP_BYTE_CONSUMER = new SkipByteConsumer();
 
     @Override
     public boolean accept(ByteChunk chunk) {
-        //什么都不做
         return true;
     }
 
