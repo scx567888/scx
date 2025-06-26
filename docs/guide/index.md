24219b8e4518aa8622f20833f1179e505d34bf17
scx567888 <scx567888@outlook.com>
2025-06-26 10:28:39 +0800

    拆分 scx-functional

--- docs/guide/index.md
+++ docs/guide/index.md
@@ -4,4 +4,6 @@
 
 [简单的 增删改查](./crud.md)
 
+[SCX Data](./scx-data.md)
+
 文档建设工作还在进行中...
--- scx-functional/src/test/java/cool/scx/functional/test/FunctionalTest.java
+++
@@ -1,9 +0,0 @@
-package cool.scx.functional.test;
-
-public class FunctionalTest {
-
-    public static void main(String[] args) {
-
-    }
-
-}
