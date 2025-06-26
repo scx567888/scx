bd16fde5bdf815173e8ee85b68be672ad5f899c0
scx567888 <scx567888@outlook.com>
2025-06-26 13:04:16 +0800

    dev

--- scx-collections/src/main/java/cool/scx/collections/count_map/CountMapEntry.java
+++
@@ -1,9 +0,0 @@
-package cool.scx.collections.count_map;
-
-/// CountMapEntry
-///
-/// @author scx567888
-/// @version 0.0.1
-record CountMapEntry<K>(K key, long count) implements ICountMapEntry<K> {
-
-}
