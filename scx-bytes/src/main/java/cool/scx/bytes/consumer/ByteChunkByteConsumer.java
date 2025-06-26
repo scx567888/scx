6b87247e5be599280cafb74fc529af86313d6303
scx567888 <scx567888@outlook.com>
2025-06-26 13:50:26 +0800

    dev

--- scx-bytes/src/main/java/cool/scx/bytes/consumer/ByteChunkByteConsumer.java
+++ scx-bytes/src/main/java/cool/scx/bytes/consumer/ByteChunkByteConsumer.java
@@ -2,22 +2,22 @@ package cool.scx.bytes.consumer;
 
 import cool.scx.bytes.ByteChunk;
 
-/// 接受为单个 DataNode
+/// ByteChunkByteConsumer
 ///
 /// @author scx567888
 /// @version 0.0.1
-public class ByteChunkByteConsumer implements ByteConsumer {
+public class ByteChunkByteConsumer implements ByteConsumer<RuntimeException> {
 
     private ByteChunk byteChunk;
 
     @Override
     public boolean accept(ByteChunk byteChunk) {
         this.byteChunk = byteChunk;
-        //只接受一次
+        // 只接受一次
         return false;
     }
 
-    public ByteChunk getByteChunk() {
+    public ByteChunk byteChunk() {
         return byteChunk;
     }
 
