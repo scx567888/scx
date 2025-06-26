bda04a29dfcff7d481a1e4f508018425cc1461e1
scx567888 <scx567888@outlook.com>
2025-06-26 13:24:06 +0800

    dev

--- scx-bytes/src/main/java/cool/scx/bytes/supplier/ByteSupplier.java
+++ scx-bytes/src/main/java/cool/scx/bytes/supplier/ByteSupplier.java
@@ -3,15 +3,13 @@ package cool.scx.bytes.supplier;
 import cool.scx.bytes.ByteChunk;
 import cool.scx.bytes.exception.ByteSupplierException;
 
-/// 数据生产者
+/// ByteSupplier
 ///
 /// @author scx567888
 /// @version 0.0.1
 public interface ByteSupplier {
 
-    /// 获取数据 如果没有数据请返回 null
-    ///
-    /// @return 数据节点 或 null
+    /// 如果没有数据请返回 null
     ByteChunk get() throws ByteSupplierException;
 
 }
