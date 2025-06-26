08cbfeb1f88b5522aaa96619c67b8c5c01affc6a
scx567888 <scx567888@outlook.com>
2025-06-26 13:49:21 +0800

    dev

--- scx-bytes/src/main/java/cool/scx/bytes/consumer/ByteConsumer.java
+++ scx-bytes/src/main/java/cool/scx/bytes/consumer/ByteConsumer.java
@@ -6,9 +6,9 @@ import cool.scx.bytes.ByteChunk;
 ///
 /// @author scx567888
 /// @version 0.0.1
-public interface ByteConsumer {
+public interface ByteConsumer<E extends Throwable> {
 
     /// @return 是否需要更多数据
-    boolean accept(ByteChunk chunk);
+    boolean accept(ByteChunk chunk) throws E;
 
 }
