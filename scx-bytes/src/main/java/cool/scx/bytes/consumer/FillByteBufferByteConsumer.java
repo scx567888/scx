b662299eab210292edbe561d4ac9ff30be940b54
scx567888 <scx567888@outlook.com>
2025-06-26 13:44:18 +0800

    dev

--- scx-bytes/src/main/java/cool/scx/bytes/consumer/FillByteBufferByteConsumer.java
+++ scx-bytes/src/main/java/cool/scx/bytes/consumer/FillByteBufferByteConsumer.java
@@ -4,31 +4,28 @@ import cool.scx.bytes.ByteChunk;
 
 import java.nio.ByteBuffer;
 
-/// 填充方式
+/// FillByteBufferByteConsumer
 ///
 /// @author scx567888
 /// @version 0.0.1
-public class FillByteBufferByteConsumer implements ByteConsumer {
+public class FillByteBufferByteConsumer implements ByteConsumer<RuntimeException> {
 
     private final ByteBuffer data;
-    private final int dataPosition;
+    private final int startPosition;
 
     public FillByteBufferByteConsumer(ByteBuffer data) {
         this.data = data;
-        this.dataPosition = data.position();
+        this.startPosition = data.position();
     }
 
     @Override
     public boolean accept(ByteChunk byteChunk) {
-        if (data.remaining() < byteChunk.length) {
-            throw new IllegalStateException("Buffer overflow: not enough space to accept more data");
-        }
         data.put(byteChunk.bytes, byteChunk.start, byteChunk.length);
         return true;
     }
 
-    public int getFilledLength() {
-        return data.position() - dataPosition;
+    public int filledLength() {
+        return data.position() - startPosition;
     }
 
 }
