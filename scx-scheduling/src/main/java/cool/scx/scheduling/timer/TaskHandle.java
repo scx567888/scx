a6bdcf5f053781ad30a9f4b12b0556ac2a3f20de
scx567888 <scx567888@outlook.com>
2025-06-27 13:15:45 +0800

    dev

--- scx-scheduling/src/main/java/cool/scx/scheduling/timer/TaskHandle.java
+++ scx-scheduling/src/main/java/cool/scx/scheduling/timer/TaskHandle.java
@@ -1,11 +1,21 @@
 package cool.scx.scheduling.timer;
 
-public interface TaskHandle {
-    
+public interface TaskHandle<V, E extends Throwable> {
+
     /// 取消任务
     boolean cancel();
 
     /// 状态
     TaskStatus status();
-    
+
+    /// 获取 结果, 仅在任务执行成功后可用
+    ///
+    /// @throws IllegalStateException 任务状态异常, 如任务 未执行,已取消 或者 已失败
+    V result() throws IllegalStateException;
+
+    /// 获取 异常, 仅在任务执行失败后可用
+    ///
+    /// @throws IllegalStateException 任务状态异常, 如任务 未执行,已取消 或者 已成功
+    E exception() throws IllegalStateException;
+
 }
