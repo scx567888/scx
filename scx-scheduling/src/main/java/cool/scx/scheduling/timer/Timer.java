79fd460383f67ee1e758ada7eba23c4934fce554
scx567888 <scx567888@outlook.com>
2025-06-27 13:08:41 +0800

    dev

--- scx-scheduling/src/main/java/cool/scx/scheduling/timer/Timer.java
+++ scx-scheduling/src/main/java/cool/scx/scheduling/timer/Timer.java
@@ -1,10 +1,15 @@
 package cool.scx.scheduling.timer;
 
+import cool.scx.functional.ScxCallable;
+import cool.scx.functional.ScxRunnable;
+
 import java.util.concurrent.TimeUnit;
 
 /// 一个最基本的定时器
 public interface Timer {
 
-    TaskHandle runAfter(Runnable action, long delay, TimeUnit unit);
-    
+    <E extends Throwable> TaskHandle<Void, E> runAfter(ScxRunnable<E> action, long delay, TimeUnit unit);
+
+    <V, E extends Throwable> TaskHandle<V, E> runAfter(ScxCallable<V, E> action, long delay, TimeUnit unit);
+
 }
