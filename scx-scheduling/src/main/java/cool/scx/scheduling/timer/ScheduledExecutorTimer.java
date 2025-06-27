4da197b53b01726748fc3e9e6abcf5e0d9bd24f5
scx567888 <scx567888@outlook.com>
2025-06-27 14:33:12 +0800

    dev

--- scx-scheduling/src/main/java/cool/scx/scheduling/timer/ScheduledExecutorTimer.java
+++ scx-scheduling/src/main/java/cool/scx/scheduling/timer/ScheduledExecutorTimer.java
@@ -53,18 +53,12 @@ public final class ScheduledExecutorTimer implements Timer {
         return new TaskHandleImpl<>(future, taskStatus);
     }
 
-    // 异常包装器
-    private static class WrapperRuntimeException extends RuntimeException {
-        public WrapperRuntimeException(Throwable cause) {
-            super(cause);
-        }
-    }
-
     private record TaskHandleImpl<V, E extends Throwable>(ScheduledFuture<?> future,
                                                           AtomicReference<TaskStatus> taskStatus) implements TaskHandle<V, E> {
 
         @Override
         public boolean cancel() {
+            // 我们不中断任务执行 也就是说 只有在 任务还未开始的时候 cancel 才有意义
             return future.cancel(false);
         }
 
@@ -80,6 +74,7 @@ public final class ScheduledExecutorTimer implements Timer {
             } catch (ExecutionException e) {
                 // 执行异常，将其转换为原始异常 E
                 Throwable cause = e.getCause();
+                // 处理 包装异常
                 if (cause instanceof WrapperRuntimeException) {
                     cause = cause.getCause();
                 }
@@ -92,23 +87,25 @@ public final class ScheduledExecutorTimer implements Timer {
 
         @Override
         public TaskStatus status() {
-            //1, 任务完成 需要细化判断具体原因
+            // 1, 任务完成 需要细化判断具体原因
             if (future.isDone()) {
                 var status = taskStatus.get();
-                //如果还没开始 就 被取消了 我们直接取消
+                // 只有在任务没开始的时候, 我们才认为 isCancelled 的值有意义
                 if (status == PENDING && future.isCancelled()) {
                     return CANCELLED;
                 }
-                //这里只剩下 三种情况 RUNNING, SUCCESS, FAILED
-                // 但是 RUNNING 是不可能的 因为 isDone 表示整个方法块一定是完成了的了 所以只剩下 SUCCESS, FAILED 可以安全返回 
+                // 这里只剩下 三种情况 RUNNING, SUCCESS, FAILED
+                // 但是因为 isDone 表示的是整个代码块执行完毕, 所以 RUNNING 是不可能的
+                // 只剩下 SUCCESS, FAILED 我们可以安全返回 
                 return status;
-            } else {//2, 任务还在执行中 这时我们可以使用 taskStatus 来判断 现在的真正状态
+            } else {// 2, 任务还在执行中 这时我们可以使用 taskStatus 来判断 现在的真正状态
                 var status = taskStatus.get();
-                //这时还没有执行
+                // 这时还没有真正执行
                 if (status == PENDING) {
                     return PENDING;
                 } else {
-                    //这时虽然 可能 status 已经被设置  SUCCESS 或 FAILED, 但鉴于整个 task 实际上还没有执行完毕所以 只返回 RUNNING
+                    // 这时虽然 可能 status 已经被设置为 SUCCESS 或 FAILED
+                    // 但鉴于整个 整个代码块 实际上可能还没有执行完毕 (尽管可能性极低), 所以 只返回 RUNNING
                     return RUNNING;
                 }
             }
@@ -124,12 +121,20 @@ public final class ScheduledExecutorTimer implements Timer {
         @Override
         public E exception() {
             var e = future.exceptionNow();
+            // 处理 包装异常
             if (e instanceof WrapperRuntimeException) {
                 e = e.getCause();
             }
             return (E) e;
         }
 
+    }
+
+    // 异常包装器
+    private static class WrapperRuntimeException extends RuntimeException {
+        public WrapperRuntimeException(Throwable cause) {
+            super(cause);
+        }
     }
 
 }
