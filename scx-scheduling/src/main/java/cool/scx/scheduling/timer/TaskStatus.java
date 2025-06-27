840319b8b60f1309dfaa36c6358e555328fa904a
scx567888 <scx567888@outlook.com>
2025-06-27 11:41:57 +0800

    dev

--- scx-scheduling/src/main/java/cool/scx/scheduling/timer/TaskStatus.java
+++ scx-scheduling/src/main/java/cool/scx/scheduling/timer/TaskStatus.java
@@ -1,7 +1,20 @@
 package cool.scx.scheduling.timer;
 
 public enum TaskStatus {
-    RUNNING,    // 运行中
-    DONE,       // 已完成
-    CANCELED,   // 已取消
+    
+    /// 已提交, 等待执行
+    PENDING,
+    
+    /// 正在执行
+    RUNNING,
+    
+    /// 执行成功
+    SUCCESS,
+    
+    /// 执行失败
+    FAILED,
+    
+    /// 已取消
+    CANCELLED
+    
 }
