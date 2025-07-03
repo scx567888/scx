872701310dab71e5840f90650c76d7662fc88557
scx567888 <scx567888@outlook.com>
2025-07-03 14:22:19 +0800

    dev

--- scx-reflect/src/main/java/cool/scx/reflect/i/ParameterInfoImpl.java
+++ scx-reflect/src/main/java/cool/scx/reflect/i/ParameterInfoImpl.java
@@ -15,8 +15,8 @@ public class ParameterInfoImpl implements ParameterInfo {
         this.rawParameter = parameter;
         this.declaringExecutable = declaringExecutable;
         this.name = parameter.getName();
-        this.annotations = parameter.getDeclaredAnnotations();
         this.parameterType = ScxReflect.getClassInfo(parameter.getParameterizedType());
+        this.annotations = parameter.getDeclaredAnnotations();
     }
 
     @Override
