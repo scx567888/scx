65b932b7c1ee752f139c97ddc929ba6a2ad1c02f
scx567888 <scx567888@outlook.com>
2025-07-03 14:20:17 +0800

    dev

--- scx-reflect/src/main/java/cool/scx/reflect/i/MemberInfo.java
+++ scx-reflect/src/main/java/cool/scx/reflect/i/MemberInfo.java
@@ -1,11 +1,14 @@
 package cool.scx.reflect.i;
 
-public interface MemberInfo {
-    
+public interface MemberInfo extends AnnotatedElementInfo {
+
     /// 持有当前 成员的 ClassInfo
     ClassInfo declaringClass();
 
     /// 访问修饰符
     AccessModifier accessModifier();
-    
+
+    /// 允许方法
+    void setAccessible(boolean flag);
+
 }
