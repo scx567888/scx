043495ef6f5e3f8be2a4a4483954724784efab56
scx567888 <scx567888@outlook.com>
2025-07-03 13:17:06 +0800

    dev

--- scx-reflect/src/main/java/cool/scx/reflect/i/ClassInfoImpl.java
+++ scx-reflect/src/main/java/cool/scx/reflect/i/ClassInfoImpl.java
@@ -1,39 +1,85 @@
 package cool.scx.reflect.i;
 
-import cool.scx.reflect.MethodInfo;
-
 import java.lang.annotation.Annotation;
-import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 
+import static cool.scx.reflect.i.ClassInfoHelper.*;
 import static cool.scx.reflect.i.ScxReflect.CLASS_INFO_CACHE;
-import static cool.scx.reflect.i.ScxReflect.getClassInfo;
+import static java.lang.reflect.AccessFlag.FINAL;
+import static java.lang.reflect.AccessFlag.STATIC;
 
 public class ClassInfoImpl implements ClassInfo {
 
     private Class<?> rawClass;
-    private String name;
+
+    private final String name;
+    private final AccessModifier accessModifier;
+    private final ClassType classType;
+    private final boolean isFinal;
+    private final boolean isStatic;
+    private final boolean isAnonymousClass;
+    private final boolean isMemberClass;
+    private final boolean isPrimitive;
+    private final boolean isArray;
+
+    private final ClassInfo superClass;
+    private final ClassInfo[] interfaces;
+
+    private final ConstructorInfo[] constructors;
+    private final ConstructorInfo defaultConstructor;
+
+    private final FieldInfo[] fields;
+    private final MethodInfo[] methods;
+
+    private final Annotation[] annotations;
+
+    private final ClassInfo enumClass;
+    private final ClassInfo componentType;
 
     public ClassInfoImpl(Type type) {
         //0, 先添加到 CLASS_INFO_CACHE 中
         CLASS_INFO_CACHE.put(type, this);
+
+        Class<?> _rawClass = null;
         if (type instanceof Class<?> c) {
-            rawClass = c;
+            _rawClass = c;
         } else if (type instanceof ParameterizedType t) {
             Type rawType = t.getRawType();
             Type[] actualTypeArguments = t.getActualTypeArguments();
             if (rawType instanceof Class<?> c) {
-                rawClass = c;
+                _rawClass = c;
             }
         }
-        name= type.getTypeName();
-        Field[] declaredFields = rawClass.getDeclaredFields();
-        for (Field field : declaredFields) {
-            Class<?> type1 = field.getType();
-            ClassInfo classInfo = getClassInfo(type1);
-//            this.addddd.add(classInfo);
-        }
+
+        this.rawClass = _rawClass;
+
+        var accessFlags = this.rawClass.accessFlags();
+
+        this.name = this.rawClass.getName();
+        this.accessModifier = _findAccessModifier(accessFlags);
+        this.classType = _findClassType(this.rawClass, accessFlags);
+        this.isFinal = accessFlags.contains(FINAL);
+        this.isStatic = accessFlags.contains(STATIC);
+        this.isAnonymousClass = this.rawClass.isAnonymousClass();
+        this.isMemberClass = this.rawClass.isMemberClass();
+        this.isPrimitive = this.rawClass.isPrimitive();
+        this.isArray = this.rawClass.isArray();
+
+        this.superClass = _findSuperClass(this.rawClass);
+        this.interfaces = _findInterfaces(this.rawClass);
+
+        this.constructors = _findConstructors(this.rawClass, this);
+        this.defaultConstructor = _findDefaultConstructor(this.constructors);
+
+        this.fields = _findFields(this.rawClass, this);
+        this.methods = _findMethods(this.rawClass, this);
+
+        this.annotations = this.rawClass.getDeclaredAnnotations();
+
+        this.enumClass = _findEnumClass(this);
+        this.componentType = _findComponentType(this);
+
     }
 
     @Override
@@ -43,67 +89,67 @@ public class ClassInfoImpl implements ClassInfo {
 
     @Override
     public String name() {
-        return "";
+        return name;
     }
 
     @Override
     public AccessModifier accessModifier() {
-        return null;
+        return accessModifier;
     }
 
     @Override
     public ClassType classType() {
-        return null;
+        return classType;
     }
 
     @Override
     public boolean isFinal() {
-        return false;
+        return isFinal;
     }
 
     @Override
     public boolean isStatic() {
-        return false;
+        return isStatic;
     }
 
     @Override
     public boolean isAnonymousClass() {
-        return false;
+        return isAnonymousClass;
     }
 
     @Override
     public boolean isMemberClass() {
-        return false;
+        return isMemberClass;
     }
 
     @Override
     public boolean isPrimitive() {
-        return false;
+        return isPrimitive;
     }
 
     @Override
     public boolean isArray() {
-        return false;
+        return isArray;
     }
 
     @Override
     public ClassInfo superClass() {
-        return null;
+        return superClass;
     }
 
     @Override
     public ClassInfo[] interfaces() {
-        return new ClassInfo[0];
+        return interfaces;
     }
 
     @Override
     public ConstructorInfo[] constructors() {
-        return new ConstructorInfo[0];
+        return constructors;
     }
 
     @Override
     public ConstructorInfo defaultConstructor() {
-        return null;
+        return defaultConstructor;
     }
 
     @Override
@@ -113,7 +159,7 @@ public class ClassInfoImpl implements ClassInfo {
 
     @Override
     public FieldInfo[] fields() {
-        return new FieldInfo[0];
+        return fields;
     }
 
     @Override
@@ -123,7 +169,7 @@ public class ClassInfoImpl implements ClassInfo {
 
     @Override
     public MethodInfo[] methods() {
-        return new MethodInfo[0];
+        return methods;
     }
 
     @Override
@@ -133,7 +179,7 @@ public class ClassInfoImpl implements ClassInfo {
 
     @Override
     public Annotation[] annotations() {
-        return new Annotation[0];
+        return annotations;
     }
 
     @Override
@@ -143,12 +189,12 @@ public class ClassInfoImpl implements ClassInfo {
 
     @Override
     public ClassInfo enumClass() {
-        return null;
+        return enumClass;
     }
 
     @Override
     public ClassInfo componentType() {
-        return null;
+        return componentType;
     }
 
     @Override
