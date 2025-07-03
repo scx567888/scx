cb9938c0df82f8aeab2a06cc4423a2ad190d89ca
scx567888 <scx567888@outlook.com>
2025-07-03 19:09:44 +0800

    dev

--- scx-reflect/src/main/java/cool/scx/reflect/i/ScxReflect.java
+++ scx-reflect/src/main/java/cool/scx/reflect/i/ScxReflect.java
@@ -1,20 +1,34 @@
 package cool.scx.reflect.i;
 
-import java.lang.reflect.Type;
+import java.lang.reflect.*;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public final class ScxReflect {
 
-    static final Map<Type, ClassInfo> CLASS_INFO_CACHE = new ConcurrentHashMap<>();
+    static final Map<Type, TypeInfo> TYPE_INFO_CACHE = new ConcurrentHashMap<>();
 
     //todo 多线程问题 ? 
-    public static ClassInfo getClassInfo(Type type) {
-        var classInfo = CLASS_INFO_CACHE.get(type);
-        if (classInfo == null) {
+    public static TypeInfo getTypeInfo(Type type) {
+        var typeInfo = TYPE_INFO_CACHE.get(type);
+        if (typeInfo != null) {
+            return typeInfo;
+        }
+        if (type instanceof Class<?> || type instanceof ParameterizedType || type instanceof GenericArrayType) {
             return new ClassInfoImpl(type);
         }
-        return classInfo;
+        if (type instanceof TypeVariable<?> typeVariable) {
+            return new TypeVariableInfoImpl(typeVariable);
+        }
+        if (type instanceof WildcardType wildcardType) {
+            return new WildcardTypeInfoImpl(wildcardType);
+        }
+        throw new IllegalArgumentException("unsupported type: " + type);
+    }
+
+    public static ClassInfo getClassInfo(Class<?> clazz) {
+        // 这里强转是安全的 
+        return (ClassInfo) getTypeInfo(clazz);
     }
 
 }
