f7fb21ccd3755aa6044ede47810c0f969de982be
scx567888 <scx567888@outlook.com>
2025-07-07 13:07:56 +0800

    dev

--- scx-reflect/src/main/java/cool/scx/reflect/ExecutableInfo.java
+++ scx-reflect/src/main/java/cool/scx/reflect/ExecutableInfo.java
@@ -4,7 +4,7 @@ package cool.scx.reflect;
 ///
 /// @author scx567888
 /// @version 0.0.1
-public interface ExecutableInfo extends MemberInfo {
+public sealed interface ExecutableInfo extends MemberInfo permits ConstructorInfo, MethodInfo {
 
     ParameterInfo[] parameters();
 
