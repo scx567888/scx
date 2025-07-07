453dbf7047936012691794863a2c45808490585e
scx567888 <scx567888@outlook.com>
2025-07-07 13:08:10 +0800

    dev

--- scx-reflect/src/main/java/cool/scx/reflect/MemberInfo.java
+++ scx-reflect/src/main/java/cool/scx/reflect/MemberInfo.java
@@ -4,15 +4,9 @@ package cool.scx.reflect;
 ///
 /// @author scx567888
 /// @version 0.0.1
-public interface MemberInfo extends AnnotatedElementInfo {
+public sealed interface MemberInfo extends AccessModifierController, AnnotatedElementInfo permits ExecutableInfo, FieldInfo {
 
     /// 持有当前 成员的 ClassInfo
     ClassInfo declaringClass();
-
-    /// 访问修饰符
-    AccessModifier accessModifier();
-
-    /// 允许方法
-    void setAccessible(boolean flag);
 
 }
