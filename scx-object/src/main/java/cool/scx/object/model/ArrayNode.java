8a1e1c5c9aab8c078eeee92bd8987da3b5f27287
scx567888 <scx567888@outlook.com>
2025-07-08 17:09:13 +0800

    dev

--- scx-object/src/main/java/cool/scx/object/model/ArrayNode.java
+++ scx-object/src/main/java/cool/scx/object/model/ArrayNode.java
@@ -1,9 +1,10 @@
 package cool.scx.object.model;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
-public class ArrayNode implements ContainerNode {
+public class ArrayNode implements ContainerNode, Iterable<Node> {
 
     private final List<Node> _children;
 
@@ -13,6 +14,27 @@ public class ArrayNode implements ContainerNode {
 
     public void add(Node node) {
         this._children.add(node);
+    }
+
+    public void add(int index, Node node) {
+        this._children.add(index, node);
+    }
+
+    public void set(int index, Node node) {
+        this._children.set(index, node);
+    }
+
+    public Node get(int index) {
+        return this._children.get(index);
+    }
+
+    public Node remove(int index) {
+        return this._children.remove(index);
+    }
+
+    @Override
+    public Iterator<Node> iterator() {
+        return _children.iterator();
     }
 
 }
