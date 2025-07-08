7cd8a1c6742323177033b057ab7aa2df90f36e70
scx567888 <scx567888@outlook.com>
2025-07-09 04:14:40 +0800

    dev

--- scx-object/src/main/java/cool/scx/object/ScxObject.java
+++ scx-object/src/main/java/cool/scx/object/ScxObject.java
@@ -29,16 +29,52 @@ public class ScxObject {
         return JSON_PARSER.readTree(json);
     }
 
+    public static <T> T fromJson(String json, TypeInfo type) throws IOException {
+        var node = fromJson(json);
+        return treeToValue(node, type);
+    }
+
+    public static <T> T fromJson(String json, Class<T> type) throws IOException {
+        return fromJson(json, ScxReflect.getType(type));
+    }
+
+    public static <T> T fromJson(String json, TypeReference<T> type) throws IOException {
+        return fromJson(json, ScxReflect.getType(type));
+    }
+
     public static String toJson(Node node) throws IOException {
         return JSON_PARSER.writeTreeToString(node);
+    }
+
+    public static String toJson(Object object) throws IOException {
+        var node = valueToTree(object);
+        return toJson(node);
     }
 
     public static Node fromXML(String xml) throws IOException {
         return XML_PARSER.readTree(xml);
     }
 
+    public static <T> T fromXML(String xml, TypeInfo type) throws IOException {
+        var node = fromXML(xml);
+        return treeToValue(node, type);
+    }
+
+    public static <T> T fromXML(String xml, Class<T> type) throws IOException {
+        return fromXML(xml, ScxReflect.getType(type));
+    }
+
+    public static <T> T fromXML(String xml, TypeReference<T> type) throws IOException {
+        return fromXML(xml, ScxReflect.getType(type));
+    }
+
     public static String toXML(Node node) throws IOException {
         return XML_PARSER.writeTreeToString(node);
+    }
+    
+    public static String toXML(Object object) throws IOException {
+        var node = valueToTree(object);
+        return toJson(node);
     }
 
     public static <T> T convertValue(Object value, TypeReference<T> type) {
