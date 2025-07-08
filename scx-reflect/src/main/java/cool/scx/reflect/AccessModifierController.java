efa3c318d3a51ec6ea372bb05b8027bfac313465
scx567888 <scx567888@outlook.com>
2025-07-08 14:31:08 +0800

    dev

--- scx-reflect/src/main/java/cool/scx/reflect/AccessModifierController.java
+++
@@ -1,9 +0,0 @@
-package cool.scx.reflect;
-
-/// 能够控制 访问的
-public sealed interface AccessModifierController extends AccessModifierOwner permits MemberInfo {
-
-    /// 允许访问
-    void setAccessible(boolean flag);
-
-}
--- scx-reflect/src/main/java/cool/scx/reflect/AccessModifierOwner.java
+++
@@ -1,8 +0,0 @@
-package cool.scx.reflect;
-
-/// 具有访问修饰符的元素
-public sealed interface AccessModifierOwner permits AccessModifierController, ClassInfo {
-
-    AccessModifier accessModifier();
-
-}
