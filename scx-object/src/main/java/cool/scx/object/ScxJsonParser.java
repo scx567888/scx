2af436f38894c2433edc05a46bd348838d27e493
scx567888 <scx567888@outlook.com>
2025-07-08 17:32:22 +0800

    dev

--- scx-object/src/main/java/cool/scx/object/ScxJsonParser.java
+++ scx-object/src/main/java/cool/scx/object/ScxJsonParser.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonToken;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import cool.scx.object.model.*;
 
 import java.io.IOException;
@@ -106,7 +105,5 @@ public class ScxJsonParser {
             default -> throw new IOException("Unsupported Node Type: " + node.getClass().getName());
         }
     }
-
-
 
 }
