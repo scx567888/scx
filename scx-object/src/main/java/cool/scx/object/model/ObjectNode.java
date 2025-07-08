72659765152af9e4e51686d3b3a927cc0be97bb1
scx567888 <scx567888@outlook.com>
2025-07-08 17:12:29 +0800

    dev

--- scx-object/src/main/java/cool/scx/object/model/ObjectNode.java
+++ scx-object/src/main/java/cool/scx/object/model/ObjectNode.java
@@ -1,9 +1,10 @@
 package cool.scx.object.model;
 
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-public class ObjectNode implements ContainerNode {
+public class ObjectNode implements ContainerNode, Iterable<Map.Entry<String, Node>> {
 
     private final Map<String, Node> _children;
 
@@ -13,6 +14,19 @@ public class ObjectNode implements ContainerNode {
 
     public void put(String name, Node node) {
         this._children.put(name, node);
+    }
+
+    public Node get(String name) {
+        return this._children.get(name);
+    }
+
+    public Node remove(String name) {
+        return this._children.remove(name);
+    }
+
+    @Override
+    public Iterator<Map.Entry<String, Node>> iterator() {
+        return _children.entrySet().iterator();
     }
 
 }
