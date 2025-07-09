beb79f11e791cffe7ec85b84483c80634cb45992
scx567888 <scx567888@outlook.com>
2025-07-09 13:16:31 +0800

    dev

--- scx-object/src/main/java/cool/scx/object/node/TextNode.java
+++ scx-object/src/main/java/cool/scx/object/node/TextNode.java
@@ -1,6 +1,9 @@
 package cool.scx.object.node;
 
-public class TextNode implements ValueNode {
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+public final class TextNode implements ValueNode {
 
     private final String _value;
 
@@ -10,6 +13,51 @@ public class TextNode implements ValueNode {
 
     public String value() {
         return _value;
+    }
+
+    @Override
+    public int asInt() {
+        return Integer.parseInt(_value);
+    }
+
+    @Override
+    public long asLong() {
+        return Long.parseLong(_value);
+    }
+
+    @Override
+    public float asFloat() {
+        return Float.parseFloat(_value);
+    }
+
+    @Override
+    public double asDouble() {
+        return Double.parseDouble(_value);
+    }
+
+    @Override
+    public BigInteger asBigInteger() {
+        return new BigInteger(_value);
+    }
+
+    @Override
+    public BigDecimal asBigDecimal() {
+        return new BigDecimal(_value);
+    }
+
+    @Override
+    public String asText() {
+        return _value;
+    }
+
+    @Override
+    public boolean asBoolean() {
+        return Boolean.parseBoolean(_value);
+    }
+
+    @Override
+    public String toString() {
+        return asText();
     }
 
 }
