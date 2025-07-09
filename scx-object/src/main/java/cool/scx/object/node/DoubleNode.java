a327695d71c1daf43de2ae0a63d9bf55138a2904
scx567888 <scx567888@outlook.com>
2025-07-09 13:07:48 +0800

    dev

--- scx-object/src/main/java/cool/scx/object/node/DoubleNode.java
+++ scx-object/src/main/java/cool/scx/object/node/DoubleNode.java
@@ -1,5 +1,8 @@
 package cool.scx.object.node;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 public final class DoubleNode implements NumericNode {
 
     private final double _value;
@@ -15,6 +18,51 @@ public final class DoubleNode implements NumericNode {
     @Override
     public Number numberValue() {
         return _value;
+    }
+
+    @Override
+    public int asInt() {
+        return (int) _value;
+    }
+
+    @Override
+    public long asLong() {
+        return (long) _value;
+    }
+
+    @Override
+    public float asFloat() {
+        return (float) _value;
+    }
+
+    @Override
+    public double asDouble() {
+        return _value;
+    }
+
+    @Override
+    public BigInteger asBigInteger() {
+        return BigDecimal.valueOf(_value).toBigInteger();
+    }
+
+    @Override
+    public BigDecimal asBigDecimal() {
+        return BigDecimal.valueOf(_value);
+    }
+
+    @Override
+    public String asText() {
+        return String.valueOf(_value);
+    }
+
+    @Override
+    public boolean asBoolean() {
+        return _value != 0;
+    }
+
+    @Override
+    public String toString() {
+        return asText();
     }
 
 }
