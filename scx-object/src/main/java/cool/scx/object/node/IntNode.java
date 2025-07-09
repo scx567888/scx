bfda66f10a9937d431f39ff0195489235b634751
scx567888 <scx567888@outlook.com>
2025-07-09 13:02:07 +0800

    dev

--- scx-object/src/main/java/cool/scx/object/node/IntNode.java
+++ scx-object/src/main/java/cool/scx/object/node/IntNode.java
@@ -1,5 +1,8 @@
 package cool.scx.object.node;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 public final class IntNode implements NumericNode {
 
     private final int _value;
@@ -15,6 +18,46 @@ public final class IntNode implements NumericNode {
     @Override
     public Number numberValue() {
         return _value;
+    }
+
+    @Override
+    public int asInt() {
+        return _value;
+    }
+
+    @Override
+    public long asLong() {
+        return _value;
+    }
+
+    @Override
+    public float asFloat() {
+        return _value;
+    }
+
+    @Override
+    public double asDouble() {
+        return _value;
+    }
+
+    @Override
+    public BigInteger asBigInteger() {
+        return BigInteger.valueOf(_value);
+    }
+
+    @Override
+    public BigDecimal asBigDecimal() {
+        return BigDecimal.valueOf(_value);
+    }
+
+    @Override
+    public String asText() {
+        return String.valueOf(_value);
+    }
+
+    @Override
+    public boolean asBoolean() {
+        return _value != 0;
     }
 
 }
