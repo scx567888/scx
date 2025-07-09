d3fd46ee2cfaac8f146252cc6d21d025f0128d9e
scx567888 <scx567888@outlook.com>
2025-07-09 13:18:23 +0800

    dev

--- scx-object/src/main/java/cool/scx/object/node/BooleanNode.java
+++ scx-object/src/main/java/cool/scx/object/node/BooleanNode.java
@@ -1,6 +1,9 @@
 package cool.scx.object.node;
 
-public class BooleanNode implements ValueNode {
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+public final class BooleanNode implements ValueNode {
 
     public final static BooleanNode TRUE = new BooleanNode(true);
     public final static BooleanNode FALSE = new BooleanNode(false);
@@ -17,6 +20,51 @@ public class BooleanNode implements ValueNode {
 
     public boolean value() {
         return _value;
+    }
+
+    @Override
+    public int asInt() {
+        return _value ? 1 : 0;
+    }
+
+    @Override
+    public long asLong() {
+        return _value ? 1L : 0L;
+    }
+
+    @Override
+    public float asFloat() {
+        return _value ? 1.0f : 0.0f;
+    }
+
+    @Override
+    public double asDouble() {
+        return _value ? 1.0d : 0.0d;
+    }
+
+    @Override
+    public BigInteger asBigInteger() {
+        return _value ? BigInteger.ONE : BigInteger.ZERO;
+    }
+
+    @Override
+    public BigDecimal asBigDecimal() {
+        return _value ? BigDecimal.ONE : BigDecimal.ZERO;
+    }
+
+    @Override
+    public String asText() {
+        return String.valueOf(_value);
+    }
+
+    @Override
+    public boolean asBoolean() {
+        return _value;
+    }
+
+    @Override
+    public String toString() {
+        return asText();
     }
 
 }
