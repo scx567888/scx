a91c68ee3326c8dd7ad999acced6ce4eebed26d5
scx567888 <scx567888@outlook.com>
2025-07-09 13:13:04 +0800

    dev

--- scx-object/src/main/java/cool/scx/object/node/ValueNode.java
+++ scx-object/src/main/java/cool/scx/object/node/ValueNode.java
@@ -1,5 +1,24 @@
 package cool.scx.object.node;
 
-public interface ValueNode extends Node {
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+public sealed interface ValueNode extends Node permits BooleanNode, NullNode, NumericNode, TextNode {
+
+    int asInt();
+
+    long asLong();
+
+    float asFloat();
+
+    double asDouble();
+
+    BigInteger asBigInteger();
+
+    BigDecimal asBigDecimal();
+
+    String asText();
+
+    boolean asBoolean();
 
 }
