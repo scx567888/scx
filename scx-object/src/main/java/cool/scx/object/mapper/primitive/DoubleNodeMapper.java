a140feea938d8fdbff81c15d3fcb517c176846e7
scx567888 <scx567888@outlook.com>
2025-07-10 18:30:42 +0800

    dev

---
+++ scx-object/src/main/java/cool/scx/object/mapper/primitive/DoubleNodeMapper.java
@@ -0,0 +1,33 @@
+package cool.scx.object.mapper.primitive;
+
+import cool.scx.object.node.DoubleNode;
+import cool.scx.object.node.Node;
+import cool.scx.object.node.NullNode;
+import cool.scx.object.node.ValueNode;
+
+public class DoubleNodeMapper extends PrimitiveNodeMapper<Double> {
+
+    public DoubleNodeMapper(boolean isPrimitive) {
+        super(isPrimitive, 0.0);
+    }
+
+    @Override
+    public Node toNode(Double value) {
+        return new DoubleNode(value);
+    }
+
+    @Override
+    public Double fromNode0(Node node) {
+        //1, 处理 null
+        if (node== NullNode.NULL){
+            return null;
+        }
+        //2, 只处理值类型
+        if (node instanceof ValueNode valueNode){
+            return valueNode.asDouble();
+        }
+        //3, 非值类型无法转换直接报错 
+        throw new IllegalArgumentException("Invalid node type: " + node.getClass());
+    }
+
+}
---
+++ scx-object/src/main/java/cool/scx/object/mapper/primitive/FloatNodeMapper.java
@@ -0,0 +1,33 @@
+package cool.scx.object.mapper.primitive;
+
+import cool.scx.object.node.FloatNode;
+import cool.scx.object.node.Node;
+import cool.scx.object.node.NullNode;
+import cool.scx.object.node.ValueNode;
+
+public class FloatNodeMapper extends PrimitiveNodeMapper<Float> {
+
+    public FloatNodeMapper(boolean isPrimitive) {
+        super(isPrimitive, 0f);
+    }
+
+    @Override
+    public Node toNode(Float value) {
+        return new FloatNode(value);
+    }
+
+    @Override
+    public Float fromNode0(Node node) {
+        //1, 处理 null
+        if (node== NullNode.NULL){
+            return null;
+        }
+        //2, 只处理值类型
+        if (node instanceof ValueNode valueNode){
+            return valueNode.asFloat();
+        }
+        //3, 非值类型无法转换直接报错 
+        throw new IllegalArgumentException("Invalid node type: " + node.getClass());
+    }
+
+}
