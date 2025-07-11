82044a1637fddd30234e11c71f7c70fe0360c082
scx567888 <scx567888@outlook.com>
2025-07-11 09:01:29 +0800

    dev

---
+++ scx-object/src/main/java/cool/scx/object/mapper/array/ByteArrayNodeMapper.java
@@ -0,0 +1,25 @@
+package cool.scx.object.mapper.array;
+
+import cool.scx.object.mapper.NodeMapper;
+import cool.scx.object.mapper.NodeMapperSelector;
+import cool.scx.object.node.ArrayNode;
+import cool.scx.object.node.IntNode;
+import cool.scx.object.node.Node;
+
+public class ByteArrayNodeMapper implements NodeMapper<byte[]> {
+
+    @Override
+    public Node toNode(byte[] value, NodeMapperSelector selector) {
+        var arrayNode = new ArrayNode();
+        for (int i : value) {
+            arrayNode.add(new IntNode(i));
+        }
+        return arrayNode;
+    }
+
+    @Override
+    public byte[] fromNode(Node node) {
+        return null;
+    }
+
+}
