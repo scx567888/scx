4bdcd48a8a615167d915bac8c03a467a9048c9cb
scx567888 <scx567888@outlook.com>
2025-07-11 18:37:53 +0800

    dev

--- scx-web/src/main/java/cool/scx/web/annotation/ScxRoute.java
+++ scx-web/src/main/java/cool/scx/web/annotation/ScxRoute.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import static cool.scx.common.constant.AnnotationValue.NULL;
+import static cool.scx.common.constant.AnnotationValues.NULL;
 
 /// 路由注解 默认会在方法上进行继承 如果想在子类禁用 请使用 NoScxRoute
 ///
