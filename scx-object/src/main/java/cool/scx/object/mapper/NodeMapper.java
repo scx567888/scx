960a89a7b403a39a45e20d08bb95a304a2a38029
scx567888 <scx567888@outlook.com>
2025-07-11 16:24:46 +0800

    dev

--- scx-object/src/main/java/cool/scx/object/mapper/NodeMapper.java
+++ scx-object/src/main/java/cool/scx/object/mapper/NodeMapper.java
@@ -2,12 +2,21 @@ package cool.scx.object.mapper;
 
 import cool.scx.object.node.Node;
 
+/// 值 和 Node 的双向映射器
 public interface NodeMapper<T> {
 
-    /// 值 转换为 Node, value 永不为 null
+    /// 将 值 转换为 Node.
+    ///
+    /// @param value    永不为 null
+    /// @param selector 可用于递归调用
+    /// @return Node
     Node toNode(T value, NodeMapperSelector selector);
 
-    /// Node 转换为 值, node 永不为 null
+    /// 将 Node 转换为 值.
+    ///
+    /// @param node     永不为 null, 但可以是 NullNode.NULL
+    /// @param selector 可用于递归调用
+    /// @return 值
     T fromNode(Node node, NodeMapperSelector selector);
 
 }
