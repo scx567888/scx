9e82e9205dc964886551ec2fbe16784ba4f9d8af
scx567888 <scx567888@outlook.com>
2025-07-24 14:47:38 +0800

    dev

--- scx-config/src/test/java/cool/scx/config/test/ScxConfigTest.java
+++ scx-config/src/test/java/cool/scx/config/test/ScxConfigTest.java
@@ -3,6 +3,7 @@ package cool.scx.config.test;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import cool.scx.config.ScxConfig;
 import cool.scx.config.ScxEnvironment;
+import cool.scx.config.source.ArgsConfigSource;
 import cool.scx.config.source.JsonFileConfigSource;
 import cool.scx.object.ScxObject;
 import org.testng.annotations.Test;
@@ -17,7 +18,7 @@ public class ScxConfigTest {
     public static void test1() throws JsonProcessingException {
         var scxEnvironment = new ScxEnvironment(ScxConfigTest.class);
         var jsonPath = scxEnvironment.getPathByAppRoot("AppRoot:scx-config.json");
-        var scxConfig = new ScxConfig(JsonFileConfigSource.of(jsonPath));
+        var scxConfig = new ScxConfig(JsonFileConfigSource.of(jsonPath), ArgsConfigSource.of("--scx.port=8888"));
         var m = scxConfig.configMapping();
         System.out.println(ScxObject.toJson(m));
         System.out.println(scxConfig.get("scx.port"));
