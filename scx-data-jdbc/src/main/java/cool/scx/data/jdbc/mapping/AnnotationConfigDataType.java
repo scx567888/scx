24e26b23c338b02741de14f06361725b097cae6b
scx567888 <scx567888@outlook.com>
2025-07-25 17:48:39 +0800

    dev

--- scx-data-jdbc/src/main/java/cool/scx/data/jdbc/mapping/AnnotationConfigDataType.java
+++ scx-data-jdbc/src/main/java/cool/scx/data/jdbc/mapping/AnnotationConfigDataType.java
@@ -2,9 +2,7 @@ package cool.scx.data.jdbc.mapping;
 
 import cool.scx.jdbc.JDBCType;
 import cool.scx.jdbc.mapping.DataType;
-import org.w3c.dom.TypeInfo;
-
-import java.lang.reflect.Type;
+import cool.scx.reflect.TypeInfo;
 
 import static cool.scx.data.jdbc.mapping.AnnotationConfigHelper.getDataTypeByJavaType;
 import static cool.scx.jdbc.JDBCType.VARCHAR;
