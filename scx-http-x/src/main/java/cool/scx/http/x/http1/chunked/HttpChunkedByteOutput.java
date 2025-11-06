efe6b8f94805f51e439c5cfced02a52fe0d7fa75
scx567888 <scx567888@outlook.com>
2025-11-06 15:22:53 +0800

    dev

--- scx-http-x/src/main/java/cool/scx/http/x/http1/chunked/HttpChunkedByteOutput.java
+++ scx-http-x/src/main/java/cool/scx/http/x/http1/chunked/HttpChunkedByteOutput.java
@@ -1,11 +1,9 @@
 package cool.scx.http.x.http1.chunked;
 
+import cool.scx.io.ByteChunk;
 import cool.scx.io.ByteOutput;
 import cool.scx.io.exception.AlreadyClosedException;
 import cool.scx.io.exception.ScxIOException;
-
-import java.io.IOException;
-import java.io.OutputStream;
 
 import static cool.scx.http.x.http1.Http1Helper.CHUNKED_END_BYTES;
 import static cool.scx.http.x.http1.Http1Helper.CRLF_BYTES;
@@ -13,59 +11,53 @@ import static cool.scx.http.x.http1.Http1Helper.CRLF_BYTES;
 public class HttpChunkedByteOutput implements ByteOutput {
 
     private static final byte[] HEX_DIGITS = {
-            '0', '1', '2', '3', '4', '5', '6', '7',
-            '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
+        '0', '1', '2', '3', '4', '5', '6', '7',
+        '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
     };
 
-    private final ByteOutput out;
+    private final ByteOutput byteOutput;
 
-    public HttpChunkedByteOutput(ByteOutput out) {
-        this.out = out;
+    public HttpChunkedByteOutput(ByteOutput byteOutput) {
+        this.byteOutput = byteOutput;
     }
 
     @Override
-    public void write(byte b)  {
-        write(new byte[]{b}, 0, 1);
+    public void write(byte b) {
+        write(ByteChunk.of(b));
     }
 
     @Override
-    public void write(byte[] b) throws ScxIOException, AlreadyClosedException {
-        write(b,0,b.length);
-    }
-
-    @Override
-    public void write(byte[] b, int off, int len)  {
+    public void write(ByteChunk b) throws ScxIOException, AlreadyClosedException {
         // 0 长度无需写入
-        if (len == 0) {
+        if (b.length == 0) {
             return;
         }
         // 写入分块
-        writeHexLength(len);    // 写入块大小
-        out.write(CRLF_BYTES);  // 写入 块大小 结束符
-        out.write(b, off, len); // 写入 数据块 内容
-        out.write(CRLF_BYTES);  // 写入 分块 结束符
+        writeHexLength(b.length);    // 写入块大小
+        byteOutput.write(CRLF_BYTES);  // 写入 块大小 结束符
+        byteOutput.write(b); // 写入 数据块 内容
+        byteOutput.write(CRLF_BYTES);  // 写入 分块 结束符
     }
 
     @Override
-    public void flush()  {
-        out.flush();
+    public void flush() {
+        byteOutput.flush();
     }
 
     @Override
     public boolean isClosed() {
-        //todo 待处理
-        return false;
+        return byteOutput.isClosed();
     }
 
     @Override
-    public void close()  {
+    public void close() {
         //写入终结分块
-        out.write(CHUNKED_END_BYTES);
-        out.close();
+        byteOutput.write(CHUNKED_END_BYTES);
+        byteOutput.close();
     }
 
     /// 直接写入十六进制表示的块大小
-    private void writeHexLength(int value)  {
+    private void writeHexLength(int value) {
         // 最大值 0xFFFFFFFF（32 位无符号整数）转换为十六进制最多 8 个字符
         var bytes = new byte[8];
         var pos = 8;
@@ -75,11 +67,11 @@ public class HttpChunkedByteOutput implements ByteOutput {
             value = value >>> 4; // 右移 4 位
         } while (value != 0);
 
-        out.write(bytes, pos, 8 - pos);
+        byteOutput.write(ByteChunk.of(bytes, pos, 8));
     }
 
-    public ByteOutput outputStream() {
-        return out;
+    public ByteOutput byteOutput() {
+        return byteOutput;
     }
 
 }
