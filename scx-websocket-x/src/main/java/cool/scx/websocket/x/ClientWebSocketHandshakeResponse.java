2b4a0668de5b849043388069571ca6203e130d18
scx567888 <scx567888@outlook.com>
2025-11-20 14:02:40 +0800

    dev

--- scx-websocket-x/src/main/java/cool/scx/websocket/x/ClientWebSocketHandshakeResponse.java
+++ scx-websocket-x/src/main/java/cool/scx/websocket/x/ClientWebSocketHandshakeResponse.java
@@ -4,12 +4,13 @@ import cool.scx.http.ScxHttpClientResponse;
 import cool.scx.http.body.ScxHttpBody;
 import cool.scx.http.headers.ScxHttpHeaders;
 import cool.scx.http.status_code.ScxHttpStatusCode;
+import cool.scx.http.version.HttpVersion;
 import cool.scx.http.x.http1.Http1ClientConnection;
 import cool.scx.websocket.ScxClientWebSocketHandshakeResponse;
 import cool.scx.websocket.ScxWebSocket;
 
 import static cool.scx.http.status_code.HttpStatusCode.SWITCHING_PROTOCOLS;
-
+import static cool.scx.http.version.HttpVersion.HTTP_1_1;
 
 public class ClientWebSocketHandshakeResponse implements ScxClientWebSocketHandshakeResponse {
 
@@ -43,6 +44,11 @@ public class ClientWebSocketHandshakeResponse implements ScxClientWebSocketHands
     @Override
     public ScxHttpStatusCode statusCode() {
         return response.statusCode();
+    }
+
+    @Override
+    public HttpVersion version() {
+        return HTTP_1_1;
     }
 
     @Override
