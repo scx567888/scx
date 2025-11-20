8057d4b8a84193aa4e7987f9d3b49592971a4287
scx567888 <scx567888@outlook.com>
2025-11-20 15:31:03 +0800

    dev

--- scx-websocket/src/main/java/cool/scx/websocket/close_info/ScxWebSocketCloseInfoImpl.java
+++
@@ -1,9 +0,0 @@
-package cool.scx.websocket.close_info;
-
-/// ScxWebSocketCloseInfo 默认实现
-///
-/// @author scx567888
-/// @version 0.0.1
-record ScxWebSocketCloseInfoImpl(int code, String reason) implements ScxWebSocketCloseInfo {
-
-}
