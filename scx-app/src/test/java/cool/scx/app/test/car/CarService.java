b3785a8370325b912d3de8289e5d1ba05a8edd9c
scx567888 <scx567888@outlook.com>
2025-11-22 17:33:00 +0800

    dev (#665)

--- scx-app/src/test/java/cool/scx/app/test/car/CarService.java
+++
@@ -1,9 +0,0 @@
-package cool.scx.app.test.car;
-
-import cool.scx.app.annotation.ScxService;
-import cool.scx.app.base.BaseModelService;
-
-@ScxService
-public class CarService extends BaseModelService<Car> {
-
-}
--- scx-app/src/test/java/cool/scx/app/test/like/LikeService.java
+++
@@ -1,9 +0,0 @@
-package cool.scx.app.test.like;
-
-import cool.scx.app.annotation.ScxService;
-import cool.scx.app.base.BaseModelService;
-
-@ScxService
-public class LikeService extends BaseModelService<Like> {
-
-}
